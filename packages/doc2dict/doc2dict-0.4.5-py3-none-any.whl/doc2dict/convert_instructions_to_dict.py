44de9e384112bf57a815fed87307557661faaf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:03:31 +0000

    Add package doc2dict/0.4.5/doc2dict-0.4.5-py3-none-any.whl

---
+++ packages/doc2dict/doc2dict-0.4.5-py3-none-any.whl/doc2dict/convert_instructions_to_dict.py
@@ -0,0 +1,1 @@
+# will move code here, for now just do new text stuff
---
+++ packages/doc2dict/doc2dict-0.4.5-py3-none-any.whl/doc2dict/txt/txt2dict.py
@@ -0,0 +1,4 @@
+
+def txt2dict(content,mapping_dict=None,encoding='utf-8'):
+    content = content.decode(encoding=encoding)
+    pass
