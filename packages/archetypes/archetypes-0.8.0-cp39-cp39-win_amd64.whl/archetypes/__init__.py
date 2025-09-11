11b1b3321e70b8d715dda938f20a86a79ca60555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:04:16 +0000

    Add package archetypes/0.8.0/archetypes-0.8.0-cp39-cp39-win_amd64.whl

---
+++ packages/archetypes/archetypes-0.8.0-cp39-cp39-win_amd64.whl/archetypes/__init__.py
@@ -0,0 +1,12 @@
+from archetypes.numpy import AA, ADA, BiAA, FairAA, FairKernelAA, KernelAA
+
+__all__ = [
+    "AA",
+    "BiAA",
+    "ADA",
+    "FairAA",
+    "KernelAA",
+    "FairKernelAA",
+]
+
+__version__ = "0.8.0"
