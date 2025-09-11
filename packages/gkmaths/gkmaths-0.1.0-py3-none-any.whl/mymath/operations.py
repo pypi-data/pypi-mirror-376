6ce87a883a3902e6dc23cb707cd46dd1fafb450a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:04:12 +0000

    Add package gkmaths/0.1.0/gkmaths-0.1.0-py3-none-any.whl

---
+++ packages/gkmaths/gkmaths-0.1.0-py3-none-any.whl/mymath/operations.py
@@ -0,0 +1,8 @@
+def multiply(a: int, b: int) -> int:
+    """Multiply two integer numbers"""
+    return a * b
+
+
+def add(a: int, b: int) -> int:
+    """Add two integer numbers"""
+    return a + b
