8d073274a1a46aa29a4599f0c9a393a978f6f6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:04:36 +0000

    Add package vyomcloudbridge/0.2.83/vyomcloudbridge-0.2.83.tar.gz

---
+++ packages/vyomcloudbridge/vyomcloudbridge-0.2.83.tar.gz/vyomcloudbridge-0.2.83/vyomcloudbridge/list_service.py
@@ -0,0 +1,26 @@
+from vyomcloudbridge.services.queue_worker import QueueWorker
+from vyomcloudbridge.services.dir_watcher import DirWatcher
+from vyomcloudbridge.services.mission_stats import MissionStats
+from vyomcloudbridge.services.machine_stats import MachineStats
+from vyomcloudbridge.services.vyom_listener import VyomListener
+from vyomcloudbridge.utils.install_specs import InstallSpecs
+from typing import Dict, Type
+
+
+AVAILABLE_SERVICES: Dict[str, Type] = {
+    "queueworker": QueueWorker,
+    "dirwatcher": DirWatcher,
+    "missionstats": MissionStats,
+    "machinestats": MachineStats,
+    "vyomlistener": VyomListener,
+}
+
+install_specs = InstallSpecs()
+if install_specs.is_full_install:
+    from vyomcloudbridge.services.mavproxy_hq import MavproxyHq
+    from vyomcloudbridge.services.ros_publisher import RosPublisher
+    from vyomcloudbridge.services.robot_stats import RobotStat
+
+    AVAILABLE_SERVICES["mavproxyhq"] = MavproxyHq
+    AVAILABLE_SERVICES["rospublisher"] = RosPublisher
+    AVAILABLE_SERVICES["robotstat"] = RobotStat
