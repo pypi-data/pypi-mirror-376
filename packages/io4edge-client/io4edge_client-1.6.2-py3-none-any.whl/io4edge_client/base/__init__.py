6f35a3f298a9bd35f523a90e25aa5d86b66072c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:03:49 +0000

    Add package io4edge-client/1.6.2/io4edge_client-1.6.2-py3-none-any.whl

---
+++ packages/io4edge-client/io4edge_client-1.6.2-py3-none-any.whl/io4edge_client/base/__init__.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifer: Apache-2.0
+from .client import Client  # noqa: F401
---
+++ packages/io4edge-client/io4edge_client-1.6.2-py3-none-any.whl/io4edge_client/version.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifer: Apache-2.0
+version = (1, 6, 2)
+VERSION = "%d.%d.%d" % version
