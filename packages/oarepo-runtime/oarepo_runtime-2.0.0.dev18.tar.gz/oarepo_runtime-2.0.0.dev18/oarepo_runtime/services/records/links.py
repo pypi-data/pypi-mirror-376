5d45d40202d5b41fbc278d87a0cd11107eb1af16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:04:01 +0000

    Add package oarepo-runtime/2.0.0.dev18/oarepo_runtime-2.0.0.dev18.tar.gz

---
+++ packages/oarepo-runtime/oarepo_runtime-2.0.0.dev18.tar.gz/oarepo_runtime-2.0.0.dev18/oarepo_runtime/services/records/links.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-runtime (see http://github.com/oarepo/oarepo-runtime).
+#
+# oarepo-runtime is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Utility for rendering URI template links."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from invenio_records_resources.services.base.links import EndpointLink
+
+
+def pagination_endpoint_links_html(endpoint: str, params: dict[str, Any] | None = None) -> dict[str, EndpointLink]:
+    """Create pagination links (prev/self/next) from the same endpoint."""
+    return {
+        "prev_html": EndpointLink(
+            endpoint,
+            when=lambda pagination, _ctx: pagination.has_prev,
+            vars=lambda pagination, _vars: _vars["args"].update({"page": pagination.prev_page.page}),
+            params=params,
+        ),
+        "self_html": EndpointLink(endpoint, params=params),
+        "next_html": EndpointLink(
+            endpoint,
+            when=lambda pagination, _ctx: pagination.has_next,
+            vars=lambda pagination, _vars: _vars["args"].update({"page": pagination.next_page.page}),
+            params=params,
+        ),
+    }
