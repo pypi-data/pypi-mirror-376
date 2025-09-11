efa3ed9115cf48187da2d2430dfe22d13cd8b065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:04:23 +0000

    Add package opik/1.8.44/opik-1.8.44-py3-none-any.whl

---
+++ packages/opik/opik-1.8.44-py3-none-any.whl/opik/evaluation/evaluation_result.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+
+import dataclasses
+
+from . import test_result
+
+
+@dataclasses.dataclass
+class EvaluationResult:
+    experiment_id: str
+    dataset_id: str
+    experiment_name: Optional[str]
+    test_results: List[test_result.TestResult]
