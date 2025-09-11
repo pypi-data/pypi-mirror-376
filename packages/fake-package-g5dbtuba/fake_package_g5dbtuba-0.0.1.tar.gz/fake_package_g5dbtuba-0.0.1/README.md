df4f067e76cc7d08a7f5054da45b441920e767cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:03:35 +0000

    Add package fake-package-g5dbtuba/0.0.1/fake_package_g5dbtuba-0.0.1.tar.gz

---
+++ packages/fake-package-g5dbtuba/fake_package_g5dbtuba-0.0.1.tar.gz/fake_package_g5dbtuba-0.0.1/README.md
@@ -0,0 +1,8 @@
+# fake-package-g5dbtuba
+
+**WARNING: This is a fake package created for security verification purposes.**
+
+DO NOT use this package in production. It contains fake credentials that are 
+intentionally exposed for testing security scanning tools.
+
+This package was created as part of a security verification task (ID: 059174040228228995733).
---
+++ packages/fake-package-g5dbtuba/fake_package_g5dbtuba-0.0.1.tar.gz/fake_package_g5dbtuba-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fake-package-g5dbtuba",
+    version="0.0.1",
+    description="Fake package for security verification - DO NOT USE",
+    packages=find_packages(),
+    install_requires=[],
+    author="Security Verification",
+    author_email="security@example.com",
+    url="https://example.com",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+)
