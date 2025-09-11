94c121ac83920278756ecdd21ffd9d627b4c5b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:04:01 +0000

    Add package snake-query-sdk/1.0.0/snake_query_sdk-1.0.0.tar.gz

---
+++ packages/snake-query-sdk/snake_query_sdk-1.0.0.tar.gz/snake_query_sdk-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="snake-query-sdk",
+    version="1.0.0",
+    author="",
+    author_email="",
+    description="Python SDK for Snake Query API - Natural language data querying with AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your-org/snake-query-python-sdk",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests>=2.25.0',
+    ],
+    keywords=["snake-query", "api", "natural-language", "data-query", "ai", "sdk"],
+)
