55bcf8fb6ba580abbeeecdd80e3e88f625477930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:03:33 +0000

    Add package dcoraid/0.17.4/dcoraid-0.17.4-py3-none-any.whl

---
+++ packages/dcoraid/dcoraid-0.17.4-py3-none-any.whl/dcoraid/dbmodel/db_api.py
@@ -0,0 +1,223 @@
+from itertools import islice
+import sys
+import urllib.parse
+
+import numpy as np
+
+from .db_core import DBInterrogator
+from .extract import DBExtract
+
+
+if sys.version_info >= (3, 12):
+    from itertools import batched
+else:
+    def batched(iterable, chunk_size):
+        iterator = iter(iterable)
+        while chunk := tuple(islice(iterator, chunk_size)):
+            yield chunk
+
+
+class APIInterrogator(DBInterrogator):
+    def __init__(self, api):
+        self.api = api.copy()
+        if api.user_id:
+            mode = "user"
+            user_data = api.get_user_dict()
+        else:
+            mode = "public"
+            user_data = None
+        super(APIInterrogator, self).__init__(mode=mode, user_data=user_data)
+
+    def get_circles(self):
+        """Return the list of DCOR Circle names
+        """
+        if self.mode == "user":
+            # Organizations the user is a member of
+            circle_dict = self.api.get("organization_list_for_user",
+                                       id=self.api.user_id,
+                                       permission="read")
+            data = [dd["name"] for dd in circle_dict]
+        else:
+            data = self.api.get("organization_list")
+        return data
+
+    def get_collections(self):
+        """Return the list of DCOR Collection names"""
+        if self.mode == "user":
+            collection_dict = self.api.get("group_list_authz", am_member=True)
+            data = [dd["name"] for dd in collection_dict]
+        else:
+            data = self.api.get("group_list")
+        if len(data) == 1000:
+            raise NotImplementedError(
+                "Reached hard limit of 1000 results! "
+                + "Please ask someone to implement this with `offset`.")
+        return data
+
+    def get_datasets_user_following(self):
+        """Return datasets the user is following"""
+        assert self.mode == "user"
+        data = self.api.get("dataset_followee_list", id=self.api.user_name)
+        return DBExtract(data)
+
+    def get_datasets_user_owned(self):
+        """Return datasets the user created"""
+        assert self.mode == "user"
+        dbextract = self.search_dataset(
+            filter_queries=[f"+creator_user_id:{self.api.user_id}"],
+            limit=0,
+        )
+        return dbextract
+
+    def get_datasets_user_shared(self):
+        """Return datasets shared with the user"""
+        assert self.mode == "user"
+        # Perform a dataset search with all circles and collections.
+        # This search may become too large (414 Request-URI Too Large).
+        # Limit the search to 20 circles/collections.
+        dbextract = DBExtract()
+
+        for circles_batch in batched(self.get_circles(), 20):
+            dbextract += self.search_dataset(
+                circles=list(circles_batch),
+                filter_queries=[f"-creator_user_id:{self.api.user_id}"],
+                limit=0,
+                )
+
+        for collections_batch in batched(self.get_collections(), 20):
+            dbextract += self.search_dataset(
+                collections=list(collections_batch),
+                filter_queries=[f"-creator_user_id:{self.api.user_id}"],
+                limit=0,
+                )
+
+        # all packages the user is a collaborator in
+        collaborated = self.api.get("package_collaborator_list_for_user",
+                                    id=self.user_data["id"])
+        for col in collaborated:
+            if col["package_id"] not in dbextract:
+                ds_dict = self.api.get("package_show", id=col["package_id"])
+                dbextract.add_datasets([ds_dict])
+
+        return dbextract
+
+    def get_users(self, ret_fullnames=False):
+        """Return the list of DCOR users"""
+        data = self.api.get("user_list")
+        user_list = []
+        full_list = []
+        for dd in data:
+            user_list.append(dd["name"])
+            if dd["fullname"]:
+                full_list.append(dd["fullname"])
+            else:
+                full_list.append(dd["name"])
+        if ret_fullnames:
+            return user_list, full_list
+        else:
+            return user_list
+
+    def search_dataset(self, query="*:*", filter_queries=None, circles=None,
+                       collections=None, circle_collection_union=False,
+                       limit=100):
+        """Search datasets via the CKAN API
+
+        Parameters
+        ----------
+        query: str
+            search query
+        filter_queries: list of str
+            SOLR `fq` filter queries (are joined with 'AND')
+        circles: list of str
+            list of circles (organizations) to search in
+        collections: list of str
+            list of collections (groups) to search in
+        circle_collection_union: bool
+            If set to True, make a union of the circle and collection
+            sets. Otherwise (default), search only for datasets that
+            are at least member of one of the circles and one of the
+            collections.
+        limit: int
+            limit number of search results; Set to 0 to get all results
+        """
+        if filter_queries is None:
+            filter_queries = []
+        # https://docs.ckan.org/en/latest/user-guide.html#search-in-detail
+        if circles:
+            solr_circles = [f"organization:{ci}" for ci in circles]
+            if len(circles) == 1:
+                solr_circle_query = solr_circles[0]
+            else:
+                solr_circle_query = f"({' OR '.join(solr_circles)})"
+        else:
+            solr_circle_query = None
+
+        if collections:
+            solr_collections = [f"groups:{co}" for co in collections]
+            if len(collections) == 1:
+                solr_collections_query = solr_collections[0]
+            else:
+                solr_collections_query = f"({' OR '.join(solr_collections)})"
+        else:
+            solr_collections_query = None
+
+        if solr_circle_query and solr_collections_query:
+            if circle_collection_union:
+                fq = f"({solr_circle_query} OR {solr_collections_query})"
+            else:
+                fq = f"({solr_circle_query} AND {solr_collections_query})"
+        elif solr_circle_query:
+            fq = f"{solr_circle_query}"
+        elif solr_collections_query:
+            fq = f"{solr_collections_query}"
+        else:
+            fq = ""
+        if fq:
+            filter_queries.append(fq)
+        if len(filter_queries) == 0:
+            final_fq = ""
+        elif len(filter_queries) == 1:
+            final_fq = filter_queries[0]
+        else:
+            final_fq = f"({' AND '.join(filter_queries)})"
+
+        if limit < 0:
+            raise ValueError(f"`limit` must be 0 or >0!, got {limit}!")
+        elif limit == 0:
+            rows = 1000  # default batch size
+            limit = np.inf
+        else:
+            rows = min(1000, limit)
+
+        num_total = np.inf  # just the initial value
+        num_retrieved = 0
+        dbextract = DBExtract()
+        while num_retrieved < min(limit, num_total) and rows:
+            data = self.api.get("package_search",
+                                q=urllib.parse.quote(query, safe=""),
+                                fq=final_fq,
+                                include_private=(self.mode == "user"),
+                                rows=rows,
+                                start=num_retrieved,
+                                )
+            if np.isinf(num_total):
+                # first iteration
+                num_total = data["count"]
+            num_retrieved += len(data["results"])
+            if num_retrieved + rows > min(limit, num_total):
+                # in the next iteration, only get the final
+                # few results.
+                rows = num_total - num_retrieved
+            dbextract.add_datasets(data["results"])
+
+        return dbextract
+
+    @property
+    def local_timestamp(self):
+        """Local database date in seconds since epoch"""
+        return self.remote_timestamp
+
+    @property
+    def local_version_score(self):
+        """Local database version"""
+        return self.remote_version_score
---
+++ packages/dcoraid/dcoraid-0.17.4-py3-none-any.whl/dcoraid/gui/main.py
@@ -0,0 +1,315 @@
+import atexit
+from contextlib import ExitStack
+import logging
+from importlib import resources
+import signal
+import sys
+import traceback as tb
+
+import dclab
+import requests
+import requests_cache
+import requests_toolbelt
+import urllib3
+
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+
+from ..api import APIOutdatedError
+from ..common import ConnectionTimeoutErrors
+from ..dbmodel import APIInterrogator, DBExtract
+from .._version import __version__
+
+from .api import get_ckan_api
+from .preferences import PreferencesDialog
+from .status_widget import StatusWidget
+from . import updater
+from .wizard import SetupWizard
+
+file_manager = ExitStack()
+atexit.register(file_manager.close)
+
+# set Qt icon theme search path
+ref = resources.files('dcoraid.img') / 'icon-theme'
+path = file_manager.enter_context(resources.as_file(ref))
+QtGui.QIcon.setThemeSearchPaths([str(path)])
+QtGui.QIcon.setThemeName(".")
+
+
+class DCORAid(QtWidgets.QMainWindow):
+    plots_changed = QtCore.pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        """Initialize DCOR-Aid
+
+        If you pass the "--version" command line argument, the
+        application will print the version after initialization
+        and exit.
+        """
+        self._update_thread = None
+        self._update_worker = None
+
+        # Settings are stored in the .ini file format. Even though
+        # `self.settings` may return integer/bool in the same session,
+        # in the next session, it will reliably return strings. Lists
+        # of strings (comma-separated) work nicely though.
+        # Some promoted widgets need the below constants set in order
+        # to access the settings upon initialization.
+        QtCore.QCoreApplication.setOrganizationName("DCOR")
+        QtCore.QCoreApplication.setOrganizationDomain("dc-cosmos.org")
+        QtCore.QCoreApplication.setApplicationName("dcoraid")
+        QtCore.QSettings.setDefaultFormat(QtCore.QSettings.Format.IniFormat)
+
+        super(DCORAid, self).__init__(*args, **kwargs)
+
+        # if "--version" was specified, print the version and exit
+        if "--version" in sys.argv:
+            print(__version__)
+            QtWidgets.QApplication.processEvents(
+                QtCore.QEventLoop.ProcessEventsFlag.AllEvents, 300)
+            sys.exit(0)
+
+        #: DCOR-Aid settings
+        self.settings = QtCore.QSettings()
+        ref_ui = resources.files("dcoraid.gui") / "main.ui"
+        with resources.as_file(ref_ui) as path_ui:
+            uic.loadUi(path_ui, self)
+
+        # setup logging
+        root_logger = logging.getLogger()
+        root_logger.addHandler(self.panel_logs.log_handler)
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(f"DCOR-Aid {__version__}")
+
+        # GUI
+        # Preferences dialog
+        self.dlg_pref = PreferencesDialog(self)
+        # Window title
+        self.setWindowTitle(f"DCOR-Aid {__version__}")
+        # Disable native menubar (e.g. on Mac)
+        self.menubar.setNativeMenuBar(False)
+        # File menu
+        self.actionPreferences.triggered.connect(self.dlg_pref.show)
+        self.actionSetupWizard.triggered.connect(self.on_wizard)
+        # Help menu
+        self.actionSoftware.triggered.connect(self.on_action_software)
+        self.actionAbout.triggered.connect(self.on_action_about)
+
+        # Display login status
+        self.status_widget = StatusWidget(self.tabWidget)
+        self.tabWidget.setCornerWidget(self.status_widget)
+        self.status_widget.clicked.connect(self.dlg_pref.on_show_server)
+
+        # Signals for user datasets (my data)
+        self.pushButton_user_refresh.clicked.connect(
+            self.on_refresh_private_data)
+
+        # Signal for requesting resource download
+        self.panel_browse_public.request_download.connect(
+            self.panel_download.download_resource)
+        self.user_filter_chain.download_resource.connect(
+            self.panel_download.download_resource)
+
+        QtWidgets.QApplication.processEvents(
+            QtCore.QEventLoop.ProcessEventsFlag.AllEvents, 300)
+
+        if self.settings.value("user scenario", "") == "anonymous":
+            # disable tabs that an anonymous user cannot use
+            self.tab_user.setEnabled(False)
+            self.tab_maintain.setEnabled(False)
+            self.tab_upload.setEnabled(False)
+            self.tab_share.setEnabled(False)
+
+        # Run wizard if necessary
+        if ((self.settings.value("user scenario", "") != "anonymous")
+                and not self.settings.value("auth/api key", "")):
+            # User has not done anything yet
+            self.on_wizard()
+
+        # check for updates
+        do_update = int(self.settings.value("check for updates", "1"))
+        self.on_action_check_update(do_update)
+
+        self.show()
+        self.raise_()
+
+        self.status_widget.request_status_update()
+
+    @QtCore.pyqtSlot(QtCore.QEvent)
+    def closeEvent(self, event):
+        root_logger = logging.getLogger()
+        while len(root_logger.handlers) > 0:
+            h = root_logger.handlers[0]
+            root_logger.removeHandler(h)
+        self.panel_upload.prepare_quit()
+        self.panel_download.prepare_quit()
+        self.status_widget.prepare_quit()
+        QtWidgets.QApplication.processEvents(
+            QtCore.QEventLoop.ProcessEventsFlag.AllEvents, 300)
+        event.accept()
+
+    @QtCore.pyqtSlot()
+    def on_action_about(self):
+        dcor = "https://dcor.mpl.mpg.de"
+        gh = "DCOR-dev/DCOR-Aid"
+        rtd = "dc.readthedocs.io"
+        about_text = (
+            f"This is the client for the <a href='{dcor}'>"
+            f"Deformability Cytometry Open Repository (DCOR)</a>.<br><br>"
+            f"Author: Paul Müller<br>"
+            f"GitHub: "
+            f"<a href='https://github.com/{gh}'>{gh}</a><br>"
+            f"Documentation: "
+            f"<a href='https://{rtd}'>{rtd}</a><br>")
+        QtWidgets.QMessageBox.about(self,
+                                    f"DCOR-Aid {__version__}",
+                                    about_text)
+
+    @QtCore.pyqtSlot(bool)
+    def on_action_check_update(self, b):
+        self.settings.setValue("check for updates", f"{int(b)}")
+        if b and self._update_thread is None:
+            self._update_thread = QtCore.QThread()
+            self._update_worker = updater.UpdateWorker()
+            self._update_worker.moveToThread(self._update_thread)
+            self._update_worker.finished.connect(self._update_thread.quit)
+            self._update_worker.data_ready.connect(
+                self.on_action_check_update_finished)
+            self._update_thread.start()
+
+            ghrepo = "DCOR-dev/DCOR-Aid"
+
+            QtCore.QMetaObject.invokeMethod(
+                self._update_worker,
+                'processUpdate',
+                QtCore.Qt.ConnectionType.QueuedConnection,
+                QtCore.Q_ARG(str, __version__),
+                QtCore.Q_ARG(str, ghrepo),
+            )
+
+    @QtCore.pyqtSlot(dict)
+    def on_action_check_update_finished(self, mdict):
+        # cleanup
+        self._update_thread.quit()
+        self._update_thread.wait()
+        self._update_worker = None
+        self._update_thread = None
+        # display message box
+        ver = mdict["version"]
+        web = mdict["releases url"]
+        dlb = mdict["binary url"]
+        msg = QtWidgets.QMessageBox()
+        msg.setWindowTitle(f"DCOR-Aid {ver} available!")
+        msg.setTextFormat(QtCore.Qt.TextFormat.RichText)
+        text = f"You can install DCOR-Aid {ver} "
+        if dlb is not None:
+            text += f'from a <a href="{dlb}">direct download</a>. '
+        else:
+            text += 'by running `pip install --upgrade dcoraid`. '
+        text += f'Visit the <a href="{web}">official release page</a>!'
+        msg.setText(text)
+        msg.exec()
+
+    @QtCore.pyqtSlot()
+    def on_action_software(self):
+        libs = [dclab,
+                requests,
+                requests_cache,
+                requests_toolbelt,
+                urllib3,
+                ]
+        sw_text = f"DCOR-Aid {__version__}\n\n"
+        sw_text += f"Python {sys.version}\n\n"
+        sw_text += "Modules:\n"
+        for lib in libs:
+            sw_text += f"- {lib.__name__} {lib.__version__}\n"
+        sw_text += f"- PyQt6 {QtCore.QT_VERSION_STR}\n"
+        sw_text += "\n Breeze icon theme by the KDE Community (LGPL)."
+        sw_text += "\n Font-Awesome icons by Fort Awesome (CC BY 4.0)."
+        if hasattr(sys, 'frozen'):
+            sw_text += "\nThis executable has been created using PyInstaller."
+        QtWidgets.QMessageBox.information(self,
+                                          "Software",
+                                          sw_text)
+
+    @QtCore.pyqtSlot()
+    def on_refresh_private_data(self):
+        self.tab_user.setCursor(QtCore.Qt.CursorShape.WaitCursor)
+        api = get_ckan_api()
+        data = DBExtract()
+        if api.is_available() and api.api_key:
+            try:
+                ai = APIInterrogator(api=api)
+                if self.checkBox_user_following.isChecked():
+                    data += ai.get_datasets_user_following()
+                if self.checkBox_user_owned.isChecked():
+                    data += ai.get_datasets_user_owned()
+                if self.checkBox_user_shared.isChecked():
+                    data += ai.get_datasets_user_shared()
+                self.user_filter_chain.set_db_extract(data)
+            except ConnectionTimeoutErrors:
+                self.logger.error(tb.format_exc())
+                QtWidgets.QMessageBox.critical(
+                    self,
+                    f"Failed to connect to {api.server}",
+                    tb.format_exc(limit=1))
+        self.tab_user.setCursor(QtCore.Qt.CursorShape.ArrowCursor)
+
+    @QtCore.pyqtSlot()
+    def on_wizard(self):
+        self.wizard = SetupWizard(self)
+        self.wizard.exec()
+
+
+def excepthook(etype, value, trace):
+    """
+    Handler for all unhandled exceptions.
+
+    :param `etype`: the exception type (`SyntaxError`,
+        `ZeroDivisionError`, etc...);
+    :type `etype`: `Exception`
+    :param string `value`: the exception error message;
+    :param string `trace`: the traceback header, if any (otherwise, it
+        prints the standard Python header: ``Traceback (most recent
+        call last)``.
+    """
+    vinfo = f"Unhandled exception in DCOR-Aid version {__version__}:\n"
+    exc_long = "".join([vinfo] + tb.format_exception(etype, value, trace))
+    exc_short = "".join([vinfo] + tb.format_exception(
+        etype, value, trace, limit=3))
+
+    # log the exception
+    logger = logging.getLogger(__name__)
+    logger.error(exc_long)
+
+    # Test connectivity
+    if etype is APIOutdatedError:
+        QtWidgets.QMessageBox.warning(
+            None,
+            "DCOR-Aid version outdated!",
+            "Your version of DCOR-Aid is outdated. Please "
+            + "update DCOR-Aid."
+        )
+
+    errorbox = QtWidgets.QMessageBox()
+    errorbox.setIcon(QtWidgets.QMessageBox.Icon.Critical)
+    copy_button = QtWidgets.QPushButton('Copy message to clipboard and close')
+    copy_button.clicked.connect(lambda: copy_text_to_clipboard(exc_long))
+    errorbox.addButton(QtWidgets.QPushButton('Close'),
+                       QtWidgets.QMessageBox.ButtonRole.YesRole)
+    errorbox.addButton(copy_button, QtWidgets.QMessageBox.ButtonRole.NoRole)
+    errorbox.setDetailedText(exc_long)
+    errorbox.setText(exc_short)
+    errorbox.exec()
+
+
+def copy_text_to_clipboard(text):
+    cb = QtWidgets.QApplication.clipboard()
+    cb.clear()
+    cb.setText(text)
+
+
+# Make Ctr+C close the app
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+# Display exception hook in separate dialog instead of crashing
+sys.excepthook = excepthook
