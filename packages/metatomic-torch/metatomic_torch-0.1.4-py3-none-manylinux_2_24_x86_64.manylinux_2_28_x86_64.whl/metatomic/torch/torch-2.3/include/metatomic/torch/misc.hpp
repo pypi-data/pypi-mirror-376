728e4464de38e11cca535ee5c8ffab38dd9de419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:04:24 +0000

    Add package metatomic-torch/0.1.4/metatomic_torch-0.1.4-py3-none-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/metatomic-torch/metatomic_torch-0.1.4-py3-none-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/metatomic/torch/torch-2.3/include/metatomic/torch/misc.hpp
@@ -0,0 +1,15 @@
+#ifndef METATOMIC_TORCH_MISC_HPP
+#define METATOMIC_TORCH_MISC_HPP
+
+#include <string>
+
+#include "metatomic/torch/exports.h"
+
+namespace metatomic_torch {
+
+/// Get the runtime version of metatensor-torch as a string
+METATOMIC_TORCH_EXPORT std::string version();
+
+}
+
+#endif
