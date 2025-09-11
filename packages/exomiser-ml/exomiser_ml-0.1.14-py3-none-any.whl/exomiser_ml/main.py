02e3f94428f8bc8f79f96769acef9d9c6604ffb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:04:01 +0000

    Add package exomiser-ml/0.1.14/exomiser_ml-0.1.14-py3-none-any.whl

---
+++ packages/exomiser-ml/exomiser_ml-0.1.14-py3-none-any.whl/exomiser_ml/main.py
@@ -0,0 +1,26 @@
+import click
+
+from exomiser_ml.cli import run_model_command, run_model_pipeline, add_features_command, split_data_command, \
+    post_process_test_dir_command, run_manual_logistic_regression_model_command, create_training_data_command
+
+
+@click.group()
+def main():
+    """main command group"""
+
+
+@main.group()
+def ml():
+    """machine learning utilities."""
+
+
+ml.add_command(add_features_command)
+ml.add_command(split_data_command)
+ml.add_command(create_training_data_command)
+ml.add_command(post_process_test_dir_command)
+ml.add_command(run_model_command)
+ml.add_command(run_model_pipeline)
+ml.add_command(run_manual_logistic_regression_model_command)
+
+if __name__ == "__main__":
+    main()
