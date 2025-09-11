5fee5d3634662cb763965192ddf550cd74dc19ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:04:29 +0000

    Add package phenofect/0.1.10/phenofect-0.1.10.tar.gz

---
+++ packages/phenofect/phenofect-0.1.10.tar.gz/phenofect-0.1.10/phenofect/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.10'
+
+import pkgutil
+import pandas as pd # type: ignore
+from io import StringIO  # noqa: F401
+
+def load_dataset(file_name):
+    data = pkgutil.get_data('phenofect', f'Data/{file_name}')
+    if data is None:
+        raise FileNotFoundError(f"File '{file_name}' does not exist in the Data directory.")
+    return pd.read_csv(StringIO(data.decode('utf-8')))
+
