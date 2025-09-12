4a09b19ea1a3d549c293050a0c4f38a1219e33f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:04:11 +0000

    Add package criteria-pattern/2.2.0/criteria_pattern-2.2.0-py3-none-any.whl

---
+++ packages/criteria-pattern/criteria_pattern-2.2.0-py3-none-any.whl/criteria_pattern/errors/__init__.py
@@ -0,0 +1,9 @@
+from .invalid_column_error import InvalidColumnError
+from .invalid_table_error import InvalidTableError
+from .sql_converter_error import SqlConverterError
+
+__all__ = (
+    'InvalidColumnError',
+    'InvalidTableError',
+    'SqlConverterError',
+)
