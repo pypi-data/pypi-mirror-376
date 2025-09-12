8415450958c40962197472d233d51b08b704b195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:03:53 +0000

    Add package PVNet/5.0.12/pvnet-5.0.12-py3-none-any.whl

---
+++ packages/PVNet/pvnet-5.0.12-py3-none-any.whl/pvnet/utils.py
@@ -0,0 +1,88 @@
+"""Utils"""
+import logging
+
+import rich.syntax
+import rich.tree
+from lightning.pytorch.utilities import rank_zero_only
+from omegaconf import DictConfig, OmegaConf
+
+logger = logging.getLogger(__name__)
+
+
+PYTORCH_WEIGHTS_NAME = "model_weights.safetensors"
+MODEL_CONFIG_NAME = "model_config.yaml"
+DATA_CONFIG_NAME = "data_config.yaml"
+DATAMODULE_CONFIG_NAME = "datamodule_config.yaml"
+FULL_CONFIG_NAME =  "full_experiment_config.yaml"
+MODEL_CARD_NAME = "README.md"
+
+
+
+def run_config_utilities(config: DictConfig) -> None:
+    """A couple of optional utilities.
+
+    Controlled by main config file:
+    - forcing debug friendly configuration
+
+    Modifies DictConfig in place.
+
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+    """
+
+    # Enable adding new keys to config
+    OmegaConf.set_struct(config, False)
+
+    # Force debugger friendly configuration if <config.trainer.fast_dev_run=True>
+    if config.trainer.get("fast_dev_run"):
+        logger.info("Forcing debugger friendly configuration! <config.trainer.fast_dev_run=True>")
+        # Debuggers don't like GPUs or multiprocessing
+        if config.trainer.get("gpus"):
+            config.trainer.gpus = 0
+        if config.datamodule.get("pin_memory"):
+            config.datamodule.pin_memory = False
+        if config.datamodule.get("num_workers"):
+            config.datamodule.num_workers = 0
+        if config.datamodule.get("prefetch_factor"):
+            config.datamodule.prefetch_factor = None
+
+    # Disable adding new keys to config
+    OmegaConf.set_struct(config, True)
+
+
+@rank_zero_only
+def print_config(
+    config: DictConfig,
+    fields: tuple[str] = (
+        "trainer",
+        "model",
+        "datamodule",
+        "callbacks",
+        "logger",
+        "seed",
+    ),
+    resolve: bool = True,
+) -> None:
+    """Prints content of DictConfig using Rich library and its tree structure.
+
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+        fields (Sequence[str], optional): Determines which main fields from config will
+        be printed and in what order.
+        resolve (bool, optional): Whether to resolve reference fields of DictConfig.
+    """
+
+    style = "dim"
+    tree = rich.tree.Tree("CONFIG", style=style, guide_style=style)
+
+    for field in fields:
+        branch = tree.add(field, style=style, guide_style=style)
+
+        config_section = config.get(field)
+        branch_content = str(config_section)
+        if isinstance(config_section, DictConfig):
+            branch_content = OmegaConf.to_yaml(config_section, resolve=resolve)
+
+        branch.add(rich.syntax.Syntax(branch_content, "yaml"))
+
+    rich.print(tree)
