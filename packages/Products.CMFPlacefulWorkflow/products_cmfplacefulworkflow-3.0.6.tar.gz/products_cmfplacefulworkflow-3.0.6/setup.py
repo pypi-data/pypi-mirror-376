e48e10d13f9025429af5aba924e1d3f3f98a231f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:06 +0000

    Add package Products.CMFPlacefulWorkflow/3.0.6/products_cmfplacefulworkflow-3.0.6.tar.gz

---
+++ packages/Products.CMFPlacefulWorkflow/products_cmfplacefulworkflow-3.0.6.tar.gz/products_cmfplacefulworkflow-3.0.6/setup.py
@@ -0,0 +1,59 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.6"
+
+
+setup(
+    name="Products.CMFPlacefulWorkflow",
+    version=version,
+    description="Workflow policies for Plone",
+    # Note: long_description is in setup.cfg
+    # to avoid needing workarounds for UnicodeDecodeErrors.
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="CMF Plone Zope2 workflow",
+    author="Encolpe DEGOUTE",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/Products.CMFPlacefulWorkflow",
+    license="GPL",
+    packages=find_packages("src"),
+    namespace_packages=["Products"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require=dict(
+        test=[
+            "plone.app.contenttypes[test]",
+            "plone.app.testing",
+            "plone.testing",
+            "transaction",
+        ],
+    ),
+    install_requires=[
+        "AccessControl",
+        "Acquisition",
+        "plone.base",
+        "Persistence",
+        "Products.CMFCore",
+        "Products.CMFPlone",
+        "Products.GenericSetup",
+        "setuptools",
+        "Zope",
+        "zope.component",
+        "zope.i18nmessageid",
+        "zope.interface",
+    ],
+)
