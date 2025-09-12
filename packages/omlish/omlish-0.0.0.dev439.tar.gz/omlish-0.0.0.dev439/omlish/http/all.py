35a340c85acecd968c0d8da42139cddbb49268ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:05:50 +0000

    Add package omlish/0.0.0.dev439/omlish-0.0.0.dev439.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev439.tar.gz/omlish-0.0.0.dev439/omlish/http/all.py
@@ -0,0 +1,59 @@
+from .. import lang as _lang
+
+
+with _lang.auto_proxy_init(globals()):
+    ##
+
+    from . import consts  # noqa
+
+    from .clients import (  # noqa
+        BaseHttpResponse,
+        HttpClient,
+        HttpClientError,
+        HttpRequest,
+        HttpResponse,
+        HttpxHttpClient,
+        StreamHttpResponse,
+        UrllibHttpClient,
+        client,
+        close_response,
+        closing_response,
+        read_response,
+        request,
+    )
+
+    from .cookies import (  # noqa
+        CookieTooBigError,
+        dump_cookie,
+        parse_cookie,
+    )
+
+    from .dates import (  # noqa
+        http_date,
+        parse_date,
+    )
+
+    from .encodings import (  # noqa
+        latin1_decode,
+        latin1_encode,
+    )
+
+    from .headers import (  # noqa
+        CanHttpHeaders,
+        HttpHeaders,
+        headers,
+    )
+
+    from .json import (  # noqa
+        JSON_TAGGER,
+        JsonTag,
+        JsonTagger,
+        json_dumps,
+        json_loads,
+    )
+
+    from .multipart import (  # noqa
+        MultipartData,
+        MultipartEncoder,
+        MultipartField,
+    )
---
+++ packages/omlish/omlish-0.0.0.dev439.tar.gz/omlish-0.0.0.dev439/omlish/lite/cached.py
@@ -0,0 +1,60 @@
+import functools
+import typing as ta
+
+
+T = ta.TypeVar('T')
+
+CallableT = ta.TypeVar('CallableT', bound=ta.Callable)
+
+
+##
+
+
+class _AbstractCachedNullary:
+    def __init__(self, fn):
+        super().__init__()
+
+        self._fn = fn
+        self._value = self._missing = object()
+        functools.update_wrapper(self, fn)
+
+    def __call__(self, *args, **kwargs):  # noqa
+        raise TypeError
+
+    def __get__(self, instance, owner=None):  # noqa
+        bound = instance.__dict__[self._fn.__name__] = self.__class__(self._fn.__get__(instance, owner))
+        return bound
+
+
+##
+
+
+class _CachedNullary(_AbstractCachedNullary):
+    def __call__(self, *args, **kwargs):  # noqa
+        if self._value is self._missing:
+            self._value = self._fn()
+        return self._value
+
+
+def cached_nullary(fn: CallableT) -> CallableT:
+    return _CachedNullary(fn)  # type: ignore
+
+
+def static_init(fn: CallableT) -> CallableT:
+    fn = cached_nullary(fn)
+    fn()
+    return fn
+
+
+##
+
+
+class _AsyncCachedNullary(_AbstractCachedNullary):
+    async def __call__(self, *args, **kwargs):
+        if self._value is self._missing:
+            self._value = await self._fn()
+        return self._value
+
+
+def async_cached_nullary(fn):  # ta.Callable[..., T]) -> ta.Callable[..., T]:
+    return _AsyncCachedNullary(fn)
