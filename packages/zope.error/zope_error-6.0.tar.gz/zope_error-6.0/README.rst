36ea6674146f995a3e1850a97f0ad638f531df8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:41 +0000

    Add package zope.error/6.0/zope_error-6.0.tar.gz

---
+++ packages/zope.error/zope_error-6.0.tar.gz/zope_error-6.0/README.rst
@@ -0,0 +1,11 @@
+``zope.error``
+==============
+
+.. image:: https://github.com/zopefoundation/zope.error/actions/workflows/tests.yml/badge.svg
+        :target: https://github.com/zopefoundation/zope.error/actions/workflows/tests.yml
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.error/badge.svg
+        :target: https://coveralls.io/github/zopefoundation/zope.error
+
+
+This package provides an error reporting utility which is able to store errors.
---
+++ packages/zope.error/zope_error-6.0.tar.gz/zope_error-6.0/setup.py
@@ -0,0 +1,80 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.error package
+"""
+
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(name='zope.error',
+      version='6.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description="An error reporting utility for Zope3",
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      license='ZPL-2.1',
+      keywords="zope3 error",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='https://github.com/zopefoundation/zope.error',
+      python_requires='>=3.9',
+      install_requires=[
+          'setuptools',
+          'zope.exceptions',
+          'zope.interface',
+          'zope.location',
+          'persistent',  # error.py imports from persistent
+      ],
+      extras_require={
+          'test': [
+              'zope.testing >= 3.8',
+              'zope.testrunner >= 6.4',
+          ],
+      },
+      include_package_data=True,
+      zip_safe=False,
+      )
