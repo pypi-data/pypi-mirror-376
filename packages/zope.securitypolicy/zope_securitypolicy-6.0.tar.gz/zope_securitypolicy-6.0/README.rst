d8ca6b751512fb4c119cda8a84559cf41a689215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:29 +0000

    Add package zope.securitypolicy/6.0/zope_securitypolicy-6.0.tar.gz

---
+++ packages/zope.securitypolicy/zope_securitypolicy-6.0.tar.gz/zope_securitypolicy-6.0/README.rst
@@ -0,0 +1,23 @@
+=====================
+ zope.securitypolicy
+=====================
+
+.. image:: https://img.shields.io/pypi/v/zope.securitypolicy.svg
+   :target: https://pypi.org/project/zope.securitypolicy/
+   :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.securitypolicy.svg
+   :target: https://pypi.org/project/zope.securitypolicy/
+   :alt: Supported Python versions
+
+.. image:: https://github.com/zopefoundation/zope.securitypolicy/actions/workflows/tests.yml/badge.svg
+   :target: https://github.com/zopefoundation/zope.securitypolicy/actions/workflows/tests.yml
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.securitypolicy/badge.svg?branch=master
+   :target: https://coveralls.io/github/zopefoundation/zope.securitypolicy?branch=master
+
+
+
+This package provides an useful security policy for Zope3. It's the
+default security policy used in "zope3 the application" and many other
+zope-based projects.
---
+++ packages/zope.securitypolicy/zope_securitypolicy-6.0.tar.gz/zope_securitypolicy-6.0/setup.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# https://zopetoolkit.readthedocs.io/
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.securitypolicy package
+"""
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(
+    name='zope.securitypolicy',
+    version='6.0',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    description='Default security policy for Zope3',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('src', 'zope', 'securitypolicy', 'zopepolicy.txt')
+        + '\n\n' +
+        read('CHANGES.rst')),
+    keywords="zope3 security policy",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    url='https://github.com/zopefoundation/zope.securitypolicy',
+    license='ZPL-2.1',
+    python_requires='>=3.9',
+    install_requires=[
+        'persistent',
+        'setuptools',
+        'zope.annotation',
+        'zope.authentication',
+        'zope.component',
+        'zope.configuration',
+        'zope.i18nmessageid',
+        'zope.interface >= 3.8',
+        'zope.location',
+        'zope.schema',
+        'zope.security',
+    ],
+    extras_require=dict(
+        test=[
+            'zope.testing',
+            'zope.testrunner >= 6.4',
+        ],
+        dublincore=[
+            'zope.dublincore >= 3.7',
+        ]),
+    include_package_data=True,
+    zip_safe=False,
+)
