8e8146f35a6d0045c2ec60c4bf3c634fe2bbe8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 09:04:30 +0000

    Add package llmling-agent/0.99.56/llmling_agent-0.99.56-py3-none-any.whl

---
+++ packages/llmling-agent/llmling_agent-0.99.56-py3-none-any.whl/llmling_agent_providers/litellm_provider/utils.py
@@ -0,0 +1,31 @@
+"""LiteLLM provider utils."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from llmling_agent.common_types import ModelProtocol
+from llmling_agent.log import get_logger
+
+
+logger = get_logger(__name__)
+
+
+@dataclass
+class Usage:
+    """Usage information for a model."""
+
+    total_tokens: int | None
+    input_tokens: int | None
+    output_tokens: int | None
+
+
+class LiteLLMModel(ModelProtocol):
+    """Compatible model class for LiteLLM."""
+
+    def __init__(self, model_name: str):
+        self._name = model_name
+
+    @property
+    def model_name(self) -> str:
+        return self._name.replace(":", "/")
