66577ff49f6217f51fdd6034a58045e6210b1b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:05:28 +0000

    Add package types-boto3-full/1.40.29/types_boto3_full-1.40.29-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.40.29-py3-none-any.whl/types_boto3_sms/__init__.py
@@ -0,0 +1,53 @@
+"""
+Main interface for sms service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_sms/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_sms import (
+        Client,
+        GetConnectorsPaginator,
+        GetReplicationJobsPaginator,
+        GetReplicationRunsPaginator,
+        GetServersPaginator,
+        ListAppsPaginator,
+        SMSClient,
+    )
+
+    session = Session()
+    client: SMSClient = session.client("sms")
+
+    get_connectors_paginator: GetConnectorsPaginator = client.get_paginator("get_connectors")
+    get_replication_jobs_paginator: GetReplicationJobsPaginator = client.get_paginator("get_replication_jobs")
+    get_replication_runs_paginator: GetReplicationRunsPaginator = client.get_paginator("get_replication_runs")
+    get_servers_paginator: GetServersPaginator = client.get_paginator("get_servers")
+    list_apps_paginator: ListAppsPaginator = client.get_paginator("list_apps")
+    ```
+"""
+
+from .client import SMSClient
+from .paginator import (
+    GetConnectorsPaginator,
+    GetReplicationJobsPaginator,
+    GetReplicationRunsPaginator,
+    GetServersPaginator,
+    ListAppsPaginator,
+)
+
+Client = SMSClient
+
+
+__all__ = (
+    "Client",
+    "GetConnectorsPaginator",
+    "GetReplicationJobsPaginator",
+    "GetReplicationRunsPaginator",
+    "GetServersPaginator",
+    "ListAppsPaginator",
+    "SMSClient",
+)
