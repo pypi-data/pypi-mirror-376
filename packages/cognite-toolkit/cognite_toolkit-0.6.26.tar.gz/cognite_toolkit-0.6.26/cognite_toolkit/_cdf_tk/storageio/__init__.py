0397f2d729f23d07eaef62b158ede6c94da20073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:40 +0000

    Add package cognite-toolkit/0.6.26/cognite_toolkit-0.6.26.tar.gz

---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.26.tar.gz/cognite_toolkit-0.6.26/cognite_toolkit/_cdf_tk/storageio/__init__.py
@@ -0,0 +1,42 @@
+from ._applications import ChartIO
+from ._asset_centric import AssetIO, BaseAssetCentricIO, FileMetadataIO
+from ._base import StorageIO, TableStorageIO
+from ._data_classes import InstanceIdCSVList, InstanceIdRow, ModelList
+from ._instances import InstanceIO
+from ._raw import RawIO
+from ._selectors import (
+    AllChartSelector,
+    AssetCentricFileSelector,
+    AssetCentricSelector,
+    AssetSubtreeSelector,
+    ChartFileSelector,
+    ChartOwnerSelector,
+    ChartSelector,
+    DataSetSelector,
+    InstanceSelector,
+    InstanceViewSelector,
+)
+
+__all__ = [
+    "AllChartSelector",
+    "AssetCentricFileSelector",
+    "AssetCentricSelector",
+    "AssetIO",
+    "AssetSubtreeSelector",
+    "BaseAssetCentricIO",
+    "ChartFileSelector",
+    "ChartIO",
+    "ChartOwnerSelector",
+    "ChartSelector",
+    "DataSetSelector",
+    "FileMetadataIO",
+    "InstanceIO",
+    "InstanceIdCSVList",
+    "InstanceIdRow",
+    "InstanceSelector",
+    "InstanceViewSelector",
+    "ModelList",
+    "RawIO",
+    "StorageIO",
+    "TableStorageIO",
+]
---
+++ packages/cognite-toolkit/cognite_toolkit-0.6.26.tar.gz/cognite_toolkit-0.6.26/last_changelog_entry.md
@@ -0,0 +1,10 @@
+## cdf 
+
+### Added
+
+- [alpha] Added a search configuration resource loader
+SearchConfigLoader.
+
+## templates
+
+No changes.
