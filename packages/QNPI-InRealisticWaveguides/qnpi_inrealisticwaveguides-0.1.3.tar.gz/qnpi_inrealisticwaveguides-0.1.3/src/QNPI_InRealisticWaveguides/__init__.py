0262582881f0d1851648f4d112f5353e4af29ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:22 +0000

    Add package QNPI-InRealisticWaveguides/0.1.3/qnpi_inrealisticwaveguides-0.1.3.tar.gz

---
+++ packages/QNPI-InRealisticWaveguides/qnpi_inrealisticwaveguides-0.1.3.tar.gz/qnpi_inrealisticwaveguides-0.1.3/src/QNPI_InRealisticWaveguides/__init__.py
@@ -0,0 +1,12 @@
+# QNPI/__init__.py
+
+# Import classes from the main module
+from .classes import Sellmeier, Dispersion, NonlinearOverlap, QNPISimulator, Outputs
+
+__all__ = [
+    "Sellmeier",
+    "Dispersion",
+    "NonlinearOverlap",
+    "QNPISimulator",
+    "Outputs",
+]
