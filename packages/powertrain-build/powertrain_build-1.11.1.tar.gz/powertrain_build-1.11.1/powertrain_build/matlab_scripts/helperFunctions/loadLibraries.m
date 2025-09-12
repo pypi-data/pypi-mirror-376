c5a8de15811221723b96a08ac5ad70a974c07219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:04:03 +0000

    Add package powertrain-build/1.11.1/powertrain_build-1.11.1.tar.gz

---
+++ packages/powertrain-build/powertrain_build-1.11.1.tar.gz/powertrain_build-1.11.1/powertrain_build/matlab_scripts/helperFunctions/loadLibraries.m
@@ -0,0 +1,11 @@
+% Copyright 2024 Volvo Car Corporation
+% Licensed under Apache 2.0.
+
+function loadLibraries(root_folder)
+    block_libraries = dir(fullfile(root_folder, '**\*.slx'));
+    for i=1:length(block_libraries)
+        block_library = block_libraries(i);
+        load_system(block_library.name);
+    end
+
+
