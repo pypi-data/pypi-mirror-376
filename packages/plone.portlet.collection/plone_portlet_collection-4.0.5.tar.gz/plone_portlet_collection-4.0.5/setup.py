9e6cdf80b110eb828ab06c9675c5707b9c497c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:45 +0000

    Add package plone.portlet.collection/4.0.5/plone_portlet_collection-4.0.5.tar.gz

---
+++ packages/plone.portlet.collection/plone_portlet_collection-4.0.5.tar.gz/plone_portlet_collection-4.0.5/setup.py
@@ -0,0 +1,63 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "4.0.5"
+
+setup(
+    name="plone.portlet.collection",
+    version=version,
+    description="A portlet that fetches results from a collection",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="collection portlet",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.portlet.collection/",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone", "plone.portlet"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.base",
+        "plone.memoize",
+        "plone.portlets",
+        "plone.app.portlets",
+        "plone.app.vocabularies",
+        "Products.GenericSetup",
+        "Products.MimetypesRegistry",
+        "plone.app.querystring",
+        "plone.app.uuid",
+        "plone.app.z3cform",
+        "plone.autoform",
+        "plone.i18n",
+        "plone.registry",
+        "Zope",
+    ],
+    extras_require={
+        "test": [
+            "lxml",
+            "plone.testing",
+            "plone.app.testing",
+            "plone.app.contenttypes[test]",
+        ],
+    },
+)
---
+++ packages/plone.portlet.collection/plone_portlet_collection-4.0.5.tar.gz/plone_portlet_collection-4.0.5/src/plone/portlet/collection/tests/test_functional_doctest.py
@@ -0,0 +1,39 @@
+"""Functional Doctests for plone.portlet.collection.
+
+These test are only triggered when Plone 4 (and plone.testing) is installed.
+"""
+
+from plone.portlet.collection.testing import PLONE_PORTLET_COLLECTION_FUNCTIONAL_TESTING
+from plone.testing import layered
+
+import doctest
+import pprint
+import unittest
+
+
+optionflags = (
+    doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE | doctest.REPORT_ONLY_FIRST_FAILURE
+)
+normal_testfiles = [
+    "functional.txt",
+]
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests(
+        [
+            layered(
+                doctest.DocFileSuite(
+                    test,
+                    optionflags=optionflags,
+                    globs={
+                        "pprint": pprint.pprint,
+                    },
+                ),
+                layer=PLONE_PORTLET_COLLECTION_FUNCTIONAL_TESTING,
+            )
+            for test in normal_testfiles
+        ]
+    )
+    return suite
