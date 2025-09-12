b74b4d359145d82cb58590d93472410f78c61aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 17:06:21 +0000

    Add package azx/0.1.7/azx-0.1.7.tar.gz

---
+++ packages/azx/azx-0.1.7.tar.gz/azx-0.1.7/src/azx/agents.py
@@ -0,0 +1,97 @@
+import base64
+import os
+from itertools import tee
+from pathlib import Path
+
+from openai import OpenAI, NOT_GIVEN
+
+
+_mime_types = {
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".gif": "image/gif",
+    ".webp": "image/webp",
+    ".bmp": "image/bmp",
+    ".tiff": "image/tiff",
+}
+
+_ocr_prompt = """
+Extract content from image.
+
+Your answer should be a json, containing abstract and full text in such struct: `{"abstract": "xxxx", "full": "xxxx"}`.
+
+The full text should be in markdown syntax. It may has lists, tables, code.
+
+If something looks like list in full text, represent it with legal markdown list syntax.
+""".strip()
+
+
+class Client:
+    def __init__(self, name: str, base_url: str, model: str, api_key: str, tools):
+        self.name = name
+        self.model = model
+        self.tools = tools
+        self._client = OpenAI(base_url=base_url, api_key=api_key)
+
+    def stream_response(self, messages: list[dict], json: bool = False):
+        response_format = {"type": "json_object"} if json else NOT_GIVEN
+        stream = self._client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            tools=self.tools,
+            response_format=response_format,
+            stream=True,
+            stream_options={"include_usage": True},
+        )
+
+        stream1, stream2 = tee(stream)
+        stream3, stream4 = tee(stream2)
+
+        return (
+            (
+                chunk.choices[0].delta.content
+                for chunk in stream1
+                if chunk.choices and chunk.choices[0].delta.content
+            ),
+            (
+                chunk.choices[0].delta.tool_calls
+                for chunk in stream3
+                if chunk.choices and chunk.choices[0].delta.tool_calls
+            ),
+            (
+                chunk.usage
+                for chunk in stream4
+                if chunk.usage is not None
+                and chunk.usage.total_tokens is not None
+                and chunk.usage.total_tokens > 0
+            ),
+        )
+
+    def ocr(self, uri, prompt=_ocr_prompt) -> str:
+        if os.path.exists(uri):
+            with open(uri, "rb") as image_file:
+                base64_data = base64.b64encode(image_file.read()).decode("utf-8")
+                mime_type = _mime_types.get(Path(uri).suffix.lower(), "image/jpeg")
+                uri = f"data:{mime_type};base64,{base64_data}"
+
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": uri},
+                    },
+                    {"type": "text", "text": prompt},
+                ],
+            }
+        ]
+
+        response = self._client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            response_format={"type": "json_object"},
+        )
+
+        return response.choices[0].message.content
---
+++ packages/azx/azx-0.1.7.tar.gz/azx-0.1.7/src/azx/renderer.py
@@ -0,0 +1,90 @@
+from markdown_it import MarkdownIt
+from rich.console import Console
+from rich.live import Live
+from rich.markdown import Markdown
+from rich.text import Text
+from rich.theme import Theme
+
+console = Console(
+    theme=Theme(
+        {
+            "markdown.block_quote": "violet",
+            "markdown.hr": "medium_purple4",
+            "markdown.link_url": "sky_blue1",
+        }
+    )
+)
+
+refresh_freq = 25
+
+
+def render_error(string):
+    text = Text(string)
+    text.stylize("red")
+    console.print(text)
+
+
+def render_user_input(string):
+    text = Text(f">>> {string}")
+    text.stylize("green")
+    console.print(text)
+
+
+def render_md_full(string):
+    console.print(Markdown(string))
+
+
+def render_md_stream(strings) -> str:
+    whole_string = ""
+    current_block = ""
+
+    def block_recognized():
+        nonlocal current_block
+        md = MarkdownIt("js-default")
+        tokens_len = 0
+        for char in _flatten_strings(strings):
+            current_block += char
+            tokens = md.parse(current_block)
+            new_block = (
+                tokens_len != len(tokens)
+                and current_block[-3:][:2] == "\n\n"
+                and char not in "-*123456789|"
+            )
+            tokens_len = len(tokens)
+            yield (char, new_block)
+
+    def new_live() -> Live:
+        live = Live(console=console, refresh_per_second=refresh_freq)
+        live.start()
+        return live
+
+    live = new_live()
+
+    for char, new_block in block_recognized():
+        if new_block:
+            current_block = char
+            live.stop()
+            console.print("")
+            live = new_live()
+
+        whole_string += char
+        live.update(Markdown(current_block))
+
+    live.stop()
+
+    return whole_string
+
+
+def render_sys_stream(strings) -> str:
+    whole_string = ""
+    with Live(console=console, refresh_per_second=refresh_freq) as live:
+        for char in _flatten_strings(strings):
+            whole_string += char
+            live.update(Text(whole_string, "bright_black"))
+    return whole_string
+
+
+def _flatten_strings(strings):
+    for string in strings:
+        for char in string:
+            yield char
