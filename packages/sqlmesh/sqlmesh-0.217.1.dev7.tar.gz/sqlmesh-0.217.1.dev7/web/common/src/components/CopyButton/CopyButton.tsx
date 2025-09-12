bd4740b36509b4f7719aa5fed98cde622058a678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:04:24 +0000

    Add package sqlmesh/0.217.1.dev7/sqlmesh-0.217.1.dev7.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.217.1.dev7.tar.gz/sqlmesh-0.217.1.dev7/web/common/src/components/CopyButton/CopyButton.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+import { Button, type ButtonProps } from '@/components/Button/Button'
+
+type TimerID = ReturnType<typeof setTimeout>
+
+export interface CopyButtonProps extends Omit<ButtonProps, 'children'> {
+  text: string
+  delay?: number
+  children: (copied: boolean) => React.ReactNode
+}
+
+export const CopyButton = React.forwardRef<HTMLButtonElement, CopyButtonProps>(
+  (
+    {
+      text,
+      title = 'Copy to clipboard',
+      variant = 'secondary',
+      size = 'xs',
+      delay = 2000,
+      disabled = false,
+      children,
+      onClick,
+      ...props
+    },
+    ref,
+  ) => {
+    const [copied, setCopied] = useState<TimerID | null>(null)
+
+    const copy = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
+      e.stopPropagation()
+
+      if (copied) {
+        clearTimeout(copied)
+      }
+
+      navigator.clipboard.writeText(text).then(() => {
+        setCopied(setTimeout(() => setCopied(null), delay))
+      })
+
+      onClick?.(e)
+    }
+
+    return (
+      <Button
+        ref={ref}
+        data-component="CopyButton"
+        title={title}
+        size={size}
+        variant={variant}
+        onClick={copy}
+        disabled={disabled || !!copied}
+        {...props}
+      >
+        {children(copied != null)}
+      </Button>
+    )
+  },
+)
+CopyButton.displayName = 'CopyButton'
