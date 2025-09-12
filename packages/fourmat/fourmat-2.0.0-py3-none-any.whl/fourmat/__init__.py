9d9933b42cfbe0ee7bf4d31ea1e250181f9520cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:05:05 +0000

    Add package fourmat/2.0.0/fourmat-2.0.0-py3-none-any.whl

---
+++ packages/fourmat/fourmat-2.0.0-py3-none-any.whl/fourmat/__init__.py
@@ -0,0 +1,30 @@
+from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+
+import click
+
+# -----------------------------------------------------------------------------
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+# -----------------------------------------------------------------------------
+
+ASSETS_DIR = Path(__file__).parent / "assets"
+
+# -----------------------------------------------------------------------------
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    pass
+
+
+# -----------------------------------------------------------------------------
+
+from . import lint  # isort:skip  # noqa: F401
