b05b2e7e43b43eb7a2b3747b5f2d10df20c31d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:37 +0000

    Add package specifyx/0.2.5/specifyx-0.2.5-py3-none-any.whl

---
+++ packages/specifyx/specifyx-0.2.5-py3-none-any.whl/specify_cli/services/template_service/__init__.py
@@ -0,0 +1,17 @@
+from .template_service import (
+    JinjaTemplateService,
+    RenderResult,
+    TemplateFolderMapping,
+    TemplateRenderResult,
+    TemplateService,
+    get_template_service,
+)
+
+__all__ = [
+    "TemplateService",
+    "JinjaTemplateService",
+    "TemplateFolderMapping",
+    "RenderResult",
+    "TemplateRenderResult",
+    "get_template_service",
+]
