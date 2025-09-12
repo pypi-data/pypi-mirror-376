4dec40566fc16a0f2b6e1e26dab20b7f80fc792e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:38 +0000

    Add package excepting/0.0.2/excepting-0.0.2-py3-none-any.whl

---
+++ packages/excepting/excepting-0.0.2-py3-none-any.whl/excepting/core/__init__.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+from typing import *
+
+__all__ = ["Exceptor"]
+
+
+class Exceptor:
+
+    __slots__ = ("_captured",)
+
+    captured: Optional[BaseException]
+
+    def __init__(self: Self) -> None:
+        self._captured = None
+
+    @contextmanager
+    def capture(self: Self, *args: type[BaseException]) -> Generator[Self, None, None]:
+        "This contextmanager captures exceptions."
+        self._captured = None
+        exc: BaseException
+        try:
+            yield self
+        except args as exc:
+            self._captured = exc
+
+    @property
+    def captured(self: Self) -> Optional[BaseException]:
+        "This property stores the captured exception."
+        return self._captured
