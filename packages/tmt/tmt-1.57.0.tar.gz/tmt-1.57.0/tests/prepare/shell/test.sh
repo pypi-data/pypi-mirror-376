750ac8f6d5556195540fc58c49da9cd2cd9f8383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:30 +0000

    Add package tmt/1.57.0/tmt-1.57.0.tar.gz

---
+++ packages/tmt/tmt-1.57.0.tar.gz/tmt-1.57.0/tests/prepare/shell/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "pushd data"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Custom Script"
+        rlRun "tmt run -rv plan -n custom" 0 "Prepare using a custom script"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Commandline Script"
+        rlRun "tmt run -arv plan -n custom \
+            prepare -h shell -s './prepare.sh'" 0 "Prepare using a custom script from cmdline"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Multiple Commandline Scripts"
+        rlRun "tmt run -arv plans -n multiple \
+            prepare -h shell -s 'touch /tmp/first' -s 'touch /tmp/second'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Remote Script"
+        rlRun -s "tmt -vvv run provision prepare finish cleanup plan -n url" 0 "Prepare using a remote script"
+        rlAssertGrep "Hello world" "$rlRun_LOG" #check for the prepare script
+        rlAssertGrep "third" "$rlRun_LOG" # check for the finish script
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+    rlPhaseEnd
+rlJournalEnd
