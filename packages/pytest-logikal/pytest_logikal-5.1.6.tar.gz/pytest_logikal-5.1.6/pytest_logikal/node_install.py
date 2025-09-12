2f685bd967ec5e4438fbf8a130f1f470fe1361bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:04:37 +0000

    Add package pytest-logikal/5.1.6/pytest_logikal-5.1.6.tar.gz

---
+++ packages/pytest-logikal/pytest_logikal-5.1.6.tar.gz/pytest_logikal-5.1.6/pytest_logikal/node_install.py
@@ -0,0 +1,13 @@
+import subprocess
+from pathlib import Path
+from sys import stderr
+
+from termcolor import colored
+
+
+def install_node_packages(node_prefix: Path | None = None) -> None:
+    node_prefix = node_prefix or Path(__file__).parent
+    if not (node_prefix / 'node_modules').exists():
+        print(colored('Installing Node.js packages', 'yellow', attrs=['bold']), file=stderr)
+        command = ['npm', 'install', '--no-save', '--prefix', str(node_prefix)]
+        subprocess.run(command, text=True, check=True)  # nosec: secure, not using untrusted input
