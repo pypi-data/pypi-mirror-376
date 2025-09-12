eb5b2bdc4bb64ccb42ff7b36acd29132b1b2a121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:14 +0000

    Add package plone.app.redirector/3.0.5/plone_app_redirector-3.0.5.tar.gz

---
+++ packages/plone.app.redirector/plone_app_redirector-3.0.5.tar.gz/plone_app_redirector-3.0.5/setup.py
@@ -0,0 +1,58 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.5"
+
+setup(
+    name="plone.app.redirector",
+    version=version,
+    description="redirection tool",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    keywords="links redirect",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.app.redirector",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.memoize",
+        "Zope>=5",
+        "BTrees",
+        "Products.CMFCore",
+        "Products.ZCatalog",
+        "persistent",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.contenttypes[test]",
+            "plone.base",
+            "plone.registry",
+            "plone.testing",
+        ]
+    },
+)
