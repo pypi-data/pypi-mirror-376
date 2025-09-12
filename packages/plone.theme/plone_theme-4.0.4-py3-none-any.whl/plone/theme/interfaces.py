38902b8b1c45b774a7f4aa1a32fd9daf317995ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:38 +0000

    Add package plone.theme/4.0.4/plone_theme-4.0.4-py3-none-any.whl

---
+++ packages/plone.theme/plone_theme-4.0.4-py3-none-any.whl/plone/theme/interfaces.py
@@ -0,0 +1,5 @@
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IDefaultPloneLayer(IDefaultBrowserLayer):
+    """A Zope 3 browser layer corresponding to Plone defaults"""
