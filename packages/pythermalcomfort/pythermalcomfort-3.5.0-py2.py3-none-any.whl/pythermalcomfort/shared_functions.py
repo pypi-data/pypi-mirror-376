f03a49e802871584d09d5132b3ff3e543e7f0374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:06:05 +0000

    Add package pythermalcomfort/3.5.0/pythermalcomfort-3.5.0-py2.py3-none-any.whl

---
+++ packages/pythermalcomfort/pythermalcomfort-3.5.0-py2.py3-none-any.whl/pythermalcomfort/shared_functions.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def valid_range(x, valid) -> np.ndarray:
+    """Filter values based on a valid range."""
+    return np.where((x >= valid[0]) & (x <= valid[1]), x, np.nan)
+
+
+def mapping(value, map_dictionary, right=True):
+    """Map a temperature array to stress categories.
+
+    Parameters
+    ----------
+    value : float, array-like
+        Temperature to map.
+    map_dictionary: dict
+        Dictionary used to map the values
+    right: bool, optional
+        Indicating whether the intervals include the right or the left bin edge.
+
+    Returns
+    -------
+    Stress category for each input temperature.
+
+    """
+    bins = np.array(list(map_dictionary.keys()))
+    words = np.append(np.array(list(map_dictionary.values())), "unknown")
+    return words[np.digitize(value, bins, right=right)]
