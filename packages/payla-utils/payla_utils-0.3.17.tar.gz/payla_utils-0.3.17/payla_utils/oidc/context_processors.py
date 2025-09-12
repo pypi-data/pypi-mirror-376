50bcad2b71fe982483af62e0b475c670fd1e30a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:28 +0000

    Add package payla-utils/0.3.17/payla_utils-0.3.17.tar.gz

---
+++ packages/payla-utils/payla_utils-0.3.17.tar.gz/payla_utils-0.3.17/payla_utils/oidc/context_processors.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.http import HttpRequest
+
+
+def get_auth_backend_status(request: HttpRequest) -> dict:
+    return {
+        "django_login_allowed": "django.contrib.auth.backends.ModelBackend" in settings.AUTHENTICATION_BACKENDS,
+        "payla_oidc_login_allowed": "mailing.auth.PaylaOIDCAuthenticationBackend" in settings.AUTHENTICATION_BACKENDS,
+    }
