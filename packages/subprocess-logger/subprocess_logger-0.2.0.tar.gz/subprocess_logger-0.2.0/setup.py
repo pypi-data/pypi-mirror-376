64ec97cd6587383620a4c64ecadd1ee01f8c7ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:04:57 +0000

    Add package subprocess-logger/0.2.0/subprocess_logger-0.2.0.tar.gz

---
+++ packages/subprocess-logger/subprocess_logger-0.2.0.tar.gz/subprocess_logger-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="subprocess_logger",
+    version="0.2.0",
+    description="Consolidated handler for subprocess logging in Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Vi-Nk",
+    url="https://github.com/Vi-Nk/subprocess-logger/",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="subprocess logging loggers",
+    license="MIT License",
+    include_package_data=True,
+)
