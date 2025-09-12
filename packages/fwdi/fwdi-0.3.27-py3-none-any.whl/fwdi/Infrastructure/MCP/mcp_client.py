048c09d70cee1e2fddfff0f3a5c6009c7e4b98d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:05:59 +0000

    Add package fwdi/0.3.27/fwdi-0.3.27-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.3.27-py3-none-any.whl/fwdi/Infrastructure/MCP/mcp_client.py
@@ -0,0 +1,75 @@
+from typing import Any, overload
+from contextlib import asynccontextmanager
+from fastmcp import Client
+from mcp.types import CallToolResult, GetPromptResult, TextResourceContents, BlobResourceContents
+from ...Application.Abstractions.External.base_mcp_client import BaseMCPClient
+
+class MCPClient(BaseMCPClient):
+    def __init__(self, api_key:str|None=None, host:str='127.0.0.1', port:int=6504, path:str='mcp'):
+        super().__init__()
+        self.__base_url:str = f"http://{host}:{port}/{path}"
+
+        if api_key:
+            self.__client:Client = Client(self.__base_url, auth=api_key)
+        else:
+            self.__client:Client = Client(self.__base_url)
+
+    @classmethod
+    def create_from_url(cls, url:str, api_key:str|None=None):
+        instance = cls()
+        instance.__base_url = url
+        if api_key:
+            instance.__client = Client(url, auth=api_key)
+        else:
+            instance.__client = Client(url)
+
+    async def check_avaible(self):
+        return True if await self.__client.ping() else False
+    
+    async def load_env(self)->bool:
+        try:
+            async with self.__client as client:
+                self.tools = await client.list_tools()
+                self.prompts = await client.list_prompts()
+                self.resources = await client.list_resources()
+                
+                return True
+            
+            return False
+        except Exception as ex:
+            print(f"ERROR:{ex}")
+            return False
+    
+    @property
+    def is_connected(self)->bool:
+        return self.__client.is_connected()
+
+    async def ping(self)->bool:
+        async with self.__client as client:
+            result = await client.ping()
+            return result
+
+    async def call_tool(self, name_fn:str, param:dict[str, Any]=None)->CallToolResult:
+        async with self.__client as client:
+            if param:
+                result = await client.call_tool(name_fn, param)
+            else:
+                result = await client.call_tool(name_fn)
+
+        return result
+
+    async def read_resource(self, uri:str)->list[TextResourceContents | BlobResourceContents]:
+        async with self.__client as client:
+            result = await client.read_resource(uri)
+
+            return result
+        return []
+
+    async def get_prompt(self, name:str, param:dict[str, Any]=None)->GetPromptResult:
+        async with self.__client as client:
+            if param:
+                result = await client.get_prompt(name, arguments=param)
+            else:
+                result = await client.get_prompt(name)
+
+        return result
