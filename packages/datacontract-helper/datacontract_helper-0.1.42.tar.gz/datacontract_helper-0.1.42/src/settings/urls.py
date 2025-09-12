f42d8a3e6e772c356741649e41c5a77a1d11da3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 17:05:17 +0000

    Add package datacontract-helper/0.1.42/datacontract_helper-0.1.42.tar.gz

---
+++ packages/datacontract-helper/datacontract_helper-0.1.42.tar.gz/datacontract_helper-0.1.42/src/settings/urls.py
@@ -0,0 +1,8 @@
+SCHEMA_REGISTRY_HOST: str = "http://localhost"
+SCHEMA_REGISTRY_PORT: int = 8081
+
+BOOTSTRAP_SERVER: str = "localhost"
+BOOTSTRAP_PORT: int = 9092
+
+
+NEXUS_URL: str = "https://nexus.k8s-analytics.ostrovok.in/#browse/browse:datacontract_pypi"
