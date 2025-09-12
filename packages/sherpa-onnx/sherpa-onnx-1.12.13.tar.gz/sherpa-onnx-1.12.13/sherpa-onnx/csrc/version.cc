2d53058647c9c9d701f1eb7d6b48693c461e5085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 17:05:48 +0000

    Add package sherpa-onnx/1.12.13/sherpa-onnx-1.12.13.tar.gz

---
+++ packages/sherpa-onnx/sherpa-onnx-1.12.13.tar.gz/sherpa-onnx-1.12.13/sherpa-onnx/csrc/version.cc
@@ -0,0 +1,24 @@
+// sherpa-onnx/csrc/version.h
+//
+// Copyright      2025  Xiaomi Corporation
+
+#include "sherpa-onnx/csrc/version.h"
+
+namespace sherpa_onnx {
+
+const char *GetGitDate() {
+  static const char *date = "Fri Sep 12 08:03:15 2025";
+  return date;
+}
+
+const char *GetGitSha1() {
+  static const char *sha1 = "32c248b8";
+  return sha1;
+}
+
+const char *GetVersionStr() {
+  static const char *version = "1.12.13";
+  return version;
+}
+
+}  // namespace sherpa_onnx
