e36ad90b0aff56d0e65e9f9ebcfa74d6ffbe182f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:05:01 +0000

    Add package LinguAligner/1.0.7/LinguAligner-1.0.7.tar.gz

---
+++ packages/LinguAligner/LinguAligner-1.0.7.tar.gz/LinguAligner-1.0.7/LinguAligner/__init__.py
@@ -0,0 +1,72 @@
+"""LinguAligner is a Python library for aligning annotations in parallel corpora. It is designed to be used in the context of parallel corpora annotation alignment, where the goal is to align annotations in the source language with annotations in the target language. """
+__version__ = "1.0.7"
+
+
+from . import aligners
+import spacy
+from . import translation
+from transformers import BertTokenizer, BertModel, logging
+import logging
+logging.getLogger("transformers.modeling_utils").setLevel(logging.ERROR)
+
+
+config_default = config= {
+    "pipeline": [ "lemma", "M_trans", "word_aligner","gestalt","levenshtein"], # can be changed according to the desired pipeline
+    "spacy_model": "pt_core_news_lg", # change according to the language
+    "WAligner_model": "bert-base-multilingual-uncased", # needed for word_aligner
+}
+ 
+class AlignmentPipeline:
+    def __init__(self, config=config_default):
+        self.config = config
+        print("Loading spacy model: " + config["spacy_model"])
+        self.nlp = spacy.load(config["spacy_model"])
+        print("Model loaded")
+        if "word_aligner" in config["pipeline"]:
+            print("Loading WAligner model: " + config["WAligner_model"])
+            self.tokenizer = BertTokenizer.from_pretrained(config["WAligner_model"])
+            self.model = BertModel.from_pretrained(config["WAligner_model"])
+            print("Model loaded")
+
+    def align_annotation(self, src_sent, src_ann, tgt_sent, trans_ann, src_ann_start=0, M_trans_dict=None):
+        pipeline = self.config["pipeline"]
+        method_used = None
+        #if "M_Trans" in pipeline and lookupTable == None:              
+            #pipeline.remove("M_Trans")
+            #print("Lookup table not provided for M_Trans method. (skipped)")
+        nlp = self.nlp
+        res = aligners.regex_string_match(tgt_sent,trans_ann) 
+        span = (-1,-1)
+        if res:
+            method_used = 'regex'
+        if not res:
+            i = 0
+            while i < len(pipeline) and not res:
+                method = pipeline[i]
+                #print(method)
+                if method == 'lemma':
+                    res = aligners.lemma_match(tgt_sent,trans_ann,nlp)
+                elif method == 'M_trans': # Mtrans is combined with lemma method since we also calculate the lemma of the translations
+                    if M_trans_dict != None: 
+                        res = aligners.resource_match(tgt_sent,src_ann,nlp,M_trans_dict)
+                elif method == 'word_aligner':
+                    res = aligners.wordAligner(src_sent,tgt_sent,src_ann,nlp, self.tokenizer, self.model)
+                elif method == 'gestalt':
+                    res = aligners.gestalt_match(tgt_sent,trans_ann,nlp)
+                elif method == 'levenshtein':
+                    res = aligners.levenshtein_match(tgt_sent,trans_ann,nlp)
+                else:
+                    print(f"Invalid alignment method: {method}")
+                if res:
+                    method_used = method
+                i += 1
+        
+        
+        if res:
+            span = aligners.closest_occurrence(tgt_sent,res,src_ann_start)
+
+        return res, span, method_used
+# Example usage:
+# alignment_pipeline = AlignmentPipeline()
+# res = alignment_pipeline.align_annotation("The ship land on the shore","O barco desembarcou na costa","land","terra",nlp) # Expected output: "teste
+#print(res)
