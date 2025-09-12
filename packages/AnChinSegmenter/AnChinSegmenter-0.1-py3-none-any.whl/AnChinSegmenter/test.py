24b11f5ca3d66c65922642882f8742c7e9f7aee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:32 +0000

    Add package AnChinSegmenter/0.1/AnChinSegmenter-0.1-py3-none-any.whl

---
+++ packages/AnChinSegmenter/AnChinSegmenter-0.1-py3-none-any.whl/AnChinSegmenter/test.py
@@ -0,0 +1,6 @@
+# import sys
+# print(sys.path)
+# import sys
+# sys.path.append('/Users/tangtang/opt/anaconda3/lib/python3.9/site-packages')
+import AnChinSegmenter
+AnChinSegmenter.main(input_path='./data/sample_data.txt',output_path='./data/sample_data_out.txt')
