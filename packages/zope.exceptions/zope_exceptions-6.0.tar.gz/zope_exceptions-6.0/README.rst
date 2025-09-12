cbb524455639d32e1b9a9386195a3765461017b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:43 +0000

    Add package zope.exceptions/6.0/zope_exceptions-6.0.tar.gz

---
+++ packages/zope.exceptions/zope_exceptions-6.0.tar.gz/zope_exceptions-6.0/README.rst
@@ -0,0 +1,26 @@
+=================
+ zope.exceptions
+=================
+
+.. image:: https://img.shields.io/pypi/v/zope.exceptions.svg
+        :target: https://pypi.python.org/pypi/zope.exceptions/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.exceptions.svg
+        :target: https://pypi.org/project/zope.exceptions/
+        :alt: Supported Python versions
+
+.. image:: https://github.com/zopefoundation/zope.exceptions/actions/workflows/tests.yml/badge.svg
+        :target: https://github.com/zopefoundation/zope.exceptions/actions/workflows/tests.yml
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.exceptions/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.exceptions?branch=master
+
+.. image:: https://readthedocs.org/projects/zopeexceptions/badge/?version=latest
+        :target: https://zopeexceptions.readthedocs.io/en/latest/
+        :alt: Documentation Status
+
+This package contains exception exceptions and implementations which are so
+general purpose that they don't belong in Zope application-specific packages.
+
+Please see https://zopeexceptions.readthedocs.io/ for the documentation.
---
+++ packages/zope.exceptions/zope_exceptions-6.0.tar.gz/zope_exceptions-6.0/setup.py
@@ -0,0 +1,76 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# https://zopetoolkit.readthedocs.io/
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.exceptions package
+"""
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(
+    name='zope.exceptions',
+    version='6.0',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    description='Zope Exceptions',
+    long_description=(read('README.rst') + '\n\n' +
+                      read('CHANGES.rst')),
+    keywords='zope exceptions',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    url='https://github.com/zopefoundation/zope.exceptions',
+    project_urls={
+        'Issue Tracker': ('https://github.com/zopefoundation/'
+                          'zope.exceptions/issues'),
+        'Sources': 'https://github.com/zopefoundation/zope.exceptions',
+    },
+    license='ZPL-2.1',
+    python_requires='>=3.9',
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'docs': ['Sphinx', 'repoze.sphinx.autointerface'],
+        'test': ['zope.testrunner >= 6.4'],
+    },
+)
