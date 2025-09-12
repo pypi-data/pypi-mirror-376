71c502e6994fc662c3d199741e1b7281dbc40086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:29 +0000

    Add package whose-encoder/0.2.0/whose_encoder-0.2.0.tar.gz

---
+++ packages/whose-encoder/whose_encoder-0.2.0.tar.gz/whose_encoder-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="whose_encoder",                  # 你要发布的包名，pip install fruit_encoder
+    version="0.2.0",                       # 版本号，每次更新要改版本！！
+    author="Ruo1.-_1",
+    author_email="213243435@sey.edu.cn",
+    description="一个将文件编码为'苹果'/'安卓'文本，以及解码还原的工具",
+    long_description=long_description,     # 来自 README.md
+    long_description_content_type="text/markdown",
+    packages=find_packages(),              # 自动发现所有包，比如 fruit_encoder/
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # 或其他协议
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',               # 支持的 Python 版本
+    include_package_data=True,
+)
