36afc929cb8986eee4952c249407a62db9970709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:04:15 +0000

    Add package LightGeometry/1.0.0/lightgeometry-1.0.0-py3-none-any.whl

---
+++ packages/LightGeometry/lightgeometry-1.0.0-py3-none-any.whl/LightGeometry/__init__.py
@@ -0,0 +1,16 @@
+from .geometry import(
+    circle_area,
+    circle_perimeter,
+    circle_radius,
+    rectangle_area,
+    rectangle_diagonal,
+    rectangle_perimeter,
+    triangle_area,
+    triangle_height,
+    triangle_perimeter,
+    polygon_perimeter,
+    distance_2d,
+    distance_3d
+)
+
+__version__ = "1.0.0"
