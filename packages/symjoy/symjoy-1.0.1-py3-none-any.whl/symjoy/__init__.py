abe9cdc058adbd3144ffa5be758acf435a77beb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:04:43 +0000

    Add package symjoy/1.0.1/symjoy-1.0.1-py3-none-any.whl

---
+++ packages/symjoy/symjoy-1.0.1-py3-none-any.whl/symjoy/__init__.py
@@ -0,0 +1,15 @@
+from .emojis import emojis
+from .symbols import symbols
+from .arrows import arrows
+from .math_symbols import math_symbols
+from .currency import currency
+from .misc import misc
+
+__all__ = ["emojis", "symbols", "arrows", "math_symbols", "currency", "misc"]
+
+__version__ = "1.0.1"
+
+__license__ = "MIT"
+
+__author__ = "Vishwanath M M"
+
