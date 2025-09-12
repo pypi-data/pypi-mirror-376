d3ece0b4a4aed68d4cb92c7ccb087caa29545183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:06 +0000

    Add package Products.CMFPlacefulWorkflow/3.0.6/products_cmfplacefulworkflow-3.0.6-py3-none-any.whl

---
+++ packages/Products.CMFPlacefulWorkflow/products_cmfplacefulworkflow-3.0.6-py3-none-any.whl/Products/CMFPlacefulWorkflow/permissions.py
@@ -0,0 +1,7 @@
+"""Zope 2 permissions"""
+
+from AccessControl.Permission import addPermission
+
+
+ManageWorkflowPolicies = "CMFPlacefulWorkflow: Manage workflow policies"
+addPermission(ManageWorkflowPolicies, ("Manager", "Site Administrator"))
---
+++ packages/Products.CMFPlacefulWorkflow/products_cmfplacefulworkflow-3.0.6-py3-none-any.whl/Products/CMFPlacefulWorkflow/testing.py
@@ -0,0 +1,54 @@
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.testing import zope
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.GenericSetup import EXTENSION
+from Products.GenericSetup import profile_registry
+
+import Products.CMFPlacefulWorkflow
+
+
+class ProductsCmfplacefulworkflowLayer(PloneSandboxLayer):
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        self.loadZCML(package=Products.CMFPlacefulWorkflow)
+        profile_registry.registerProfile(
+            name="exportimport",
+            title="Test Placeful Workflow Profile",
+            description=("Tests the placeful workflow policy handler."),
+            path="profiles/exportimport",
+            product="Products.CMFPlacefulWorkflow.tests",
+            profile_type=EXTENSION,
+            for_=ISiteRoot,
+        )
+        zope.installProduct(app, "Products.CMFPlacefulWorkflow")
+
+    def setUpPloneSite(self, portal):
+        portal.acl_users.userFolderAddUser(
+            SITE_OWNER_NAME, SITE_OWNER_PASSWORD, ["Manager"], []
+        )
+        applyProfile(portal, "Products.CMFPlacefulWorkflow:CMFPlacefulWorkflow")
+
+
+PRODUCTS_CMFPLACEFULWORKFLOW_FIXTURE = ProductsCmfplacefulworkflowLayer()
+
+
+PRODUCTS_CMFPLACEFULWORKFLOW_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PRODUCTS_CMFPLACEFULWORKFLOW_FIXTURE,),
+    name="ProductsCmfplacefulworkflowLayer:IntegrationTesting",
+)
+
+
+PRODUCTS_CMFPLACEFULWORKFLOW_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PRODUCTS_CMFPLACEFULWORKFLOW_FIXTURE,),
+    name="ProductsCmfplacefulworkflowLayer:FunctionalTesting",
+)
