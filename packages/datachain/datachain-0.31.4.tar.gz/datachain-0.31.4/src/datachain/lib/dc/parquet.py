12aa57cef624296b39c378456ecc6f2c609f9b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 07:07:58 +0000

    Add package datachain/0.31.4/datachain-0.31.4.tar.gz

---
+++ packages/datachain/datachain-0.31.4.tar.gz/datachain-0.31.4/src/datachain/lib/dc/parquet.py
@@ -0,0 +1,62 @@
+import os
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+from datachain.lib.data_model import DataType
+from datachain.query import Session
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec
+
+    from .datachain import DataChain
+
+    P = ParamSpec("P")
+
+
+def read_parquet(
+    path: Union[str, os.PathLike[str], list[str], list[os.PathLike[str]]],
+    partitioning: Any = "hive",
+    output: Optional[dict[str, DataType]] = None,
+    column: str = "",
+    model_name: str = "",
+    source: bool = True,
+    session: Optional[Session] = None,
+    settings: Optional[dict] = None,
+    **kwargs,
+) -> "DataChain":
+    """Generate chain from parquet files.
+
+    Parameters:
+        path: Storage URI with directory. URI must start with storage prefix such
+            as `s3://`, `gs://`, `az://` or "file:///".
+        partitioning: Any pyarrow partitioning schema.
+        output: Dictionary defining column names and their corresponding types.
+        column: Created column name.
+        model_name: Generated model name.
+        source: Whether to include info about the source file.
+        session: Session to use for the chain.
+        settings: Settings to use for the chain.
+
+    Example:
+        Reading a single file:
+        ```py
+        import datachain as dc
+        dc.read_parquet("s3://mybucket/file.parquet")
+        ```
+
+        Reading a partitioned dataset from a directory:
+        ```py
+        import datachain as dc
+        dc.read_parquet("s3://mybucket/dir")
+        ```
+    """
+    from .storage import read_storage
+
+    chain = read_storage(path, session=session, settings=settings, **kwargs)
+    return chain.parse_tabular(
+        output=output,
+        column=column,
+        model_name=model_name,
+        source=source,
+        format="parquet",
+        partitioning=partitioning,
+    )
