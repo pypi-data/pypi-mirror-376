6edec2e6c076694cf5673421980966f6b4b42e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:04:00 +0000

    Add package hassette/0.4.0/hassette-0.4.0-py3-none-any.whl

---
+++ packages/hassette/hassette-0.4.0-py3-none-any.whl/hassette/core/apps/__init__.py
@@ -0,0 +1,10 @@
+from .app import App, AppSync
+from .app_config import DEFAULT_CONFIG, AppConfig, AppConfigT
+
+__all__ = [
+    "DEFAULT_CONFIG",
+    "App",
+    "AppConfig",
+    "AppConfigT",
+    "AppSync",
+]
