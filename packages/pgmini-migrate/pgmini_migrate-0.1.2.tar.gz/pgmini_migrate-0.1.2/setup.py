6de7c281bb56c5363ea87dae26e37d761fd3e939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:44 +0000

    Add package pgmini-migrate/0.1.2/pgmini_migrate-0.1.2.tar.gz

---
+++ packages/pgmini-migrate/pgmini_migrate-0.1.2.tar.gz/pgmini_migrate-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pgmini-migrate",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["psycopg2-binary", 'psycopg', 'python-dotenv'],
+    entry_points={
+        "console_scripts": [
+            "pgmigrate=pgmini_migrate.cli:main",
+        ],
+    },
+)
