ee95e513a17347cd458e2eb3b6401efdc86a324c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 07:07:36 +0000

    Add package mcdreforged/2.15.2b1/mcdreforged-2.15.2b1-py3-none-any.whl

---
+++ packages/mcdreforged/mcdreforged-2.15.2b1-py3-none-any.whl/mcdreforged/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. versionadded:: v2.15.0
+	Module ``mcdreforged`` now contains everything in ``mcdreforged.api.all``. All MCDR API components can be imported from here
+"""
+
+from mcdreforged.api.all import *
+
+# noinspection PyPep8Naming
+from mcdreforged.constants.core_constant import VERSION_PYPI as __version__
