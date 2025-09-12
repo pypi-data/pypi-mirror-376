5715f96af684b9f820c80caaeddd36e7d140a66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 09:04:32 +0000

    Add package lookup-license/0.1.26/lookup_license-0.1.26.tar.gz

---
+++ packages/lookup-license/lookup_license-0.1.26.tar.gz/lookup_license-0.1.26/lookup_license/config.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+module_name = 'lookup_license'
+module_author = 'hesa'
+lookup_license_version = "0.1.26"
+
+default_minimum_score = 0.9
+http_timeout = 10 # seconds
+
+module_name = 'lookup_license'
+
+DESCRIPTION = """NAME
+  lookup-license
+
+DESCRIPTION
+  Python tool to identify license from license text or license names.
+"""
+
+EPILOG = """
+AUTHOR
+  Henrik Sandklef
+
+PROJECT SITE
+  https://github.com/hesa/lookup-license
+
+REPORTING BUGS
+  File a ticket at https://github.com/hesa/lookup-license/issues
+
+COPYRIGHT
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  * https://github.com/aboutcode-org/scancode-toolkit - to identify license from license text
+  * https://github.com/hesa/foss-licenses - to identify license frorm "weird" names
+"""
