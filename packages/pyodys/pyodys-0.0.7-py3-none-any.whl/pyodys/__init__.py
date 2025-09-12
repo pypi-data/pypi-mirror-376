7065b04d7fb9c9ea64de52c1e91c5d2aac867c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:46 +0000

    Add package pyodys/0.0.7/pyodys-0.0.7-py3-none-any.whl

---
+++ packages/pyodys/pyodys-0.0.7-py3-none-any.whl/pyodys/__init__.py
@@ -0,0 +1,4 @@
+from .solveurs.runge_kutta.ButcherTableau import ButcherTableau
+from .solveurs.runge_kutta.RKSolverWithButcherTableau import RKSolverWithButcherTableau, PyOdysError
+from .systemes.ODEProblem import ODEProblem 
+from .utils.interpolation import *
