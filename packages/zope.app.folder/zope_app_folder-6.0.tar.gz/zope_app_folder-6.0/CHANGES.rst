9c5724daa938daeeb35a6d972b2023ec068d84c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:20 +0000

    Add package zope.app.folder/6.0/zope_app_folder-6.0.tar.gz

---
+++ packages/zope.app.folder/zope_app_folder-6.0.tar.gz/zope_app_folder-6.0/CHANGES.rst
@@ -0,0 +1,61 @@
+=======
+CHANGES
+=======
+
+6.0 (2025-09-12)
+----------------
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
+
+- Add support for Python 3.12, 3.13.
+
+- Drop support for Python 3.7, 3.8.
+
+
+5.0 (2023-02-07)
+----------------
+
+- Drop support for Python 2.7, 3.4, 3.5, 3.6.
+
+- Add support for Python 3.7, 3.8, 3.9, 3.10, 3.11.
+
+
+4.0.0 (2017-05-09)
+------------------
+
+- Add support for Python 3.4. 3.5, 3.6 and PyPy.
+
+
+3.5.2 (2010-12-28)
+------------------
+
+- Removed testing.py. There are no functional tests and ftesting.zcml now.
+
+- Removed unneeded dependencies.
+
+
+3.5.1 (2009-02-14)
+------------------
+
+- Added missing dependency to zope.app.content.
+
+- Fixed e-mail address and homepage.
+
+- Fixed buildout.cfg, as there is no test extra.
+
+
+3.5.0 (2009-01-31)
+------------------
+
+- Everything except zope.app.folder.browser moved to zope.site
+  and zope.container.
+
+- Import IPossibleSite from zope.location.interfaces
+  instead of deprecated place in zope.app.component.interfaces.
+
+- Use zope.container instead of zope.app.container.
+
+3.4.0 (2007-10-24)
+------------------
+
+- Initial release independent of the main Zope tree.
---
+++ packages/zope.app.folder/zope_app_folder-6.0.tar.gz/zope_app_folder-6.0/setup.py
@@ -0,0 +1,95 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.folder package
+
+"""
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+version = '6.0'
+
+tests_require = [
+    'zope.app.container',
+    'zope.browsermenu',
+    'zope.browserpage',
+    'zope.browserresource',
+    'zope.configuration',
+    'zope.testing',
+    'zope.testrunner >= 6.4',
+]
+
+setup(name='zope.app.folder',
+      version=version,
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description='Folder Content Type for Zope 3',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope3 folder site local component",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='http://github.com/zopefoundation/zope.app.folder',
+      project_urls={
+          'Issue Tracker': ('https://github.com/zopefoundation/'
+                            'zope.app.folder/issues'),
+          'Sources': 'https://github.com/zopefoundation/zope.app.folder',
+      },
+      license='ZPL-2.1',
+      python_requires='>=3.9',
+      install_requires=[
+          'setuptools',
+          'zope.container',
+          'zope.site',
+          'zope.app.content >= 4.0.0',
+      ],
+      extras_require={
+          'test': tests_require,
+          'zcml': [
+              'zope.app.container',
+              'zope.browsermenu',
+              'zope.browserpage',
+              'zope.browserresource',
+          ],
+      },
+      tests_require=tests_require,
+      include_package_data=True,
+      zip_safe=False,
+      )
