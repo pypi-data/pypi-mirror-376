d56f002b9494beb1c1d17e3a2c9ee11de8c06ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:58 +0000

    Add package autumn-py/3.0.2/autumn_py-3.0.2.tar.gz

---
+++ packages/autumn-py/autumn_py-3.0.2.tar.gz/autumn_py-3.0.2/autumn/asgi/routes/core.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from . import _extract, _build_response
+from ...models.meta import CustomerData
+
+if TYPE_CHECKING:
+    from starlette.requests import Request
+
+
+async def attach_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    customer_data = identify["customer_data"]
+
+    response = await autumn.attach(customer_id,
+                                   customer_data=CustomerData(**customer_data),
+                                   **json)
+    return _build_response(response)
+
+
+async def check_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    response = await autumn.check(customer_id, **json)
+    return _build_response(response)
+
+
+async def track_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    response = await autumn.track(customer_id, **json)
+    return _build_response(response)
+
+
+async def cancel_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    response = await autumn.cancel(customer_id, **json)
+    return _build_response(response)
+
+
+async def billing_portal_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    json.pop("open_in_new_tab", None)  # temp: fix
+    json.pop("openInNewTab", None)  # temp: fix
+    response = await autumn.customers.get_billing_portal(customer_id, **json)
+    return _build_response(response)
+
+
+async def checkout_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    json.pop("open_in_new_tab", None)  # temp: fix
+    json.pop("openInNewTab", None)  # temp: fix
+    response = await autumn.checkout(customer_id, **json)
+    return _build_response(response)
+
+
+async def query_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    json.pop("open_in_new_tab", None)  # temp: fix
+    response = await autumn.query(customer_id=customer_id, **json)
+    return _build_response(response)
