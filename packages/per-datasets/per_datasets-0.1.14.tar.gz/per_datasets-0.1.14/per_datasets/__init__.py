a87f0095b385f449a8b2f3529b32a3d576a6fe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:46 +0000

    Add package per-datasets/0.1.14/per_datasets-0.1.14.tar.gz

---
+++ packages/per-datasets/per_datasets-0.1.14.tar.gz/per_datasets-0.1.14/per_datasets/__init__.py
@@ -0,0 +1,10 @@
+"""
+PER Datasets - A module for loading reservoir datasets
+"""
+
+__version__ = "0.1.14"
+
+from .talkaholic.reservoir import Reservoir
+from .reservoir import load_random, load
+
+__all__ = ['load_random', 'load', 'Reservoir', '__version__']
