5a8bf88171f6a78048b72faa711c7112f8efc7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:29 +0000

    Add package quantum-scoring-sdk/0.3.4/quantum_scoring_sdk-0.3.4.tar.gz

---
+++ packages/quantum-scoring-sdk/quantum_scoring_sdk-0.3.4.tar.gz/quantum_scoring_sdk-0.3.4/setup.py
@@ -0,0 +1,33 @@
+﻿from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='quantum-scoring-sdk',
+    version='0.3.4',
+    author='Jaime Alexander Jimenez Lozano',
+    author_email='jaimeajl@hotmail.com',
+    description='Quantum-native framework with IBM Quantum hardware support and enterprise license control',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'qiskit>=0.45.0',
+        'qiskit-aer>=0.13.0',
+        'requests'
+    ],
+    extras_require={
+        'ibm': ['qiskit-ibm-runtime>=0.15.0']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    python_requires='>=3.8',
+)
