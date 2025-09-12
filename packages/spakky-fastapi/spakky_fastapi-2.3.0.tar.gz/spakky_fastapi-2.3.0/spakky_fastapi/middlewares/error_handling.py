f55c0a934438a14f44122e87a3207d593c4ca85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:29 +0000

    Add package spakky-fastapi/2.3.0/spakky_fastapi-2.3.0.tar.gz

---
+++ packages/spakky-fastapi/spakky_fastapi-2.3.0.tar.gz/spakky_fastapi-2.3.0/spakky_fastapi/middlewares/error_handling.py
@@ -0,0 +1,34 @@
+from traceback import print_exc
+from typing import Awaitable, Callable, TypeAlias
+
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+from spakky_fastapi.error import AbstractSpakkyFastAPIError, InternalServerError
+
+Next: TypeAlias = Callable[[Request], Awaitable[Response]]
+
+
+class ErrorHandlingMiddleware(BaseHTTPMiddleware):
+    __debug: bool
+
+    def __init__(
+        self,
+        app: ASGIApp,
+        dispatch: DispatchFunction | None = None,
+        debug: bool = False,
+    ) -> None:
+        super().__init__(app, dispatch)
+        self.__debug = debug
+
+    async def dispatch(self, request: Request, call_next: Next) -> Response:
+        try:
+            return await call_next(request)
+        except AbstractSpakkyFastAPIError as e:
+            return e.to_response()
+        except Exception:
+            if self.__debug:
+                print_exc()
+            return InternalServerError().to_response(self.__debug)
