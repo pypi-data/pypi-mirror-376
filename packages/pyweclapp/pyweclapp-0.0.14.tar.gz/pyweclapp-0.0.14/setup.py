cca37f39a61533d8392ed7168db4c86137f623b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:56 +0000

    Add package pyweclapp/0.0.14/pyweclapp-0.0.14.tar.gz

---
+++ packages/pyweclapp/pyweclapp-0.0.14.tar.gz/pyweclapp-0.0.14/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+try:
+    with open("src/README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "Provides methods, classes and classbuilders to integrate with the Weclapp API"
+
+try:
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+except FileNotFoundError:
+    required = ["requests>=2.28.2",
+                "pydantic>=1.10.4",
+                "jsonschema>=4.17.3",
+                "pytz>=2022.7.1"]
+
+setup(
+    name="pyweclapp",
+    version="0.0.14",
+    description="Provides methods, classes and classbuilders to integrate with the Weclapp API",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AltruanGmbH/pyweclapp",
+    author="Altruan GmbH",
+    author_email="admin@altruan.de",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    python_requires=">=3.9",
+)
