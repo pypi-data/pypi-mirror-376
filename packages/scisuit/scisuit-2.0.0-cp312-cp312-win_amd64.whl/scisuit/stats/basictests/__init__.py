0a10224ef7d7dea9df60ed5bd5418182228aba1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:58 +0000

    Add package scisuit/2.0.0/scisuit-2.0.0-cp312-cp312-win_amd64.whl

---
+++ packages/scisuit/scisuit-2.0.0-cp312-cp312-win_amd64.whl/scisuit/stats/basictests/__init__.py
@@ -0,0 +1,17 @@
+from ._test_f import test_f, test_f_Result
+
+from ._test_t import test_t, test_t1_result, test_t2_result, test_tpaired_result
+from ._test_z import test_z, test_z1_Result, test_z2_Result
+
+
+from ._test_normality import \
+	anderson, ADTestRes, \
+	ks_1samp, Ks1SampletestResult, \
+	shapiro, ShapiroTestResult
+
+from ._test_cor import cor_test, cortest_Result
+
+from ._poisson1sample import test_poisson1sample, test_poisson1sample_Result
+
+
+
