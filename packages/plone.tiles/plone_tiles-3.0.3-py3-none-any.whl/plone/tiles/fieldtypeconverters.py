8ca36cdabd0b00a721f5dbd45e011a6cb5afca38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:05:38 +0000

    Add package plone.tiles/3.0.3/plone_tiles-3.0.3-py3-none-any.whl

---
+++ packages/plone.tiles/plone_tiles-3.0.3-py3-none-any.whl/plone/tiles/fieldtypeconverters.py
@@ -0,0 +1,39 @@
+from plone.tiles.interfaces import IFieldTypeConverter
+from zope.interface import implementer
+
+
+@implementer(IFieldTypeConverter)
+class NoConverter:
+
+    def __init__(self, field):
+        self.field = field
+
+    token = None
+
+
+class TextConverter(NoConverter):
+    token = "text"
+
+
+class LongConverter(NoConverter):
+    token = "long"
+
+
+class FloatConverter(NoConverter):
+    token = "float"
+
+
+class BoolConverter(NoConverter):
+    token = "boolean"
+
+
+class TupleConverter(NoConverter):
+    token = "tuple"
+
+
+class ListConverter(NoConverter):
+    token = "list"
+
+
+class DictConverter(NoConverter):
+    token = "record"
