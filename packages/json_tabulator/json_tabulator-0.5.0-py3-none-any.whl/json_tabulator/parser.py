5d5aeda8c2717022057008c27d4a7076a46b8baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:06:01 +0000

    Add package json_tabulator/0.5.0/json_tabulator-0.5.0-py3-none-any.whl

---
+++ packages/json_tabulator/json_tabulator-0.5.0-py3-none-any.whl/json_tabulator/parser.py
@@ -0,0 +1,77 @@
+
+from .expression import Expression, STAR, INDEX, PATH
+
+from parsy import string, regex, eof, alt, seq, ParseError
+
+
+dot = string('.').then(eof.should_fail('expression to continue'))
+star = string('*').result(STAR)
+root = string('$').result([])
+forbidden = ''.join(['"', "'", '\\.', '\\$', '\\*', '\\[\\]', '\\(\\)'])
+lparen = string('(')
+rparen = string(')')
+lbracket = string('[')
+rbracket = string(']')
+
+
+def make_quoted_member(q: str):
+    def unquote(s: str) -> str: return s.replace('\\' + q, q)
+    return string(q) >> regex(f'(\\\\{q}|[^{q}])+').map(unquote) << string(q)
+
+
+unquoted_member = regex(f'[^{forbidden}0-9][^{forbidden}]*')
+quoted_member = (make_quoted_member('"') | make_quoted_member("'"))
+number = regex(r'\d+').map(int)
+func_index =  lparen >> string('index').result(INDEX) << rparen
+func_path = lparen >> string('path').result(PATH) << rparen
+function = alt(func_index, func_path)
+
+subscript = lbracket >> alt(number, quoted_member, star) << rbracket
+
+initial_segment = alt(
+    root,
+    unquoted_member,
+    quoted_member,
+    subscript,
+    star,
+)
+inner_segment = alt(
+    dot >> unquoted_member,
+    dot >> quoted_member,
+    dot >> star,
+    dot >> function,
+    dot.optional() >> subscript
+)
+
+
+def concat_list(*args):
+    res = []
+    for a in args:
+        if isinstance(a, list):
+            res += a
+        else:
+            res.append(a)
+    return res
+
+
+expression = seq(initial_segment, inner_segment.many()).combine(concat_list)
+
+
+class InvalidExpression(ValueError):
+    pass
+
+
+def parse_expression(string: str) -> Expression:
+    try:
+        res = Expression(expression.parse(string))
+    except ParseError:
+        raise InvalidExpression(string)
+
+    for i, part in enumerate(res):
+        if part in (INDEX, PATH):
+            if i == 0 or res[i - 1] != STAR:
+                raise InvalidExpression(string)
+            if i < len(res) - 1:
+                raise InvalidExpression(string)
+
+    return res
---
+++ packages/json_tabulator/json_tabulator-0.5.0-py3-none-any.whl/json_tabulator/query.py
@@ -0,0 +1,78 @@
+from typing import Any
+from dataclasses import dataclass
+from collections import defaultdict
+
+from .expression import Expression, STAR, INDEX, PATH, is_function
+
+
+def nested_get(data, keys) -> tuple[Any, bool]:
+    res = data
+    for k in keys:
+        if isinstance(res, dict):
+            if k not in res:
+                return None, False
+            res = res[k]
+        elif isinstance(res, list):
+            if not isinstance(k, int) or k >= len(res):
+                return None, False
+            res = res[k]
+    return res, True
+
+
+@dataclass
+class QueryPlan:
+    path: Expression
+    extracts: dict[Expression, dict[str, tuple]]
+
+    @classmethod
+    def from_dict(cls, query: dict[str, Expression]) -> 'QueryPlan':
+        steps = defaultdict(dict)
+        query_path = Expression()
+        for name, expr in query.items():
+            table = expr.get_table()
+
+            if not table.coincides_with(query_path):
+                raise ValueError(f'Illegal query: Paths {table} and {query_path} are not compatible.')
+
+            query_path = max(query_path, table, key=len)
+            tail = expr[len(table):]
+            if is_function(tail):
+                steps[table][name] = tail[0]
+            else:
+                steps[table][name] = tuple(tail)
+
+        return cls(path=query_path, extracts=steps)
+
+    def execute(self, data, omit_missing_attributes: bool):
+        def _extract(data, item, path):
+            if isinstance(item, tuple):
+                return nested_get(data, item)
+            elif item == INDEX:
+                return path[-1], True
+            elif item == PATH:
+                return Expression(path).to_string(), True
+
+        def _recurse(data, head, tail, path, extract):
+            if head in self.extracts:
+                extract = dict(extract)
+                for name, item in self.extracts[head].items():
+                    value, success = _extract(data, item, path)
+                    if success or not omit_missing_attributes:
+                        extract[name] = value
+            if tail:
+                current, tail = tail[0], tail[1:]
+                head = head + (current,)
+                if current is STAR and isinstance(data, list):
+                    for idx, item in enumerate(data):
+                        yield from _recurse(item, head, tail, path + (idx,), extract)
+                elif current is STAR and isinstance(data, dict):
+                    for idx, item in data.items():
+                        yield from _recurse(item, head, tail, path + (idx,), extract)
+                elif isinstance(current, str) and isinstance(data, dict):
+                    yield from _recurse(data.get(current), head, tail, path + (current,), extract)
+                elif isinstance(current, int) and isinstance(data, list) and current < len(data):
+                    yield from _recurse(data[current], head, tail, path + (current,), extract)
+            else:
+                yield extract
+
+        yield from _recurse(data, Expression(), self.path, (), {})
