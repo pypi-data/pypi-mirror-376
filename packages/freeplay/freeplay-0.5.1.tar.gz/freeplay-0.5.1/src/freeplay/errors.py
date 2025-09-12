cfa1864a2dfd82b7b132ec5b25be39d0488ee077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 07:06:50 +0000

    Add package freeplay/0.5.1/freeplay-0.5.1.tar.gz

---
+++ packages/freeplay/freeplay-0.5.1.tar.gz/freeplay-0.5.1/src/freeplay/errors.py
@@ -0,0 +1,43 @@
+from requests import Response
+import warnings
+
+
+class FreeplayError(Exception):
+    pass
+
+
+class FreeplayConfigurationError(FreeplayError):
+    pass
+
+
+class FreeplayClientError(FreeplayError):
+    pass
+
+
+class FreeplayServerError(FreeplayError):
+    pass
+
+
+class LLMClientError(FreeplayError):
+    pass
+
+
+class LLMServerError(FreeplayError):
+    pass
+
+
+def freeplay_response_error(message: str, response: Response) -> FreeplayError:
+    full_message = f'{message} [{response.status_code}]'
+
+    if response.status_code in range(400, 500):
+        return FreeplayClientError(full_message)
+    else:
+        return FreeplayServerError(full_message)
+
+
+class FreeplayClientWarning(UserWarning):
+    pass
+
+
+def log_freeplay_client_warning(message: str) -> None:
+    warnings.warn(message, FreeplayClientWarning)
