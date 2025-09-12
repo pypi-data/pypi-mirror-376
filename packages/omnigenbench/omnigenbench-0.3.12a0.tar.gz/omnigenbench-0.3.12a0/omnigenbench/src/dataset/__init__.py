7500aeee0c9a29ea7b9e1c9e68228f2f774bce30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 06:04:12 +0000

    Add package omnigenbench/0.3.12a0/omnigenbench-0.3.12a0.tar.gz

---
+++ packages/omnigenbench/omnigenbench-0.3.12a0.tar.gz/omnigenbench-0.3.12a0/omnigenbench/src/dataset/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# file: __init__.py
+# time: 22:33 08/04/2024
+# author: YANG, HENG <hy345@exeter.ac.uk> (杨恒)
+# github: https://github.com/yangheng95
+# huggingface: https://huggingface.co/yangheng
+# google scholar: https://scholar.google.com/citations?user=NPq5a_0AAAAJ&hl=en
+# Copyright (C) 2019-2024. All Rights Reserved.
+"""
+This package contains dataset-related modules.
+"""
+
+# Original torch-based datasets (for backward compatibility)
+from .omni_dataset import OmniDatasetForSequenceClassification
+from .omni_dataset import OmniDatasetForSequenceRegression
+from .omni_dataset import OmniDatasetForTokenClassification
+from .omni_dataset import OmniDatasetForTokenRegression
