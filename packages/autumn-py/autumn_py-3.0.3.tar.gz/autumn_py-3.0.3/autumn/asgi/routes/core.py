0e10eb6b2f780c38d702ff2abf25aa5a90907544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:05:49 +0000

    Add package autumn-py/3.0.3/autumn_py-3.0.3.tar.gz

---
+++ packages/autumn-py/autumn_py-3.0.3.tar.gz/autumn_py-3.0.3/autumn/asgi/routes/core.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ...utils import _build_payload as _build_kwargs
+from ...models.meta import CustomerData
+from . import _build_response, _extract
+
+if TYPE_CHECKING:
+    from starlette.requests import Request
+
+
+async def attach_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    customer_data = identify["customer_data"]
+
+    kwargs = _build_kwargs(json, autumn.attach)
+
+    response = await autumn.attach(
+        customer_id, customer_data=CustomerData(**customer_data), **kwargs
+    )
+    return _build_response(response)
+
+
+async def check_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(json, autumn.check)
+
+    response = await autumn.check(customer_id, **kwargs)
+    return _build_response(response)
+
+
+async def track_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(json, autumn.track)
+
+    response = await autumn.track(customer_id, **kwargs)
+    return _build_response(response)
+
+
+async def cancel_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(json, autumn.cancel)
+
+    response = await autumn.cancel(customer_id, **kwargs)
+    return _build_response(response)
+
+
+async def billing_portal_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(
+        json,
+        autumn.customers.get_billing_portal,
+        ignore={"open_in_new_tab", "openInNewTab"},
+    )
+    response = await autumn.customers.get_billing_portal(customer_id, **kwargs)
+    return _build_response(response)
+
+
+async def checkout_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(
+        json, autumn.checkout, ignore={"open_in_new_tab", "openInNewTab"}
+    )
+
+    response = await autumn.checkout(customer_id, **kwargs)
+    return _build_response(response)
+
+
+async def query_route(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    kwargs = _build_kwargs(
+        json, autumn.query, ignore={"open_in_new_tab", "openInNewTab"}
+    )
+
+    response = await autumn.query(customer_id=customer_id, **kwargs)
+    return _build_response(response)
