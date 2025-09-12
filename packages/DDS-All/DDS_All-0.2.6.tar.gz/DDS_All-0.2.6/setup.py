1b1e20057aee4b86443a102e9d8540108ccf0733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:21 +0000

    Add package DDS-All/0.2.6/DDS_All-0.2.6.tar.gz

---
+++ packages/DDS-All/DDS_All-0.2.6.tar.gz/DDS_All-0.2.6/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+    def has_ext_modules(self):
+        return True
+
+setup(
+    name="DDS_All",
+    version="0.2.6",  # 确保版本号正确
+    description="添加不同版本Python编译出的.pyd文件",
+    author="JackyJia",
+    packages=find_packages(),
+    package_data={
+        'DDS_All': [
+            'bin/cp39-win_amd64/*.pyd',
+            'bin/cp310-win_amd64/*.pyd',
+            'bin/cp311-win_amd64/*.pyd',
+            'bin/cp312-win_amd64/*.pyd',
+            'bin/cp313-win_amd64/*.pyd',
+            'libs/*.dll',
+            'libs/*.lib',
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.9, <=3.13",
+    distclass=BinaryDistribution,
+    options={
+        'bdist_wheel': {
+            'plat_name': 'win_amd64',
+        }
+    },
+)
