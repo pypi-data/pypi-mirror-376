5de1f9fc15c19cf79330662eeb41a1d0b25d5a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 08:04:49 +0000

    Add package nanodjango/0.12.2/nanodjango-0.12.2-py3-none-any.whl

---
+++ packages/nanodjango/nanodjango-0.12.2-py3-none-any.whl/nanodjango/__init__.py
@@ -0,0 +1,10 @@
+import pluggy
+
+from .app import Django
+from .defer import defer
+
+# Register pluggy hook
+hookimpl = pluggy.HookimplMarker("nanodjango")
+
+__version__ = "0.12.2"
+__all__ = ["Django", "defer", "hookimpl"]
