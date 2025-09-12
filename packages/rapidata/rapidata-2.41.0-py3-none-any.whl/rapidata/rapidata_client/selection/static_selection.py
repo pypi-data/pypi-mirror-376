b34e92e32f6748b49faba4e2882fac254d7941df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 17:05:10 +0000

    Add package rapidata/2.41.0/rapidata-2.41.0-py3-none-any.whl

---
+++ packages/rapidata/rapidata-2.41.0-py3-none-any.whl/rapidata/rapidata_client/selection/static_selection.py
@@ -0,0 +1,17 @@
+from rapidata.api_client.models.static_selection import (
+    StaticSelection as StaticSelectionModel,
+)
+from rapidata.rapidata_client.selection._base_selection import RapidataSelection
+
+
+class StaticSelection(RapidataSelection):
+    """StaticSelection Class
+
+    Given a list of RapidIds, theses specific rapids will be shown in order for every session.
+
+    Args:
+        rapid_ids (list[str]): List of rapid ids to show.
+    """
+
+    def __init__(self, rapid_ids: list[str]):
+        self.rapid_ids = rapid_ids
