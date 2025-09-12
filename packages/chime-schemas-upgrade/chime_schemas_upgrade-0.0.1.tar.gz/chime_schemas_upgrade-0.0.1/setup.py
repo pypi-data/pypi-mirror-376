e3c8e3be8dc62d6ef097d57ef2b429e55e85a511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:06:07 +0000

    Add package chime-schemas-upgrade/0.0.1/chime_schemas_upgrade-0.0.1.tar.gz

---
+++ packages/chime-schemas-upgrade/chime_schemas_upgrade-0.0.1.tar.gz/chime_schemas_upgrade-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-schemas-upgrade',
+    packages=find_packages(include=['chime-schemas-upgrade']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
