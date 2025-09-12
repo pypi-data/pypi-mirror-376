7ed3a4df3089f7e6e39b209356c4d97fc1705126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 14:05:24 +0000

    Add package per-datasets/0.1.1/per_datasets-0.1.1.tar.gz

---
+++ packages/per-datasets/per_datasets-0.1.1.tar.gz/per_datasets-0.1.1/per_datasets/__init__.py
@@ -0,0 +1,9 @@
+"""
+PER Datasets - A module for loading reservoir datasets
+"""
+
+__version__ = "0.1.1"
+
+from .reservoir import initialize, load_random, load_all, load_dataset_by_id, get_dataset_info, load_random_reservoir
+
+__all__ = ['initialize', 'load_random', 'load_all', 'load_dataset_by_id', 'get_dataset_info', 'load_random_reservoir', '__version__']
