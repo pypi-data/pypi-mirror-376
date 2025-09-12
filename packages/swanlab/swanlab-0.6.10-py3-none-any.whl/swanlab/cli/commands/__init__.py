fbfb85e7194954fdb2821920f47b63dcbb31866e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:51 +0000

    Add package swanlab/0.6.10/swanlab-0.6.10-py3-none-any.whl

---
+++ packages/swanlab/swanlab-0.6.10-py3-none-any.whl/swanlab/cli/commands/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024/6/12 21:17
+@File: __init__.py
+@IDE: pycharm
+@Description:
+    暴露子命令
+"""
+from .auth import login, logout
+from .converter import convert
+from .dashboard import watch
+from .sync import sync
