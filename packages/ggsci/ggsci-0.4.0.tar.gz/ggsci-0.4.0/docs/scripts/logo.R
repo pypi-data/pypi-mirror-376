2b6b3a09695e6a4a21d4371f5161461c875f0584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 15:05:35 +0000

    Add package ggsci/0.4.0/ggsci-0.4.0.tar.gz

---
+++ packages/ggsci/ggsci-0.4.0.tar.gz/ggsci-0.4.0/docs/scripts/logo.R
@@ -0,0 +1,17 @@
+hexSticker::sticker(
+  subplot = ~ plot.new(),
+  s_x = 1,
+  s_y = 1,
+  s_width = 0.1,
+  s_height = 0.1,
+  package = "",
+  p_x = 1,
+  p_y = 1,
+  p_size = 26,
+  h_size = 1.2,
+  p_color = "#F06060",
+  h_fill = "#FFF9F2",
+  h_color = "#F06060",
+  dpi = 320,
+  filename = "docs/assets/logo.png"
+)
