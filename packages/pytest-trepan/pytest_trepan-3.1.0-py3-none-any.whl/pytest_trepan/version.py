5e47e012ffb5270b28ad880d2ba4eb8562c92c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:03:55 +0000

    Add package pytest-trepan/3.1.0/pytest_trepan-3.1.0-py3-none-any.whl

---
+++ packages/pytest-trepan/pytest_trepan-3.1.0-py3-none-any.whl/pytest_trepan/version.py
@@ -0,0 +1,7 @@
+# This file is needs to be multi-lingual in both Python and POSIX
+# shell which "execfile" or "source" it respectively.
+
+# This file should define a variable __version__ which we use as the
+# debugger version number.
+
+__version__="3.1.0"  # NOQA
