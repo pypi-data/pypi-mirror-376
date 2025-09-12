0a7bef4fffdd96cc7a8a4a94024125609f1942cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 05:04:02 +0000

    Add package hamtaa-texttools/1.0.2/hamtaa_texttools-1.0.2.tar.gz

---
+++ packages/hamtaa-texttools/hamtaa_texttools-1.0.2.tar.gz/hamtaa_texttools-1.0.2/texttools/__init__.py
@@ -0,0 +1,9 @@
+from .tools.the_tool import TheTool
+from .utils.batch_manager.batch_manager import SimpleBatchManager
+from .utils.batch_manager.batch_runner import BatchJobRunner
+
+__all__ = [
+    "TheTool",
+    "SimpleBatchManager",
+    "BatchJobRunner",
+]
---
+++ packages/hamtaa-texttools/hamtaa_texttools-1.0.2.tar.gz/hamtaa_texttools-1.0.2/texttools/utils/__init__.py
@@ -0,0 +1,4 @@
+from .batch_manager.batch_manager import SimpleBatchManager
+from .batch_manager.batch_runner import BatchJobRunner
+
+__all__ = ["SimpleBatchManager", "BatchJobRunner"]
