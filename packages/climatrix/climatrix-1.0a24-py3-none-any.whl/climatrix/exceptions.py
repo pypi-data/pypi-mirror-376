e1bd3e0f82efda6c528bed981b2ea84690c5d5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:22 +0000

    Add package climatrix/1.0a24/climatrix-1.0a24-py3-none-any.whl

---
+++ packages/climatrix/climatrix-1.0a24-py3-none-any.whl/climatrix/exceptions.py
@@ -0,0 +1,32 @@
+class DatasetCreationError(ValueError):
+    pass
+
+
+class TimeRelatedDatasetNotSupportedError(DatasetCreationError):
+    pass
+
+
+class LongitudeConventionMismatch(ValueError):
+    pass
+
+
+class MissingAxisError(KeyError):
+    pass
+
+
+class SubsettingByNonDimensionAxisError(ValueError):
+    pass
+
+
+class AxisMatchingError(ValueError):
+    pass
+
+
+class DomainMismatchError(ValueError):
+    pass
+
+
+class MissingDependencyError(ImportError):
+    """Raised when required plotting dependencies are not installed."""
+
+    pass
