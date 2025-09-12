4276641498ab02745dc6a749549c99460667c4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:26 +0000

    Add package zope.configuration/7.0/zope_configuration-7.0.tar.gz

---
+++ packages/zope.configuration/zope_configuration-7.0.tar.gz/zope_configuration-7.0/setup.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# https://zopetoolkit.readthedocs.io/
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(name='zope.configuration',
+      version='7.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description='Zope Configuration Markup Language (ZCML)',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope configuration zcml",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='https://github.com/zopefoundation/zope.configuration',
+      license='ZPL-2.1',
+      python_requires='>=3.9',
+      extras_require={
+          'docs': ['Sphinx', 'repoze.sphinx.autointerface'],
+          'test': ['zope.testing', 'zope.testrunner >= 6.4']
+      },
+      install_requires=[
+          'setuptools',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema >= 4.9',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
