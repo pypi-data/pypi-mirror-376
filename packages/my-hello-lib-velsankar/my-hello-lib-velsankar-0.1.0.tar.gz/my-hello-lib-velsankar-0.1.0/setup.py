77b781cdc331385d18fdf45f050949421046787b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 16:05:23 +0000

    Add package my-hello-lib-velsankar/0.1.0/my-hello-lib-velsankar-0.1.0.tar.gz

---
+++ packages/my-hello-lib-velsankar/my-hello-lib-velsankar-0.1.0.tar.gz/my-hello-lib-velsankar-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-hello-lib-velsankar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple hello world logging library",
+    author="Your Name",
+    python_requires=">=3.7",
+)
