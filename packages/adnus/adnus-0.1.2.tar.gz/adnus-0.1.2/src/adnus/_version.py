29b6f9a2012e9d9f84df85e443a447080a224697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-12 03:11:29 +0000

    Add package adnus/0.1.2/adnus-0.1.2.tar.gz

---
+++ packages/adnus/adnus-0.1.2.tar.gz/adnus-0.1.2/src/adnus/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "0.1.2"
+__license__ = "MIT"
+__description__ = "adnus (AdNuS):  Advanced Number Systems."
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/adnus"
+__docs__ = "https://WhiteSymmetry.github.io/adnus"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.9"]  # Diğer bağımlılıkları da ekleyebilirsiniz
