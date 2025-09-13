3c4c0ffaef4f614de00829cef53f75850ecfe38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 07:05:25 +0000

    Add package nixbot/170/nixbot-170.tar.gz

---
+++ packages/nixbot/nixbot-170.tar.gz/nixbot-170/nixbot/modules/tbl.py
@@ -0,0 +1,58 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
+
+MD5 = {
+    "flt": "396ebd6bf47b01f49eb101cb71828135",
+    "fnd": "c409961ceb48b1f88b97f88f0343c02c",
+    "irc": "e64a89cf7b14d4a4fa0b5351459037c5",
+    "log": "9b9163ecb14458983d771ed6000e4176",
+    "lst": "433ee5986ed30c7b9bf50de0c628a859",
+    "mbx": "0248126270d35f63abccc7849bef229b",
+    "mdl": "414951722594f27d34f02b02d181bee6",
+    "req": "793704d64443c4f19512924e294943ea",
+    "rss": "9c0a35a1c64aa2de86dd8cebf7d09631",
+    "rst": "20c19bb5096a5f8a90adb0e36daa7df5",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "b23521ded991053d3675b8cb9fe08f79",
+    "thr": "e5c3d225b8ba29b1a54fe364c0d8e743",
+    "tmr": "edf8a1f52a7fe19a8880b4e7f4bbe630",
+    "udp": "d9be502dfd01013be510e373441193b5",
+    "upt": "75b44c8e16985ea18beeb9509fada740",
+    "web": "c6e280690a385635774d3601aade99ee",
+    "wsd": "8d7d9f833b345ebf3b0046b86168297c",
+}
