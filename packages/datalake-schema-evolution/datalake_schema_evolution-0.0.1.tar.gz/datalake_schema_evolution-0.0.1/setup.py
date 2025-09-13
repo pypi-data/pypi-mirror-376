1133fbd54de57161207f7b5f67a257041f96cc44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:05:43 +0000

    Add package datalake-schema-evolution/0.0.1/datalake_schema_evolution-0.0.1.tar.gz

---
+++ packages/datalake-schema-evolution/datalake_schema_evolution-0.0.1.tar.gz/datalake_schema_evolution-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='datalake-schema-evolution',
+    packages=find_packages(include=['datalake-schema-evolution']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
