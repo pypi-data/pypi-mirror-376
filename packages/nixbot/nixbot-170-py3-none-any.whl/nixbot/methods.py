87e9eed4a1ae0353b6388ccac2a861b31ef57a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 07:05:25 +0000

    Add package nixbot/170/nixbot-170-py3-none-any.whl

---
+++ packages/nixbot/nixbot-170-py3-none-any.whl/nixbot/methods.py
@@ -0,0 +1,263 @@
+# This file is placed in the Public Domain.
+
+
+"methods"
+
+
+import hashlib
+import os
+import time
+
+
+from .objects import items, keys
+
+
+j = os.path.join
+
+
+def edit(obj, setter, skip=True):
+    for key, val in items(setter):
+        if skip and val == "":
+            continue
+        try:
+            setattr(obj, key, int(val))
+            continue
+        except ValueError:
+            pass
+        try:
+            setattr(obj, key, float(val))
+            continue
+        except ValueError:
+            pass
+        if val in ["True", "true"]:
+            setattr(obj, key, True)
+        elif val in ["False", "false"]:
+            setattr(obj, key, False)
+        else:
+            setattr(obj, key, val)
+
+
+def fmt(obj, args=None, skip=None, plain=False, empty=False, newline=False):
+    if args is None:
+        args = keys(obj)
+    if skip is None:
+        skip = []
+    txt = ""
+    for key in args:
+        if key.startswith("__"):
+            continue
+        if key in skip:
+            continue
+        value = getattr(obj, key, None)
+        if value is None:
+            continue
+        if not empty and not value:
+            continue
+        if plain:
+            txt += f"{value} "
+        elif isinstance(value, str):
+            txt += f'{key}="{value}" '
+        else:
+            txt += f"{key}={value} "
+        if newline:
+            txt += "\n"
+    return txt.strip()
+
+
+def fqn(obj):
+    kin = str(type(obj)).split()[-1][1:-2]
+    if kin == "type":
+        kin = f"{obj.__module__}.{obj.__name__}"
+    return kin
+
+
+def name(obj):
+    typ = type(obj)
+    if "__builtins__" in dir(typ):
+        return obj.__name__
+    if "__self__" in dir(obj):
+        return f"{obj.__self__.__class__.__name__}.{obj.__name__}"
+    if "__class__" in dir(obj) and "__name__" in dir(obj):
+        return f"{obj.__class__.__name__}.{obj.__name__}"
+    if "__class__" in dir(obj):
+        return f"{obj.__class__.__module__}.{obj.__class__.__name__}"
+    if "__name__" in dir(obj):
+        return f"{obj.__class__.__name__}.{obj.__name__}"
+    return ""
+
+
+def parse(obj, txt=None):
+    if txt is None:
+        if "txt" in dir(obj):
+            txt = obj.txt
+        else:
+            txt = ""
+    args = []
+    obj.args   = getattr(obj, "args", [])
+    obj.cmd    = getattr(obj, "cmd", "")
+    obj.gets   = getattr(obj, "gets", "")
+    obj.index  = getattr(obj, "index", None)
+    obj.inits  = getattr(obj, "inits", "")
+    obj.mod    = getattr(obj, "mod", "")
+    obj.opts   = getattr(obj, "opts", "")
+    obj.result = getattr(obj, "result", "")
+    obj.sets   = getattr(obj, "sets", {})
+    obj.silent = getattr(obj, "silent", "")
+    obj.txt    = txt or getattr(obj, "txt", "")
+    obj.otxt   = obj.txt or getattr(obj, "otxt", "")
+    _nr = -1
+    for spli in obj.otxt.split():
+        if spli.startswith("-"):
+            try:
+                obj.index = int(spli[1:])
+            except ValueError:
+                obj.opts += spli[1:]
+            continue
+        if "-=" in spli:
+            key, value = spli.split("-=", maxsplit=1)
+            obj.silent[key] = value
+            obj.gets[key] = value
+            continue
+        if "==" in spli:
+            key, value = spli.split("==", maxsplit=1)
+            obj.gets[key] = value
+            continue
+        if "=" in spli:
+            key, value = spli.split("=", maxsplit=1)
+            if key == "mod":
+                if obj.mod:
+                    obj.mod += f",{value}"
+                else:
+                    obj.mod = value
+                continue
+            obj.sets[key] = value
+            continue
+        _nr += 1
+        if _nr == 0:
+            obj.cmd = spli
+            continue
+        args.append(spli)
+    if args:
+        obj.args = args
+        obj.txt  = obj.cmd or ""
+        obj.rest = " ".join(obj.args)
+        obj.txt  = obj.cmd + " " + obj.rest
+    else:
+        obj.txt = obj.cmd or ""
+
+
+def search(obj, selector, matching=False):
+    res = False
+    if not selector:
+        return res
+    for key, value in items(selector):
+        val = getattr(obj, key, None)
+        if not val:
+            continue
+        if matching and value == val:
+            res = True
+        elif str(value).lower() in str(val).lower() or value == "match":
+            res = True
+        else:
+            res = False
+            break
+    return res
+
+
+"utilities"
+
+
+def elapsed(seconds, short=True):
+    txt = ""
+    nsec = float(seconds)
+    if nsec < 1:
+        return f"{nsec:.2f}s"
+    yea = 365 * 24 * 60 * 60
+    week = 7 * 24 * 60 * 60
+    nday = 24 * 60 * 60
+    hour = 60 * 60
+    minute = 60
+    yeas = int(nsec / yea)
+    nsec -= yeas * yea
+    weeks = int(nsec / week)
+    nsec -= weeks * week
+    nrdays = int(nsec / nday)
+    nsec -= nrdays * nday
+    hours = int(nsec / hour)
+    nsec -= hours * hour
+    minutes = int(nsec / minute)
+    nsec -= int(minute * minutes)
+    sec = int(nsec)
+    if yeas:
+        txt += f"{yeas}y"
+    if weeks:
+        nrdays += weeks * 7
+    if nrdays:
+        txt += f"{nrdays}d"
+    if short and txt:
+        return txt.strip()
+    if hours:
+        txt += f"{hours}h"
+    if minutes:
+        txt += f"{minutes}m"
+    if sec:
+        txt += f"{sec}s"
+    txt = txt.strip()
+    return txt
+
+
+def extract_date(daystr):
+    daystr = daystr.encode('utf-8', 'replace').decode("utf-8")
+    res = time.time()
+    for fmt in FORMATS:
+        try:
+            res = time.mktime(time.strptime(daystr, fmt))
+            break
+        except ValueError:
+            pass
+    return res
+
+
+def md5sum(path):
+    with open(path, "r", encoding="utf-8") as file:
+        txt = file.read().encode("utf-8")
+        return hashlib.md5(txt).hexdigest()
+
+
+def spl(txt):
+    try:
+        result = txt.split(",")
+    except (TypeError, ValueError):
+        result = [
+            txt,
+        ]
+    return [x for x in result if x]
+
+
+FORMATS = [
+    "%Y-%M-%D %H:%M:%S",
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%d",
+    "%d-%m-%Y",
+    "%d-%m",
+    "%m-%d",
+]
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'edit',
+        'elapsed',
+        'extract_date',
+        'fmt',
+        'fqn',
+        'j',
+        'md5sum',
+        'name',
+        'parse',
+        'search',
+        'spl'
+    )
---
+++ packages/nixbot/nixbot-170-py3-none-any.whl/nixbot/runtime.py
@@ -0,0 +1,149 @@
+# This file is placed in the Public Domain.
+
+
+"runtime"
+
+
+import logging
+import os
+import queue
+import threading
+import time
+import _thread
+
+
+from .methods import name
+
+
+LEVELS = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'warn': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+}
+
+
+class Thread(threading.Thread):
+
+    def __init__(self, func, *args, daemon=True, **kwargs):
+        super().__init__(None, self.run, None, (), daemon=daemon)
+        self.name = kwargs.get("name", name(func))
+        self.queue = queue.Queue()
+        self.result = None
+        self.starttime = time.time()
+        self.stopped = threading.Event()
+        self.queue.put((func, args))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        yield from dir(self)
+
+    def run(self):
+        func, args = self.queue.get()
+        try:
+            self.result = func(*args)
+        except (KeyboardInterrupt, EOFError):
+            _thread.interrupt_main()
+        except Exception as ex:
+            logging.exception(ex)
+            _thread.interrupt_main()
+
+    def join(self, timeout=None):
+        try:
+            super().join(timeout)
+            return self.result
+        except (KeyboardInterrupt, EOFError):
+            _thread.interrupt_main()
+
+
+"timer/repeater"
+
+
+class Timy(threading.Timer):
+
+    def __init__(self, sleep, func, *args, **kwargs):
+        super().__init__(sleep, func)
+        self.name = kwargs.get("name", name(func))
+        self.sleep = sleep
+        self.state = {}
+        self.state["latest"] = time.time()
+        self.state["starttime"] = time.time()
+        self.starttime = time.time()
+
+
+class Timed:
+
+    def __init__(self, sleep, func, *args, thrname="", **kwargs):
+        self.args = args
+        self.func = func
+        self.kwargs = kwargs
+        self.sleep = sleep
+        self.name = thrname or kwargs.get("name", name(func))
+        self.target = time.time() + self.sleep
+        self.timer = None
+
+    def run(self):
+        self.timer.latest = time.time()
+        self.func(*self.args)
+
+    def start(self):
+        self.kwargs["name"] = self.name
+        timer = Timy(self.sleep, self.run, *self.args, **self.kwargs)
+        timer.start()
+        self.timer = timer
+
+    def stop(self):
+        if self.timer:
+            self.timer.cancel()
+
+
+class Repeater(Timed):
+
+    def run(self):
+        launch(self.start)
+        super().run()
+
+
+"utility"
+
+
+def launch(func, *args, **kwargs):
+    thread = Thread(func, *args, **kwargs)
+    thread.start()
+    return thread
+
+
+def level(loglevel="debug"):
+    if loglevel != "none":
+        format_short = "%(asctime)-8s %(message)-80s"
+        datefmt = "%H:%M:%S"
+        logging.basicConfig(datefmt=datefmt, format=format_short, force=True)
+        logging.getLogger().setLevel(LEVELS.get(loglevel))
+
+
+def rlog(loglevel, txt, ignore=None):
+    if ignore is None:
+        ignore = []
+    for ign in ignore:
+        if ign in str(txt):
+            return
+    logging.log(LEVELS.get(loglevel), txt)
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'Pool',
+        'Repeater',
+        'Thread',
+        'Timed',
+        'launch',
+        'level',
+        'rlog'
+   )
