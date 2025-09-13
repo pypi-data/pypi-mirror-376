f426fe67198a8304cee7f212a984e30a0ec527f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:43 +0000

    Add package odoo13-addon-connector-elasticsearch/13.0.2.5.1.dev5/odoo13_addon_connector_elasticsearch-13.0.2.5.1.dev5-py3-none-any.whl

---
+++ packages/odoo13-addon-connector-elasticsearch/odoo13_addon_connector_elasticsearch-13.0.2.5.1.dev5-py3-none-any.whl/odoo/addons/connector_elasticsearch/models/se_backend_elasticsearch.py
@@ -0,0 +1,51 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SeBackendElasticsearch(models.Model):
+
+    _name = "se.backend.elasticsearch"
+    _inherit = [
+        "se.backend.spec.abstract",
+        "server.env.techname.mixin",
+        "server.env.mixin",
+    ]
+    _description = "Elasticsearch Backend"
+
+    _search_engine_name = "elasticsearch"
+    _record_id_key = "objectID"
+
+    es_server_host = fields.Char(string="ElasticSearch host")
+    # `tech_name` should come from `server.env.techname.mixin`
+    # but `se.backend.spec.abstract` adds delegation inheritance
+    # with `se.backend` which inherits as well from `server.env.techname.mixin`
+    # hence the field is not created in this specific model table
+    tech_name = fields.Char(
+        related="se_backend_id.tech_name", store=True, readonly=False
+    )
+    api_key_id = fields.Char(help="Elasticsearch Api Key ID", string="Api Key ID")
+    api_key = fields.Char(help="Elasticsearch Api Key")
+    es_timeout = fields.Integer(
+        string="Elasticsearch timeout",
+        default=10,
+        help="Elastic Search request timeout",
+    )
+    es_max_retries = fields.Integer(
+        string="Elasticsearch max retries",
+        default=0,
+        help="Number of retries, when an occurs. "
+        "0 or negative means no retries and the exception is raised.",
+    )
+    es_retry_on_timeout = fields.Boolean(
+        string="Elasticsearch retry on timeout",
+        help="If set, retry when a connection timeout occurs. "
+        "Otherwise, the retries are only on other errors",
+    )
+
+    @property
+    def _server_env_fields(self):
+        env_fields = super()._server_env_fields
+        env_fields.update({"es_server_host": {}})
+        return env_fields
