883d26226707c2c8d964eb2050f5be0029a22cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:37 +0000

    Add package NepTrainKit/2.6.2/neptrainkit-2.6.2-cp310-cp310-win_amd64.whl

---
+++ packages/NepTrainKit/neptrainkit-2.6.2-cp310-cp310-win_amd64.whl/NepTrainKit/core/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/10/17 15:42
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+from .message import MessageManager
+from .structure import Structure,process_organic_clusters,get_clusters
+
+from .card_manager import CardManager,load_cards_from_directory
