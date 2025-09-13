cdb7564659ebb2bafc439f1539490cd7569c61f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:33 +0000

    Add package stx-pysdk/1.0.15/stx_pysdk-1.0.15-py3-none-any.whl

---
+++ packages/stx-pysdk/stx_pysdk-1.0.15-py3-none-any.whl/stxsdk/utils.py
@@ -0,0 +1,34 @@
+import requests
+from typing import List, Optional
+from stxsdk.typings import DataType, FailureResponse, SuccessResponse
+from stxsdk.config.configs import Configs
+
+
+def format_failure_response(
+    errors: List[str], message: Optional[str] = None
+) -> FailureResponse:
+    return FailureResponse(
+        success=False, data=None, errors=errors, message=message or "Request Failed"
+    )
+
+
+def format_success_response(
+    data: DataType, message: Optional[str] = None
+) -> SuccessResponse:
+    return SuccessResponse(
+        success=True,
+        data=data,
+        errors=None,
+        message=message or "Request Processed Successfully",
+    )
+
+def get_latest_graphql_version(env: str):
+
+    response = requests.get(f"{Configs.GRAPHQL_VERSION_URL.format(env=env)}")
+    if response.ok:
+        version = response.json().get("api_version", "")
+        if version:
+            version = "v"+version
+    else:
+        version = Configs.GRAPHQL_VERSION
+    return version
