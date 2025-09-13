fe2d99f562b617c83e77e6b6a15548d233ed810a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:06:27 +0000

    Add package inspect-ai/0.3.132/inspect_ai-0.3.132-py3-none-any.whl

---
+++ packages/inspect-ai/inspect_ai-0.3.132-py3-none-any.whl/inspect_ai/hooks/__init__.py
@@ -0,0 +1,29 @@
+from inspect_ai.hooks._hooks import (
+    ApiKeyOverride,
+    EvalSetEnd,
+    EvalSetStart,
+    Hooks,
+    ModelUsageData,
+    RunEnd,
+    RunStart,
+    SampleEnd,
+    SampleStart,
+    TaskEnd,
+    TaskStart,
+    hooks,
+)
+
+__all__ = [
+    "ApiKeyOverride",
+    "Hooks",
+    "ModelUsageData",
+    "EvalSetStart",
+    "EvalSetEnd",
+    "RunEnd",
+    "RunStart",
+    "SampleEnd",
+    "SampleStart",
+    "TaskEnd",
+    "TaskStart",
+    "hooks",
+]
