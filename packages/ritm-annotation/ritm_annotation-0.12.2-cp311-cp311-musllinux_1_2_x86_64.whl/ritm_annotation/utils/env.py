11566695205605a3e3d99230d5ae20cee58b9e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:52 +0000

    Add package ritm-annotation/0.12.2/ritm_annotation-0.12.2-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/ritm-annotation/ritm_annotation-0.12.2-cp311-cp311-musllinux_1_2_x86_64.whl/ritm_annotation/utils/env.py
@@ -0,0 +1,26 @@
+import logging
+from gettext import gettext as _
+from typing import Dict
+
+from easydict import EasyDict as edict
+
+logger = logging.getLogger(__name__)
+
+
+def load_cfg_from_env(cfg: edict, env: Dict[str, str]):
+    for k, v in env.items():
+        if k.startswith("RITM_"):
+            cfgkey = k.replace("RITM_", "").replace("__", ".")
+            logger.warning(
+                _(
+                    "Changing configuration entry from environment variable: {k}={v}"
+                ).format(k=cfgkey, v=v)  # noqa:E501
+            )  # noqa: E501
+            *parts, last = cfgkey.split(".")
+            this_cfg = cfg
+            for part in parts:
+                if this_cfg.get(part) is None:
+                    this_cfg[part] = edict()
+                this_cfg = this_cfg[part]
+            this_cfg[last] = v
+    return cfg
