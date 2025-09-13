0dcdda51095dcc820e694c75f5b6997d77591d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:41 +0000

    Add package byzh-ai/0.0.9.1/byzh_ai-0.0.9.1.tar.gz

---
+++ packages/byzh-ai/byzh_ai-0.0.9.1.tar.gz/byzh_ai-0.0.9.1/byzh/ai/Butils/__init__.py
@@ -0,0 +1,11 @@
+from .get_device import b_get_device
+from .get_params import b_get_params
+from .get_flops import b_get_flops
+from .get_gpu import b_get_gpu_nvidia
+from .load_model import b_load_model
+
+all = [
+    'get_device', 'get_params', 'get_flops',
+    'get_gpu_nvidia',
+    'load_model'
+]
