71039926f3fa050692693b9d0333f4b3fda4abfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:08 +0000

    Add package fracbm/0.1.2/fracbm-0.1.2.tar.gz

---
+++ packages/fracbm/fracbm-0.1.2.tar.gz/fracbm-0.1.2/fracbm/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fractional Brownian Motion (fBM) generators.
+"""
+
+from .generate import cholesky, daviesharte
+from .covariance import fBMcov, toeplitz
+
+__all__ = ["cholesky", "daviesharte", "fBMcov", "toeplitz"]
+
+__version__ = "0.1.2"
