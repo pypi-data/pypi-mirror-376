95800cd8182e8f7959a3e7460b14e579481c2068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 14:05:45 +0000

    Add package svc-infra/0.1.246/svc_infra-0.1.246.tar.gz

---
+++ packages/svc-infra/svc_infra-0.1.246.tar.gz/svc_infra-0.1.246/src/svc_infra/api/fastapi/db/nosql/mongo/health.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from fastapi import APIRouter
+
+from svc_infra.db.nosql.mongo.client import ping_mongo
+
+
+def make_mongo_health_router(
+    *, prefix: str = "/_mongo/health", include_in_schema: bool = False
+) -> APIRouter:
+    router = APIRouter(prefix=prefix, include_in_schema=include_in_schema)
+
+    @router.get("")
+    async def healthcheck():
+        ok = await ping_mongo()
+        return {"ok": ok}
+
+    return router
