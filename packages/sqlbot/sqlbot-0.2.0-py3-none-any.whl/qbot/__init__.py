59c2e2a2b5968c9276d1fa83388ab3571d40cd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 14:07:10 +0000

    Add package sqlbot/0.2.0/sqlbot-0.2.0-py3-none-any.whl

---
+++ packages/sqlbot/sqlbot-0.2.0-py3-none-any.whl/qbot/__init__.py
@@ -0,0 +1,9 @@
+"""SQLBot: Database Query Bot with AI-powered natural language processing."""
+
+__version__ = "0.2.0"
+__author__ = "SQLBot Team"
+__email__ = "team@qbot.dev"
+
+from .repl import main
+
+__all__ = ["main"]
---
+++ packages/sqlbot/sqlbot-0.2.0-py3-none-any.whl/qbot/__version__.py
@@ -0,0 +1,4 @@
+"""Version information for QBot."""
+__version__ = "0.2.0"
+
+
