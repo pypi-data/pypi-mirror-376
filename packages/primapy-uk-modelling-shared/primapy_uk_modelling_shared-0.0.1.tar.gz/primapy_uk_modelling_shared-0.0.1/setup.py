caa01fe337a8c2990cfe2ae447b1ce632cdc0eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:05:50 +0000

    Add package primapy-uk-modelling-shared/0.0.1/primapy_uk_modelling_shared-0.0.1.tar.gz

---
+++ packages/primapy-uk-modelling-shared/primapy_uk_modelling_shared-0.0.1.tar.gz/primapy_uk_modelling_shared-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="primapy-uk-modelling-shared",
+    version="0.0.1",
+    author="Prima",
+    author_email="teampriceless@prima.it",
+    description="",
+    packages=["primapy_uk_modelling_shared"],
+    python_requires=">=3.10",
+)
