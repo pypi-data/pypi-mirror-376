8f5ea6e93e10be4358fbff623740062449c91111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 07:05:29 +0000

    Add package ynab-amazon-categorizer/0.2.1/ynab_amazon_categorizer-0.2.1-py3-none-any.whl

---
+++ packages/ynab-amazon-categorizer/ynab_amazon_categorizer-0.2.1-py3-none-any.whl/ynab_amazon_categorizer/__init__.py
@@ -0,0 +1,7 @@
+"""YNAB Amazon Categorizer - Automatically categorize Amazon transactions in YNAB."""
+
+__version__ = "0.2.1"
+__author__ = "dizzlkheinz"
+__description__ = (
+    "Automatically categorize Amazon transactions in YNAB with rich item information"
+)
