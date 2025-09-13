c16ba38767bb58823ad2047cf378c9f11b1a21a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:05:34 +0000

    Add package pkg-deploy/1.0.1/pkg_deploy-1.0.1-py3-none-any.whl

---
+++ packages/pkg-deploy/pkg_deploy-1.0.1-py3-none-any.whl/pkg_deploy/upload.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import sys
+import logging
+import subprocess
+from pathlib import Path
+from abc import ABC, abstractmethod
+
+from .build import DeployConfig
+
+
+logger = logging.getLogger(__name__)
+
+
+class Upload(ABC):
+    """Deploy Base class"""
+
+    @abstractmethod
+    def upload(self, config: DeployConfig, dist_dir: Path) -> bool:
+        pass
+
+
+class NexusUpload(Upload):
+    """Nexus Deploy"""
+
+    @staticmethod
+    def get_wheel_files(config: DeployConfig):
+        wheel_files = []
+        for binary in (config.project_dir / 'dist').iterdir():
+            if config.package_name.replace("-", "_") in binary.name and binary.suffix == '.whl':
+                wheel_files.append(binary.name)
+        if len(wheel_files) != 1:
+            raise ValueError(f"Unable to determine wheel, candidates are: {wheel_files}")
+        wheel_file = wheel_files[0]
+        logger.info(f"Built {wheel_file}")
+        return wheel_file
+
+    def upload(self, config: DeployConfig, dist_dir: Path) -> bool:
+        try:
+            if not config.repository_url:
+                raise ValueError("Repository URL is required for Nexus deployment")
+
+            wheel_file = self.get_wheel_files(config)
+
+            cmd = [sys.executable, "-m", "twine", "upload",
+                   "--repository-url", config.repository_url,
+                   f"dist/{wheel_file}",
+                   "--disable-progress-bar"
+                   ]
+            if config.username:
+                cmd.extend(["--username", config.username])
+            if config.password:
+                cmd.extend(["--password", config.password])
+
+            logger.info(f"Running: {' '.join(cmd)}")
+
+            if config.dry_run:
+                logger.info(f"DRY RUN: wheel files from dist directory: {wheel_file}")
+                logger.info(f"DRY RUN: cmd: {cmd}")
+            else:
+                result = subprocess.run(cmd, capture_output=True, text=True)
+                if result.returncode != 0:
+                    raise ValueError(f"Upload failed, \nstdout: {result.stdout}\nstderr: {result.stderr}")
+                logger.info(f"Package uploaded to {config.repository_url} successfully")
+            return True
+        except Exception as e:
+            logger.error(f"Package upload error: {e}")
+            return False
