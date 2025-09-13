ad49b3543570e7d7e44367c82b7877fa73ef15eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:10:05 +0000

    Add package yt-astro-analysis/1.1.4/yt_astro_analysis-1.1.4-cp314-cp314-macosx_10_13_x86_64.whl

---
+++ packages/yt-astro-analysis/yt_astro_analysis-1.1.4-cp314-cp314-macosx_10_13_x86_64.whl/yt_astro_analysis/__init__.py
@@ -0,0 +1,30 @@
+"""
+API for yt astro analysis.
+
+
+
+"""
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2017, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
+__version__ = "1.1.4"
+
+from yt_astro_analysis import (
+    cosmological_observation,
+    halo_analysis,
+    ppv_cube,
+    radmc3d_export,
+)
+
+__all__ = [
+    "cosmological_observation",
+    "halo_analysis",
+    "ppv_cube",
+    "radmc3d_export",
+]
