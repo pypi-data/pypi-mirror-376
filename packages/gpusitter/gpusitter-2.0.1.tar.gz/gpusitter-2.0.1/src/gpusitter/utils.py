8d5f62b1fe648c90ff7c39a6bea7af46c9a86786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:06:14 +0000

    Add package gpusitter/2.0.1/gpusitter-2.0.1.tar.gz

---
+++ packages/gpusitter/gpusitter-2.0.1.tar.gz/gpusitter-2.0.1/src/gpusitter/utils.py
@@ -0,0 +1,50 @@
+import time
+from contextlib import nullcontext
+
+from rich.live import Live
+from rich.spinner import Spinner
+
+from gpusitter.logger import console
+
+
+def compute_storage_size(memory: int, dtype: str = "float32", len_shape: int = 3) -> list[int]:
+    """Compute the storage size required for a given GPU memory.
+
+    Args:
+        memory (int): The GPU memory size in MiB.
+        dtype (str): float32 or float64
+        len_shape (int): The length of the shape.
+
+    Returns:
+        int: The estimated storage size in bytes.
+    """
+    if dtype not in ("float32", "float64"):
+        raise ValueError("dtype must be 'float32' or 'float64'.")
+
+    bytes_per_element = 4 if dtype == "float32" else 8
+
+    memory = memory * 0.9
+
+    sz = pow(memory * 1024 * 1024 // bytes_per_element, 1 / len_shape)
+
+    return [int(sz)] * len_shape
+
+
+def countdown_timer(minutes: int | float, description: str = "Waiting", debug: bool = False) -> None:
+    """Display a spinner with MM:SS countdown for the given minutes."""
+    total_seconds = int(minutes * 60)
+
+    context = nullcontext() if debug else Live(console=console, refresh_per_second=10)
+    with context as live:
+        for remaining in range(total_seconds, 0, -1):
+            mins, secs = divmod(remaining, 60)
+
+            text = f"{description} in {mins:02d}:{secs:02d}"
+            if debug:
+                console.log(f"[blue]Debug:[/blue] {text}")
+            else:
+                spinner = Spinner("dots", text=f"[green]{text}[/green]")
+                live.update(spinner)
+            time.sleep(1)
+
+    console.log(f"\n[green]{description} completed![/green]")
