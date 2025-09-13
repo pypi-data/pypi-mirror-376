f1be26c84eb99dcd440b78406d04a61f87c1cfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:05:54 +0000

    Add package Flowfile/0.4.0/flowfile-0.4.0.tar.gz

---
+++ packages/Flowfile/flowfile-0.4.0.tar.gz/flowfile-0.4.0/flowfile/flowfile/web/static/assets/MultiSelect-61b98268.js
@@ -0,0 +1,5 @@
+import { _ as _sfc_main } from "./MultiSelect.vue_vue_type_script_setup_true_lang-2a7c8312.js";
+import "./index-5ec791df.js";
+export {
+  _sfc_main as default
+};
---
+++ packages/Flowfile/flowfile-0.4.0.tar.gz/flowfile-0.4.0/flowfile/flowfile/web/static/assets/SingleSelect-7298811a.js
@@ -0,0 +1,5 @@
+import { _ as _sfc_main } from "./SingleSelect.vue_vue_type_script_setup_true_lang-43807bad.js";
+import "./index-5ec791df.js";
+export {
+  _sfc_main as default
+};
