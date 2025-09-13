b97161e86d5f373b7e4f7f68d5ec0f7e25cf9005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:17 +0000

    Add package deseasion-backend/0.1.2/deseasion_backend-0.1.2.tar.gz

---
+++ packages/deseasion-backend/deseasion_backend-0.1.2.tar.gz/deseasion_backend-0.1.2/src/deseasion/backend/extensions/migrate.py
@@ -0,0 +1,14 @@
+from importlib import resources
+
+from flask import Flask
+from flask_migrate import Migrate
+
+from ..models import db
+
+migrate = Migrate()
+
+
+def init_app(app: Flask):
+    """Create the migrate group of commands."""
+    with resources.path("deseasion.backend", "migrations") as migrations_path:
+        migrate.init_app(app, db, str(migrations_path))
