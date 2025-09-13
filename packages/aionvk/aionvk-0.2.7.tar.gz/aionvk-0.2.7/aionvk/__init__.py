4b9673aa2a25e6e702cdf147737c974c554e112e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:06:10 +0000

    Add package aionvk/0.2.7/aionvk-0.2.7.tar.gz

---
+++ packages/aionvk/aionvk-0.2.7.tar.gz/aionvk-0.2.7/aionvk/__init__.py
@@ -0,0 +1,23 @@
+from .bot import Bot, Dispatcher, Router
+from .builders import Button, KeyboardBuilder
+from .client import VKClient
+from .exceptions import APIError, AionVKError
+from .magic import F
+from .types import Callback, Message
+
+__all__ = [
+    "Bot",
+    "Dispatcher",
+    "Router",
+    "KeyboardBuilder",
+    "Button",
+    "F",
+    "VKClient",
+    "APIError",
+    "AionVKError",
+    "Callback",
+    "Message",
+]
+
+Message.model_rebuild()
+Callback.model_rebuild()
