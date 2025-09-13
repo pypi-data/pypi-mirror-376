0b043438eac86b9bed3ef834f7bcab72a054265a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:41 +0000

    Add package byzh-ai/0.0.9.2/byzh_ai-0.0.9.2.tar.gz

---
+++ packages/byzh-ai/byzh_ai-0.0.9.2.tar.gz/byzh_ai-0.0.9.2/byzh/ai/Butils/grid_func.py
@@ -0,0 +1,85 @@
+from itertools import product
+from typing import Sequence, Callable, Literal
+import warnings
+class B_GridFunc():
+    def __init__(
+            self,
+            mode:Literal["product", "zip"] = "product",
+            run_verbose=False,
+            **kwargs: Sequence
+    ):
+        '''
+        :param mode: "product": 所有情况, "zip": 对应情况
+        :param kwargs: 值需要被[]包裹
+        '''
+        self.mode = mode
+        self.run_verbose = run_verbose
+        self.keys = []
+
+        for key, value in kwargs.items():
+            setattr(self, key+"_list", value)
+            self.keys.append(key)
+
+        self.values_lst = self._get_values_lst()
+
+    def _get_values_lst(self):
+        temp = [getattr(self, key + "_list") for key in self.keys]
+        if self.mode == "product":
+            return list(product(*temp))
+        elif self.mode == "zip":
+            # 检查是否个数相同
+            if len(set(map(len, temp))) != 1:
+                warnings.warn("zip模式下, 如果各部分的len不一样, 则超出部分不参与遍历")
+            return list(zip(*temp))
+        else:
+            raise ValueError("Invalid mode")
+
+    def _set_values(self, index):
+        for key, value in zip(self.keys, self.values_lst[index]):
+            setattr(self, key, value)
+
+    def run(self, func: Callable, *args, **kwargs):
+        '''
+        运行函数
+        :param func:
+        :param args:
+        :param kwargs:
+        :return:
+        '''
+        total = len(self.values_lst)
+        for i in range(total):
+            self._set_values(i)
+            func(*args, **kwargs)
+            if self.run_verbose:
+                print(f"[B_GridFunc] 完成{i+1}/{total}次迭代")
+                print(f"[B_GridFunc] 当前参数为:")
+                for key in self.keys:
+                    print(f"[B_GridFunc] \t{key}={getattr(self, key)}")
+
+
+
+if __name__ == '__main__':
+
+    # 示例 1: product 笛卡尔积模式
+    def demo_func1():
+        print(f"x={wrapper.x}, y={wrapper.y}, x+y={wrapper.x + wrapper.y}, z={wrapper.z}")
+    print("=== product 模式 ===")
+    wrapper = B_GridFunc(
+        mode="product",
+        x=[1, 2, 3],
+        y=[10, 20],
+        z=[50]
+    )
+    wrapper.run(demo_func1)
+
+    # 示例 2: zip 模式
+    def demo_func2(awa, qwq):
+        print(f"x={wrapper.x}, y={wrapper.y}, x+y={wrapper.x + wrapper.y}, awa={awa}, qwq={qwq}")
+    print("\n=== zip 模式 ===")
+    wrapper = B_GridFunc(
+        "zip",
+        True,
+        x=[1, 2, 3],
+        y=[10, 20, 30],
+    )
+    wrapper.run(demo_func2, "哈哈哈", qwq="呜呜呜")
