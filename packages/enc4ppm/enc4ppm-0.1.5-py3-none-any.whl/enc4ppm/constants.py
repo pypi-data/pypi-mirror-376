661076fd2474748a90cce1d5d2c3771727d8c578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:33 +0000

    Add package enc4ppm/0.1.5/enc4ppm-0.1.5-py3-none-any.whl

---
+++ packages/enc4ppm/enc4ppm-0.1.5-py3-none-any.whl/enc4ppm/constants.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class LabelingType(Enum):
+    NEXT_ACTIVITY = 'next_activity'
+    REMAINING_TIME = 'remaining_time'
+    OUTCOME = 'outcome'
+    CUSTOM = 'custom'
+    NONE = 'none'
+
+
+class CategoricalEncoding(Enum):
+    STRING = 'string'
+    ONE_HOT = 'one_hot'
+
+
+class PrefixStrategy(Enum):
+    UP_TO_SPECIFIED = 'up_to_specified'
+    ONLY_SPECIFIED = 'only_specified'
