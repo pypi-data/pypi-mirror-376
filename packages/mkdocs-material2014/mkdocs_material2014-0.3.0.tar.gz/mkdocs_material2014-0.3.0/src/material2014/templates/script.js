796c1bb8e9dfa0e1c0b655f8dc7a8df5c5507a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:06:53 +0000

    Add package mkdocs-material2014/0.3.0/mkdocs_material2014-0.3.0.tar.gz

---
+++ packages/mkdocs-material2014/mkdocs_material2014-0.3.0.tar.gz/mkdocs_material2014-0.3.0/src/material2014/templates/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.collapsible.expandable').collapsible({
+        accordion: false
+    });
+    $(".d-search-button").click(e => {
+        e.preventDefault();
+        $("body").addClass("d-search-open");
+    });
+    $(".d-search-close-button").click(e => {
+        e.preventDefault();
+        $("body").removeClass("d-search-open");
+    });
+    if (!$(".d-cool-page-start").length) {
+        $('.navbar-fixed').removeClass("d-seamless-cool-page-navbar");
+    }
+    $(".d-toc-wrapper").pushpin({
+        top: 320,
+        offset: 64
+    })
+    $('h1,h2,h3,.scollspy').scrollSpy();
+    $('.tabs').tabs({
+        swipeable: false,
+        duration: 150
+    });
+    $('.mkdocs-ezglossary-link').addClass("tooltipped");
+    $('.mkdocs-ezglossary-link').each((i, obj) => {
+        obj = $(obj);
+        obj.attr("data-tooltip", obj.attr("title"));
+        obj.attr("title", null);
+    });
+    $('.tooltipped').tooltip();
+});
+
+$(document).scroll(function () {
+    $('.navbar-fixed').removeClass("no-transition");
+    if ($(document).scrollTop() >= 180 && $(".d-cool-page-start").length) {
+        $('.navbar-fixed').removeClass("d-no-text-cool-page-navbar");
+    } else {
+        $('.navbar-fixed').addClass("d-no-text-cool-page-navbar");
+    }
+    if ($(document).scrollTop() >= 240 && $(".d-cool-page-start").length) {
+        $('.navbar-fixed').removeClass("d-seamless-cool-page-navbar");
+    } else {
+        $('.navbar-fixed').addClass("d-seamless-cool-page-navbar");
+    }
+});
