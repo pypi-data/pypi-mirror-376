f3c05c8efb0db5eb7f42ea378e9aef066380a93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:22 +0000

    Add package lhub_integ/1.0.14/lhub_integ-1.0.14-py3-none-any.whl

---
+++ packages/lhub_integ/lhub_integ-1.0.14-py3-none-any.whl/lhub_integ/common/constants.py
@@ -0,0 +1,15 @@
+"""
+    All Global Constants
+"""
+
+EXECUTION_START_TIME = '__execution_start_time_ms'
+EXECUTION_END_TIME = '__execution_end_time_ms'
+
+SSL_VERIFY_KEY = '__lh_ssl_verify'
+THREADS_TO_USE = '__lh_multithread_count'
+PATH_TO_CA_CERT = 'CURL_CA_BUNDLE'
+TIME_BETWEEN_ROWS = "time_between_consecutive_requests_milliseconds"
+
+# IO Helper
+INPUT_DATA_JSON_FOLDER_PATH = '__lh_integration_input_data_json_folder_path'
+OUTPUT_DATA_JSON_FILE_PATH = '__lh_integration_output_data_json_file_path'
