edb3967d6b0b12e734edcae39bd0dc966c385089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:06:28 +0000

    Add package snowglobe-sdk/0.0.1/snowglobe_sdk-0.0.1.tar.gz

---
+++ packages/snowglobe-sdk/snowglobe_sdk-0.0.1.tar.gz/snowglobe_sdk-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="snowglobe-sdk",
+    version="0.0.1",
+    author="Snowglobe",
+    author_email="hi@snowglobe.so",
+    description="Simulation engine for AI Agents.",
+    long_description="Simulation engine for AI Agents.",
+    long_description_content_type="text/plain",
+    url="https://snowglobe.so",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires=">=3.7",
+)
