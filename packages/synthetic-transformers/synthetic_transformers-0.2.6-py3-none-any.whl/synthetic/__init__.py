cfab26f015153e0b9e56d039d511471fb6d41ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:05:50 +0000

    Add package synthetic-transformers/0.2.6/synthetic_transformers-0.2.6-py3-none-any.whl

---
+++ packages/synthetic-transformers/synthetic_transformers-0.2.6-py3-none-any.whl/synthetic/__init__.py
@@ -0,0 +1,1 @@
+from synthetic.transformer import Transformer
---
+++ packages/synthetic-transformers/synthetic_transformers-0.2.6-py3-none-any.whl/synthetic/commands/__init__.py
@@ -0,0 +1,7 @@
+from synthetic.commands.signature import SignatureMatch, CommandSignature
+from synthetic.commands.base import (
+    Command,
+    CommandInput,
+    CommandOutput,
+    CommandExecution,
+)
