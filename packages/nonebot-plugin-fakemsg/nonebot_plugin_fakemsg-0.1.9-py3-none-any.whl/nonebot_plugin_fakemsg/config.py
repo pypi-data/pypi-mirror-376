b0fbe537360726c0f775590cb2dd49b82732d935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:41 +0000

    Add package nonebot-plugin-fakemsg/0.1.9/nonebot_plugin_fakemsg-0.1.9-py3-none-any.whl

---
+++ packages/nonebot-plugin-fakemsg/nonebot_plugin_fakemsg-0.1.9-py3-none-any.whl/nonebot_plugin_fakemsg/config.py
@@ -0,0 +1,11 @@
+from nonebot.plugin import get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    user_split: str = "|"
+    message_split: str = " "
+    fakemsg_whitelist: list[str] = []
+
+
+config = get_plugin_config(Config)
