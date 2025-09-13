97d075c4c9b273dcce8d9c207f53925d3ebb0934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:07:20 +0000

    Add package contrast-agent/10.16.0/contrast_agent-10.16.0-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/contrast-agent/contrast_agent-10.16.0-cp311-cp311-musllinux_1_2_aarch64.whl/contrast/utils/loggers/__init__.py
@@ -0,0 +1,13 @@
+# Copyright © 2025 Contrast Security, Inc.
+# See https://www.contrastsecurity.com/enduser-terms-0317a for more details.
+import sys
+
+
+DEFAULT_LOG_PATH = "contrast-agent.log"
+DEFAULT_SECURITY_LOG_PATH = "security.log"
+DEFAULT_SECURITY_LOG_LEVEL = "INFO"
+DEFAULT_LOG_LEVEL = "INFO"
+DEFAULT_PROGNAME = "Contrast Agent"
+LOGGER_NAME = "contrast"
+
+DEFAULT_AGENT_LOGGER_PATH = sys.stderr
