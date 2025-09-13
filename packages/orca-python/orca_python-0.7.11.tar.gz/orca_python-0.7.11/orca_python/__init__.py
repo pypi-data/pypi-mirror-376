c48d04926fe774f370a3b29105a28097d8f1c3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:06:09 +0000

    Add package orca-python/0.7.11/orca_python-0.7.11.tar.gz

---
+++ packages/orca-python/orca_python-0.7.11.tar.gz/orca_python-0.7.11/orca_python/__init__.py
@@ -0,0 +1,23 @@
+from orca_python.main import (
+    Window,
+    Processor,
+    EmitWindow,
+    NoneResult,
+    WindowType,
+    ArrayResult,
+    ValueResult,
+    StructResult,
+    ExecutionParams,
+)
+
+__all__ = [
+    "Processor",
+    "EmitWindow",
+    "Window",
+    "WindowType",
+    "StructResult",
+    "ValueResult",
+    "ArrayResult",
+    "NoneResult",
+    "ExecutionParams",
+]
