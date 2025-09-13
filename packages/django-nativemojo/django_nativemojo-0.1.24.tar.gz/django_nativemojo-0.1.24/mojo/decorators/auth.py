1c0eac71f9e5fe59819906ff1fe9147ed1efa082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:05:35 +0000

    Add package django-nativemojo/0.1.24/django_nativemojo-0.1.24.tar.gz

---
+++ packages/django-nativemojo/django_nativemojo-0.1.24.tar.gz/django_nativemojo-0.1.24/mojo/decorators/auth.py
@@ -0,0 +1,30 @@
+from functools import wraps
+import mojo.errors
+from mojo.helpers import logit
+
+logger = logit.get_logger("error", "error.log")
+
+def requires_perms(*required_perms):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            if not request.user.is_authenticated:
+                raise mojo.errors.PermissionDeniedException()
+            perms = set(required_perms)
+            if not request.user.has_permission(perms):
+                logger.error(f"{request.user.username} is missing {perms}")
+                raise mojo.errors.PermissionDeniedException()
+            return func(request, *args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def requires_auth():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            if not request.user.is_authenticated:
+                raise mojo.errors.PermissionDeniedException()
+            return func(request, *args, **kwargs)
+        return wrapper
+    return decorator
