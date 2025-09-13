cd6c7c8dc86c7d3a63cfaa55f704eab382e3b83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:06:05 +0000

    Add package libjam/0.1.6/libjam-0.1.6-py3-none-any.whl

---
+++ packages/libjam/libjam-0.1.6-py3-none-any.whl/libjam/__init__.py
@@ -0,0 +1,15 @@
+# Importing classes
+from .captain import Captain
+from .drawer import Drawer
+from .typewriter import Typewriter
+from .clipboard import Clipboard
+from .notebook import Notebook
+from .flashcard import Flashcard
+
+# Instancing
+captain = Captain()
+drawer = Drawer()
+typewriter = Typewriter()
+clipboard = Clipboard()
+notebook = Notebook()
+flashcard = Flashcard()
