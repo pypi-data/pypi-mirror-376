327d79d214927aa0c8daf2c2eaa972e0455be6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:50 +0000

    Add package relationalai/0.9.11/relationalai-0.9.11.tar.gz

---
+++ packages/relationalai/relationalai-0.9.11.tar.gz/relationalai-0.9.11/src/relationalai/early_access/builder/annotations.py
@@ -0,0 +1,6 @@
+# In your_package/submodule.py
+from .builder import Relationship
+
+external = Relationship.builtins["external"]
+concept_population = Relationship.builtins["concept_population"]
+from_cdc = Relationship.builtins["from_cdc"]
