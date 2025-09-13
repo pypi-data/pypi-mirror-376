d3364387047d775e1554c76a2bbb2013aef7349a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:07:17 +0000

    Add package aip-agents-binary/0.0.0b2/aip_agents_binary-0.0.0b2-cp312-cp312-win_amd64.whl

---
+++ packages/aip-agents-binary/aip_agents_binary-0.0.0b2-cp312-cp312-win_amd64.whl/aip_agents/types/__init__.pyi
@@ -0,0 +1,3 @@
+from aip_agents.types.a2a_events import A2AEvent as A2AEvent, A2AStreamEventType as A2AStreamEventType, ToolCallInfo as ToolCallInfo, ToolResultInfo as ToolResultInfo
+
+__all__ = ['A2AEvent', 'A2AStreamEventType', 'ToolCallInfo', 'ToolResultInfo']
