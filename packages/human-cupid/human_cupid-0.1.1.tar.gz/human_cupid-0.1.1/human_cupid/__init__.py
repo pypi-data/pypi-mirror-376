587a77e0aef396e6470eb4f2e8c04f42940316c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:05:46 +0000

    Add package human-cupid/0.1.1/human_cupid-0.1.1.tar.gz

---
+++ packages/human-cupid/human_cupid-0.1.1.tar.gz/human_cupid-0.1.1/human_cupid/__init__.py
@@ -0,0 +1,5 @@
+from human_cupid.cupid import Cupid
+from human_cupid.sub import SubprofileAnalyzer
+from human_cupid.super import SuperprofileAnalyzer
+
+__all__ = ["Cupid", "SubprofileAnalyzer", "SuperprofileAnalyzer"]
