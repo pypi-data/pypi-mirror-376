de8b380f3c7f7f544be2bfef113d0a57563ebf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 07:05:31 +0000

    Add package adieu-allianceauth-customizations/0.0.9/adieu_allianceauth_customizations-0.0.9.tar.gz

---
+++ packages/adieu-allianceauth-customizations/adieu_allianceauth_customizations-0.0.9.tar.gz/adieu_allianceauth_customizations-0.0.9/adieu_allianceauth_customizations/__init__.py
@@ -0,0 +1,13 @@
+"""Initialize the app"""
+
+__version__ = "0.0.9"
+__title__ = "ADIEU Auth Customizations"
+__package_name__ = "adieu-allianceauth-customizations"
+__package_name_verbose__ = "Black Rose AllianceAuth Customizations"
+__package_name_useragent__ = "ADIEU-AllianceAuth-Customizations"
+__app_name__ = "adieu_allianceauth_customizations"
+__github_url__ = f"https://github.com/blackrose-eve/{__package_name__}"
+__user_agent__ = (
+    f"{__package_name_useragent__}/{__version__} "
+    f"(+{__github_url__}) requests/{requests.__version__}"
+)
