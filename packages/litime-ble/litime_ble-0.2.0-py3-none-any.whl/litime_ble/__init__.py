36e5b599a7f5278038e2b88535d7ef51099de2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:06:22 +0000

    Add package litime-ble/0.2.0/litime_ble-0.2.0-py3-none-any.whl

---
+++ packages/litime-ble/litime_ble-0.2.0-py3-none-any.whl/litime_ble/__init__.py
@@ -0,0 +1,39 @@
+from .client import BatteryClient
+from .models import BatteryStatus, ChargeState
+from .errors import (
+    BatteryError,
+    BatteryConnectionError,
+    ProtocolError,
+    BatteryTimeoutError,
+)
+from .logging import configure_logging, DEBUG, INFO, WARNING, ERROR
+from .discovery import (
+    discover_devices,
+    discover_devices_sync,
+    find_litime_batteries,
+    find_litime_batteries_sync,
+    format_device_info,
+)
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "BatteryClient",
+    "BatteryStatus",
+    "ChargeState",
+    "BatteryError",
+    "BatteryConnectionError",
+    "ProtocolError",
+    "BatteryTimeoutError",
+    "configure_logging",
+    "DEBUG",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "discover_devices",
+    "discover_devices_sync",
+    "find_litime_batteries",
+    "find_litime_batteries_sync",
+    "format_device_info",
+    "__version__",
+]
