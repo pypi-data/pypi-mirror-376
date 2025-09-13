6645016267c8edf32bcfd1ead2e303745e488096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:06:28 +0000

    Add package cleanframe/0.2.1/cleanframe-0.2.1.tar.gz

---
+++ packages/cleanframe/cleanframe-0.2.1.tar.gz/cleanframe-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cleanframe',
+    version='0.2.1',
+    author='Fayez Hesham',
+    author_email='fayezhesham510@gmail.com',
+    description='A Python library for cleaning and validating pandas DataFrames.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fayezhesham/cleanframe',
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pandas>=1.0.0',
+    ],
+
+    # package_data={
+    #     'cleanframe': ['data/*.csv'],
+    # },
+    # include_package_data=True, # Set to True to include data files specified in MANIFEST.in
+)
