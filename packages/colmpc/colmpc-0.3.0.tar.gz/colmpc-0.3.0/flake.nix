1f5143baa78337fba2545bc78952a9d6ba62a4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:07:11 +0000

    Add package colmpc/0.3.0/colmpc-0.3.0.tar.gz

---
+++ packages/colmpc/colmpc-0.3.0.tar.gz/colmpc-0.3.0/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Collision avoidance for MPC";
+
+  inputs = {
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
+      perSystem =
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
+          packages =
+            let
+              src = lib.fileset.toSource {
+                root = ./.;
+                fileset = lib.fileset.unions [
+                  ./examples
+                  ./include
+                  ./python
+                  ./tests
+                  ./CMakeLists.txt
+                  ./package.xml
+                  ./pyproject.toml
+                ];
+              };
+            in
+            {
+              default = self'.packages.py-colmpc;
+              colmpc = pkgs.colmpc.overrideAttrs { inherit src; };
+              py-colmpc = pkgs.python3Packages.colmpc.overrideAttrs {
+                inherit src;
+              };
+            };
+        };
+    };
+}
