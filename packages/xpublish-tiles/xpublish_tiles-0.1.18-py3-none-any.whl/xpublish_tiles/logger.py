536aa0e54add6431fdd2dac89f26f0d1531c91e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 04:06:06 +0000

    Add package xpublish-tiles/0.1.18/xpublish_tiles-0.1.18-py3-none-any.whl

---
+++ packages/xpublish-tiles/xpublish_tiles-0.1.18-py3-none-any.whl/xpublish_tiles/logger.py
@@ -0,0 +1,8 @@
+"""
+Logging setup and shared logger for the xpublish_tiles package.
+"""
+
+import logging
+
+# Set up a shared logger for the xpublish_tiles package
+logger = logging.getLogger("xpublish_tiles")
