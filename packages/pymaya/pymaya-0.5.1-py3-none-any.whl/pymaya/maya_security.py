f7db996b2215a707429f79f8165215ff0f6b0611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 16:05:22 +0000

    Add package pymaya/0.5.1/pymaya-0.5.1-py3-none-any.whl

---
+++ packages/pymaya/pymaya-0.5.1-py3-none-any.whl/pymaya/maya_security.py
@@ -0,0 +1,51 @@
+from functools import partial
+from typing import List, Dict, Any
+from datetime import date
+
+from cachetools import cachedmethod
+from cachetools.keys import hashkey
+
+from pymaya.maya_base import MayaBase
+from pymaya.request_classes.maya_all_securities_request import MayaAllSecuritiesRequest
+from pymaya.request_classes.maya_security_data_request import MayaSecurityDataRequest
+from pymaya.request_classes.maya_security_request import MayaSecurityHistoricalRequest
+from pymaya.utils import streamify, Language
+
+
+class MayaSecurity(MayaBase):
+    @cachedmethod(lambda self: self.cache, key=partial(hashkey, "all"))
+    def get_all_securities(self, lang: Language = Language.ENGLISH):
+        return self._send_request(MayaAllSecuritiesRequest(lang))
+
+    @staticmethod
+    def get_names(english_details: Dict[str, Any], hebrew_details: Dict[str, Any]):
+        return {
+            "english_short_name": english_details.get('Name'),
+            "english_long_name": english_details.get('LongName') or english_details.get('SecurityLongName'),
+            "hebrew_short_name": hebrew_details.get("Name"),
+            "hebrew_long_name": hebrew_details.get("LongName") or hebrew_details.get('SecurityLongName'),
+        }
+
+    def get_details(self, security_id: str, lang: Language = Language.ENGLISH):
+        return self._send_request(MayaSecurityDataRequest(security_id, lang=lang))
+
+    def get_price_history_chunk(
+        self, security_id: str, from_date: date, to_date: date, page: int, lang: Language = Language.ENGLISH
+    ) -> Dict:
+        return self._send_request(MayaSecurityHistoricalRequest(security_id, from_date, to_date, page, lang=lang))
+
+    @streamify
+    def get_price_history(
+        self,
+        security_id: str,
+        from_date: date,
+        to_date: date = date.today(),
+        page: int = 1,
+        lang: Language = Language.ENGLISH,
+    ) -> List[Dict]:
+
+        if not to_date:
+            to_date = date.today()
+
+        data = self.get_price_history_chunk(security_id, from_date, to_date, page, lang)
+        return data.get("Items", [])
---
+++ packages/pymaya/pymaya-0.5.1-py3-none-any.whl/pymaya/request_classes/maya_historical_request.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from datetime import date
+
+from pymaya.request_classes.maya_fund_base_request import MayaFundBaseRequest
+from pymaya.utils import Language
+
+
+class MayaHistoricalRequest(MayaFundBaseRequest):
+    end_point = "fund/history"
+    method = "POST"
+
+    def __init__(
+        self,
+        security_id: str,
+        from_date: Optional[date],
+        to_date: Optional[date],
+        page: int = 1,
+        period: int = 0,
+        lang: Language = Language.ENGLISH,
+    ):
+        super(MayaHistoricalRequest, self).__init__(lang, data={})
+        self.request.headers["Content-Type"] = "application/x-www-form-urlencoded"
+        self.request.data["DateFrom"] = from_date.isoformat() if from_date else None
+        self.request.data["DateTo"] = to_date.isoformat() if to_date else None
+        self.request.data["FundId"] = security_id
+        self.request.data["Page"] = page
+        self.request.data["Period"] = period
