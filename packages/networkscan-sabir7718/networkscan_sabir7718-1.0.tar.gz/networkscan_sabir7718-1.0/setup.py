61decf56da83326c53eb60bde335dc863fc518c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:34 +0000

    Add package networkscan-sabir7718/1.0/networkscan_sabir7718-1.0.tar.gz

---
+++ packages/networkscan-sabir7718/networkscan_sabir7718-1.0.tar.gz/networkscan_sabir7718-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="networkscan-sabir7718",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "networkscan = networkscan.networkscan:main"
+        ]
+    },
+    author="SABIR7718",
+    description="A fast multi-threaded network port scanner",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
