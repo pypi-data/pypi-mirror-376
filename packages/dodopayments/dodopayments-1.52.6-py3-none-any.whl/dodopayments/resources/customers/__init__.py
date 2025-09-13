2e5c56913d0c76574a55314fab5cbbf6ee9bb2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 15:06:28 +0000

    Add package dodopayments/1.52.6/dodopayments-1.52.6-py3-none-any.whl

---
+++ packages/dodopayments/dodopayments-1.52.6-py3-none-any.whl/dodopayments/resources/customers/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .customers import (
+    CustomersResource,
+    AsyncCustomersResource,
+    CustomersResourceWithRawResponse,
+    AsyncCustomersResourceWithRawResponse,
+    CustomersResourceWithStreamingResponse,
+    AsyncCustomersResourceWithStreamingResponse,
+)
+from .customer_portal import (
+    CustomerPortalResource,
+    AsyncCustomerPortalResource,
+    CustomerPortalResourceWithRawResponse,
+    AsyncCustomerPortalResourceWithRawResponse,
+    CustomerPortalResourceWithStreamingResponse,
+    AsyncCustomerPortalResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "CustomerPortalResource",
+    "AsyncCustomerPortalResource",
+    "CustomerPortalResourceWithRawResponse",
+    "AsyncCustomerPortalResourceWithRawResponse",
+    "CustomerPortalResourceWithStreamingResponse",
+    "AsyncCustomerPortalResourceWithStreamingResponse",
+    "CustomersResource",
+    "AsyncCustomersResource",
+    "CustomersResourceWithRawResponse",
+    "AsyncCustomersResourceWithRawResponse",
+    "CustomersResourceWithStreamingResponse",
+    "AsyncCustomersResourceWithStreamingResponse",
+]
