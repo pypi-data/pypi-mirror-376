cfbbf18750b6f0ab1a3e41460da51fb6d6164147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:06:15 +0000

    Add package cog/0.16.7/cog-0.16.7.tar.gz

---
+++ packages/cog/cog-0.16.7.tar.gz/cog-0.16.7/pkg/global/global.go
@@ -0,0 +1,13 @@
+package global
+
+var (
+	Version                 = "dev"
+	Commit                  = ""
+	BuildTime               = "none"
+	Debug                   = false
+	ProfilingEnabled        = false
+	ReplicateRegistryHost   = "r8.im"
+	ReplicateWebsiteHost    = "replicate.com"
+	LabelNamespace          = "run.cog."
+	CogBuildArtifactsFolder = ".cog"
+)
