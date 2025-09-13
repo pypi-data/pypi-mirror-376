fdb303645026ecf18656b894e12270f1deaa2825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:40 +0000

    Add package autogemini/0.1.6.post1/autogemini-0.1.6.post1-py3-none-any.whl

---
+++ packages/autogemini/autogemini-0.1.6.post1-py3-none-any.whl/autogemini/__main__.py
@@ -0,0 +1,6 @@
+"""Command line interface for AutoGemini."""
+
+from .main import main
+
+if __name__ == "__main__":
+    main()
---
+++ packages/autogemini/autogemini-0.1.6.post1-py3-none-any.whl/autogemini/main.py
@@ -0,0 +1,7 @@
+def main():
+    """Main entry point for the AutoGemini package."""
+    print("Hello from AutoGemini!")
+
+
+if __name__ == "__main__":
+    main()
