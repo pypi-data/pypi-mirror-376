484fa5ac93dc4465baee209f1be7263d1ac4f072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 14:07:06 +0000

    Add package degirum-face/0.1.1/degirum_face-0.1.1-py3-none-any.whl

---
+++ packages/degirum-face/degirum_face-0.1.1-py3-none-any.whl/degirum_face/_version.py
@@ -0,0 +1,10 @@
+#
+# _version.py: package version
+#
+# Copyright DeGirum Corporation 2025
+# All rights reserved
+#
+
+# >>> increment version here vvv
+__version_info__ = ("0", "1", "1")
+__version__ = ".".join(__version_info__)
