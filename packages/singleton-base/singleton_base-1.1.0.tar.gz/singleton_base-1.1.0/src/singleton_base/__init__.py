45e64c8097fec978f923af9d1c64ecdbd995d19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 03:09:31 +0000

    Add package singleton-base/1.1.0/singleton_base-1.1.0.tar.gz

---
+++ packages/singleton-base/singleton_base-1.1.0.tar.gz/singleton_base-1.1.0/src/singleton_base/__init__.py
@@ -0,0 +1,7 @@
+"""Singleton Base Class to make any other class a singleton with proper type hinting."""
+
+from .singleton_base import SingletonBase
+
+__version__ = "1.2.0"
+
+__all__ = ["SingletonBase", "__version__"]
