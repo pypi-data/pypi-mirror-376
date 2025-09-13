12a0e4a8d15966eebadcbe41c1c8283df5d65ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 05:05:33 +0000

    Add package ax3-redactor/3.0.1/ax3_redactor-3.0.1-py3-none-any.whl

---
+++ packages/ax3-redactor/ax3_redactor-3.0.1-py3-none-any.whl/redactor/mixins.py
@@ -0,0 +1,26 @@
+class RedactorMixin:
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        for field in getattr(self, 'redactor_fields', []):
+            form.base_fields[field].widget.attrs['enable-redactor'] = True
+
+        for field in getattr(self, 'basic_redactor_fields', []):
+            form.base_fields[field].widget.attrs['enable-basic-redactor'] = True
+
+        return form
+
+    class Media:
+        css = {
+            'all': [
+                'vendor/redactor/redactor.min.css',
+                'redactor/admin-redactor.css',
+            ]
+        }
+        js = [
+            'vendor/redactor/redactor.min.js',
+            'vendor/redactor/plugins/imagemanager.min.js',
+            'vendor/redactor/plugins/video.min.js',
+            'vendor/redactor/plugins/widget.min.js',
+            'redactor/admin-redactor.js',
+        ]
