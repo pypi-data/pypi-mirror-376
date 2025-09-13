ec354f807e73ef489ddd76bc0d14955492f4ec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:06:17 +0000

    Add package fetchx/0.0.33/fetchx-0.0.33.tar.gz

---
+++ packages/fetchx/fetchx-0.0.33.tar.gz/fetchx-0.0.33/package/fetchx/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+from .fetchx import call, fetch, fetch_setup, translate_fetch, smart_translate_fetch
+from .generaltypes import null, true, false
+
+__all__ = (
+    "call",
+    "fetch",
+    "fetch_setup",
+    "translate_fetch",
+    "smart_translate_fetch",
+    "null",
+    "true",
+    "false"
+)
+
+
