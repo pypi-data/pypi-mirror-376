1edbab050884fe7d961e6b47cbefa7341073a4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 16:05:28 +0000

    Add package counted-float/0.8.4/counted_float-0.8.4-py3-none-any.whl

---
+++ packages/counted-float/counted_float-0.8.4-py3-none-any.whl/counted_float/config/__init__.py
@@ -0,0 +1,15 @@
+from counted_float._core.counting.config import (
+    get_default_consensus_flop_weights,
+    get_default_empirical_flop_weights,
+    get_default_theoretical_flop_weights,
+    get_flop_weights,
+    set_flop_weights,
+)
+
+__all__ = [
+    "get_default_consensus_flop_weights",
+    "get_default_empirical_flop_weights",
+    "get_default_theoretical_flop_weights",
+    "get_flop_weights",
+    "set_flop_weights",
+]
