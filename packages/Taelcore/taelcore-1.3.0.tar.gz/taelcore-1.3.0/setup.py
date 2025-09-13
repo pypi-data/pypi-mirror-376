7901b4ce0edbac470443168b5b7f5c11c3713121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 06:05:42 +0000

    Add package Taelcore/1.3.0/taelcore-1.3.0.tar.gz

---
+++ packages/Taelcore/taelcore-1.3.0.tar.gz/taelcore-1.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Taelcore',  
+    version='1.3.0',
+    description="Dimension reduction with best linear combination for optimal reduction of embeddings (Taelcore)",
+    url="https://github.com/MorillaLab/Taelcore",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Ian MORILLA',  
+    author_email='ian.morilla@math.univ-paris13.fr',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
