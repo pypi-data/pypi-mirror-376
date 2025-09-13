7bae3a36792f89ecc765b337b27982a9bd88f712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-13 16:05:58 +0000

    Add package PetroMap/0.1.3/petromap-0.1.3.tar.gz

---
+++ packages/PetroMap/petromap-0.1.3.tar.gz/petromap-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Read the README.md for PyPI long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PetroMap",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "scipy",
+        "plotly",
+        "scikit-learn"
+    ],
+    author="Nashat Jumaah Omar",
+    description="A Contour Mapping Utility for Oil and Gas Engineers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # <-- This is important
+    url="https://github.com/Nashat90/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
