5d25b320755509a3b21558a7dc5d4981200fc37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:27 +0000

    Add package ruff-quickfix/0.2.1/ruff_quickfix-0.2.1-py3-none-any.whl

---
+++ packages/ruff-quickfix/ruff_quickfix-0.2.1-py3-none-any.whl/ruff_quickfix/__init__.py
@@ -0,0 +1,11 @@
+"""
+File:        ruff_quickfix/__init__.py
+Author:      Neal Joslin
+Date:        2024-08-17
+Email:       neal@joslin.io
+Description: import entry
+"""
+
+from .cli import cli
+
+__all__ = ["cli"]
