9d3fdc026667be0cc78f5c5403e1a1e58afbc4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:18:43 +0000

    Add package Bac-info/0.0.2/bac_info-0.0.2.tar.gz

---
+++ packages/Bac-info/bac_info-0.0.2.tar.gz/bac_info-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name="Bac_info", 
+    version="0.0.2",
+    author="Aziz boudriga",
+    author_email="",
+    description="A collection of utility functions for common programming tasks including input validation, array/matrix operations, string processing, and mathematical calculations.",
+    long_description=open("README.md", encoding="utf-8").read()+"\n\n"+open("changelog.txt", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    url="",
+    packages=find_packages(),  # Automatically finds all packages in your project
+    install_requires=[
+        # List your dependencies here, e.g.:
+        # "requests>=2.25.1",
+    ],
+    python_requires='>=3.6',
+)
