982aa53823782857947bb41adee385bfaf3f5655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 04:08:12 +0000

    Add package autisticstuff/0.1.4b1/autisticstuff-0.1.4b1-py3-none-any.whl

---
+++ packages/autisticstuff/autisticstuff-0.1.4b1-py3-none-any.whl/autisticstuff/__init__.py
@@ -0,0 +1,8 @@
+"""
+autisticstuff
+-> fastapi: respsys (unified response system and exc handler, APIExceptions and more), utils
+-> logs: logging system with interception
+-> safe: RetryableClientSession(aiohttp.ClientSession), safe aiogram funcs
+-> sqla: layers (BaseMapping, BaseRepository), types (PydanticJSON), utils (update_with_callback, validate_kwargs_for_model)
+-> utils: config (AppSettings, SettingsField), enums (for URS), lru (lru-cache wrappers for objects), progbars
+"""
