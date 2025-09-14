4e1c34806892a6f53d075a6cbcd256de7653b8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:06:12 +0000

    Add package django-enhanced-pagination/1.0.7/django_enhanced_pagination-1.0.7-py3-none-any.whl

---
+++ packages/django-enhanced-pagination/django_enhanced_pagination-1.0.7-py3-none-any.whl/django_enhanced_pagination/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoEnhancedPaginationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_enhanced_pagination'
+    verbose_name = 'Django Enhanced Pagination'
