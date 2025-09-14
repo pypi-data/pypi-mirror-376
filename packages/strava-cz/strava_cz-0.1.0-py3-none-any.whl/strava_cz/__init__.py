61479cd8d520ce6dec22501087709c4c34b61741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:05:45 +0000

    Add package strava-cz/0.1.0/strava_cz-0.1.0-py3-none-any.whl

---
+++ packages/strava-cz/strava_cz-0.1.0-py3-none-any.whl/strava_cz/__init__.py
@@ -0,0 +1,12 @@
+"""
+StravaCZ - High level API pro interakci s webovou aplikaci Strava.cz
+"""
+
+from .main import StravaCZ
+
+__version__ = "0.1.0"
+__author__ = "Vojtěch Nerad"
+__email__ = "ja@jsem-nerad.cz"
+
+# Public API
+__all__ = ["StravaCZ"]
