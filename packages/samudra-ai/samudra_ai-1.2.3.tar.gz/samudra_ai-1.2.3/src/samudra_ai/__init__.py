25124d14240e3a76319affe46042c9f7c2980227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:18:43 +0000

    Add package samudra-ai/1.2.3/samudra_ai-1.2.3.tar.gz

---
+++ packages/samudra-ai/samudra_ai-1.2.3.tar.gz/samudra_ai-1.2.3/src/samudra_ai/__init__.py
@@ -0,0 +1,16 @@
+# File: src/samudra_ai/__init__.py
+from .core import SamudraAI
+from .data_loader import load_and_mask_dataset
+from .trainer import prepare_training_data, plot_training_history
+from .evaluator import evaluate_model
+from .preprocess_dcpp import preprocess_dcpp
+
+__version__ = "1.2.3"
+__all__ = [
+    'SamudraAI',
+    'load_and_mask_dataset',
+    'prepare_training_data',
+    'plot_training_history',
+    'evaluate_model',
+    "preprocess_dcpp",
+]
