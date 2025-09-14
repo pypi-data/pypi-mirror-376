23b0e40be7c76d2f7d83813bc809c369f0528367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:16 +0000

    Add package limoon/0.0.12/limoon-0.0.12.tar.gz

---
+++ packages/limoon/limoon-0.0.12.tar.gz/limoon-0.0.12/src/limoon/utils.py
@@ -0,0 +1,23 @@
+from typing import Iterator
+
+from requests_html import HTML
+
+from . import model
+
+
+def entry_parser(html: HTML, max_entry: int = None) -> Iterator[model.Entry]:
+    entry_items = html.find("ul#entry-item-list", first=True).find("li#entry-item")
+
+    for item in entry_items[:max_entry]:
+        author = item.find("a.entry-author", first=True)
+        content = item.find("div.content", first=True)
+        favorite_count = item.attrs["data-favorite-count"]
+        created = item.find("a.entry-date", first=True)
+
+        yield model.Entry(
+            int(item.attrs["data-id"]),
+            author.text,
+            content.text,
+            int(favorite_count),
+            created.text,
+        )
