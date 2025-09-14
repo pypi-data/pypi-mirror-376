b9ef4355ca033f0bdc5f4c3fa3f5405e536b8887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:18:41 +0000

    Add package TemporalVAE/1.0.1/temporalvae-1.0.1.tar.gz

---
+++ packages/TemporalVAE/temporalvae-1.0.1.tar.gz/temporalvae-1.0.1/TemporalVAE/__init__.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8 -*-
+"""
+@Project ：TemporalVAE 
+@File    ：__init__.py.py
+@Author  ：awa121
+@Date    ：2025/6/9 22:16 
+
+Description: 
+"""
+
+from . import tools as tl
+from . import datasets, utils
+from .version import __version__
