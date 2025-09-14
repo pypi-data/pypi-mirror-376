2b7f5cb7ebb8d8fce41731b61fbdfd0f360d8c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:35 +0000

    Add package lemonade/1.0b2/lemonade-1.0b2.tar.gz

---
+++ packages/lemonade/lemonade-1.0b2.tar.gz/lemonade-1.0b2/setup.py
@@ -0,0 +1,24 @@
+
+from distutils.core import setup
+
+setup(name = 'lemonade',
+      version = '1.0b2',
+      description = 'Port of the LEMON Parser Generator',
+      repository_urlx = 'https://github.com/leifboo/lemonade',
+
+      scripts = ['bin/lemonade'],
+      packages = ['lemonade'],
+      package_data = { 'lemonade': ['lempar.tmpl'] },
+      
+      classifiers = [
+          'License :: Public Domain',
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Code Generators',
+          'Topic :: Software Development :: Compilers',
+          ],
+      
+      author = 'Leif Strand',
+      author_email = 'leif.c.strand@gmail.com',
+      )
