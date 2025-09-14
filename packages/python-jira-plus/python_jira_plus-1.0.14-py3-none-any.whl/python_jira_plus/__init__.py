0e77c99c89b9e6e0e44f835db76084aad9037812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:39 +0000

    Add package python-jira-plus/1.0.14/python_jira_plus-1.0.14-py3-none-any.whl

---
+++ packages/python-jira-plus/python_jira_plus-1.0.14-py3-none-any.whl/python_jira_plus/__init__.py
@@ -0,0 +1,33 @@
+# python-jira-plus
+
+"""
+Python JIRA Plus
+
+An enhanced Python client for JIRA with better error handling,
+pagination, metadata validation, and more.
+"""
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from python_jira_plus.jira_plus import JiraPlus, ServerType
+from python_jira_plus.describe_allowed_value import describe_allowed_value
+
+load_dotenv()
+
+BASIC_FIELDS = [
+    'created',
+    'updated',
+    'issuetype',
+    'project',
+    'status',
+    'summary',
+    'description',
+    'reporter',
+    'assignee',
+    'priority',
+    'labels',
+    'resolution',
+    'parent'
+]
+__all__ = ['JiraPlus', 'ServerType', 'BASIC_FIELDS']
