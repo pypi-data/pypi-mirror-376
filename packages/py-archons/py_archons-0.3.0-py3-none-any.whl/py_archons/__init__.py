549857c5e5780334659112c309ce8519852339e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 14:06:08 +0000

    Add package py-archons/0.3.0/py_archons-0.3.0-py3-none-any.whl

---
+++ packages/py-archons/py_archons-0.3.0-py3-none-any.whl/py_archons/__init__.py
@@ -0,0 +1,6 @@
+"""Py-Archons: A simple Python package."""
+
+from py_archons.core import get_names, get_cities, get_countries
+
+__version__ = "0.3.0"
+__all__ = ["get_names", "get_cities", "get_countries"]
---
+++ packages/py-archons/py_archons-0.3.0-py3-none-any.whl/py_archons/core.py
@@ -0,0 +1,30 @@
+"""Core functionality for py-archons package."""
+
+
+def get_names():
+    """
+    Return an array of names.
+    
+    Returns:
+        list[str]: A list containing "guapo" and "bandida".
+    """
+    return ["guapo", "bandida"]
+
+
+def get_cities():
+    """
+    Return an array of cities.
+    
+    Returns:
+        list[str]: A list containing "new york" and "london".
+    """
+    return ["new york", "london"]
+
+def get_countries():
+    """
+    Return an array of countries.
+    
+    Returns:
+        list[str]: A list containing "united states" and "united kingdom".
+    """
+    return ["united states", "united kingdom"]
