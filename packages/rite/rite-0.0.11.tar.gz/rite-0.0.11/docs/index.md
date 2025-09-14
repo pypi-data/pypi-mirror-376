be45723fb7d19969cc889558916dc19e993a74a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:36 +0000

    Add package rite/0.0.11/rite-0.0.11.tar.gz

---
+++ packages/rite/rite-0.0.11.tar.gz/rite-0.0.11/docs/index.md
@@ -0,0 +1,23 @@
+<header>
+<p align="center">
+    <img src=".github/images/rite_logo.png" width="20%" height="20%" alt="rite Logo">
+</p>
+<h3 align='center'>Python Utility Package</h3>
+</header>
+
+<br/>
+
+---
+
+**rite** is a Python Utility Package.
+
+---
+
+**rite** is an open-source project by [Scape Agency](https://www.scape.agency "Scape Agency website").
+
+## Scape Agency
+
+Scape Agency is a spatial innovation collective that dreams, discovers and designs the everyday of tomorrow. We blend design thinking with emerging technologies to create a brighter perspective for people and planet. Our products and services naturalise technology in liveable and sustainable –scapes that spark the imagination and inspire future generations.
+
+- website: [scape.agency](https://www.scape.agency "Scape Agency website")
+- github: [github.com/scape-agency](https://github.com/scape-agency "Scape Agency Github")
---
+++ packages/rite/rite-0.0.11.tar.gz/rite-0.0.11/src/rite/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides Rite package information
+=================================
+
+
+
+"""
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Future
+from __future__ import annotations
+
+# Import | Standard Library
+
+# Import | Libraries
+
+# Import | Local Modules
+
+
+# =============================================================================
+# Information
+# =============================================================================
+
+__author__ = "Lars van Vianen"
+__copyright__ = "Copyright 2022, Scape Agency"
+__credits__ = [
+    "Lars van Vianen",
+]
+__license__ = "MIT License"
+__version__ = "0.0.11"
+__maintainer__ = "Scape Agency"
+__email__ = "info@scape.agency"
+__status__ = "Alpha"
