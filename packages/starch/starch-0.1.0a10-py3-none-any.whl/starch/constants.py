fbcd746494fdba42cdc3d16d19e3fa559df35dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:33 +0000

    Add package starch/0.1.0a10/starch-0.1.0a10-py3-none-any.whl

---
+++ packages/starch/starch-0.1.0a10-py3-none-any.whl/starch/constants.py
@@ -0,0 +1,39 @@
+"""
+Constants
+=========
+
+This module contains constants used throughout the Starch code package.
+
+"""
+
+from pathlib import Path
+from typing import Final
+
+from platformdirs import (
+    user_cache_path,
+    user_config_path,
+    user_data_path,
+    user_log_path
+)
+
+
+# ─── package metadata ───────────────────────────────────────────────────────────── ✦ ─
+#
+__author__ = "Caleb Rice"
+__email__ = "hyletic@proton.me"
+__license__ = "MIT"
+
+# VERSION_CONFIG: {"base":"0.1.0","phase":"a","build":10}
+__version__ = "0.1.0a10"
+
+    
+# ─── constants ──────────────────────────────────────────────────────────────────── ✦ ─
+#
+PACKAGE_NAME: Final[str] = __package__.split(".")[-1]
+STARCH_CACHE_PATH: Final[Path] = user_cache_path(PACKAGE_NAME, ensure_exists=True)
+STARCH_CONFIG_PATH: Final[Path] = user_config_path(PACKAGE_NAME, ensure_exists=True)
+STARCH_DATA_PATH: Final[Path] = user_data_path(PACKAGE_NAME, ensure_exists=True)
+STARCH_LOG_PATH: Final[Path] = user_log_path(PACKAGE_NAME, ensure_exists=True)
+STARCH_CONFIG_FILEPATH: Final[Path] = STARCH_CONFIG_PATH / "config.json"
+STARCH_LOG_FILEPATH: Final[Path] = STARCH_LOG_PATH / f"{__package__}.log"
+
