81ae27099c9e4be69c6c7d4f73154dd548ba1a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 14:06:27 +0000

    Add package pysigil/0.5.0/pysigil-0.5.0.tar.gz

---
+++ packages/pysigil/pysigil-0.5.0.tar.gz/pysigil-0.5.0/src/pysigil/__init__.py
@@ -0,0 +1,20 @@
+from .core import Sigil, SigilError
+from .merge_policy import parse_key
+from .policy import policy
+from .toolkit import helpers_for
+
+
+# Toggle visibility of machine-specific scopes in the UI.  When ``False``
+# machine scopes such as ``user-local`` and ``project-local`` are hidden.
+show_machine_scope = False
+
+
+__all__ = [
+    "Sigil",
+    "SigilError",
+    "parse_key",
+    "policy",
+    "helpers_for",
+    "show_machine_scope",
+
+]
