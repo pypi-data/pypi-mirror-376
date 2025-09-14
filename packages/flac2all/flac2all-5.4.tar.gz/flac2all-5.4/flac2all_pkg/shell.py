e63b6a925eef43431f43d92c060ecb5b401c2676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:05 +0000

    Add package flac2all/5.4/flac2all-5.4.tar.gz

---
+++ packages/flac2all/flac2all-5.4.tar.gz/flac2all-5.4/flac2all_pkg/shell.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# vim ts=4 expandtab si
+import os
+
+from shutil import copyfile
+
+
+class filecopy:
+	def __init__(self, opts):
+		self.opts = opts
+
+	def convert(self, infile, outfile):
+		# The outfile will contain "_copy" as a folder, we
+		# use that as a placeholder which we replace with the
+		# current given mode here
+		outfile = outfile.replace("_copy", self.opts['copymode'])
+		try:
+			# First we check if the directory exists, and if it doesn't
+			# Create it
+			copydir = outfile.rsplit('/', 1)[0]
+			if not os.path.exists(copydir):
+				os.makedirs(copydir)
+			copyfile(infile, outfile)
+		except Exception as e:
+			return [
+				infile,
+				outfile,
+				"_copy",
+				"Error: %s" % str(e),
+				-1,
+				-1
+			]
+		return [
+			infile,
+			outfile,
+			"_copy",
+			"SUCCESS",
+			0,
+			-1
+		]
+
+
+class shell:
+	def generateoutdir(self, indir, outdir, dirpath):
+		# if we find the dirpath in the current output path, we replace
+		# it with the new output path. (so that we don't get
+		# /mnt/convertedfromflac/mnt/flac/[file].mp3, in this case
+		# "/mnt/" exist in both)
+		if (str.find(os.path.split(indir)[0], dirpath) != -1):
+			return os.path.split(indir)[0].replace(dirpath, outdir)
+		else:
+			# if we do not find an instance of dir path in output
+			# path (this shows that they are the same), just
+			# return the output
+			return outdir
+
+	def parse_escape_chars(self, input_file, quoteonly=False):
+		if quoteonly:
+			# characters which must be escaped in the shell, note
+			# "[" and "]" seems to be automatically escaped
+			# (strange, look into this)
+			escChars = ["\"", "*", ";", " ", "'", "(", ")", "&", "`"]
+
+			for char in escChars:
+				# add an escape character to the character
+				input_file = input_file.replace(char, '\\' + char)
+		else:
+			input_file = input_file.replace("\"", "\\\"")
+
+		return input_file
+
+	def getfiles(self, path):
+		outfiles = []
+		for root, dirs, files in os.walk(path):
+			for infile in files:
+				outfiles.append(
+					os.path.abspath(
+						os.path.join(root, infile)
+					)
+				)
+		return outfiles
---
+++ packages/flac2all/flac2all-5.4.tar.gz/flac2all-5.4/flac2all_pkg/vorbis.py
@@ -0,0 +1,62 @@
+# vim: ts=4 autoindent noexpandtab
+from time import time
+import subprocess as sp
+
+if __name__ == '__main__' and __package__ is None:
+	from os import path, sys
+	sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+try:
+	from config import ipath
+except ImportError:
+	from .config import ipath
+
+
+# Class that deals with vorbis
+class vorbis:
+	def __init__(self, opts):
+		self.opts = [x for x in opts.split(' ') if x.strip() != ""]
+
+	def convert(self, infile, outfile):
+		# oggenc automatically parses the flac file + metadata, quite wonderful
+		# really, we don't need to do anything here
+		# The binary itself deals with the tag conversion etc
+		# Which makes our life rather easy
+		startTime = time()
+		outfile = "%s.ogg" % outfile.strip()
+
+		cmd = [
+			"%soggenc" % ipath.oggencpath,
+			"-Q",
+			"-o",
+			outfile,
+		]
+		if len(self.opts) != 0:
+			cmd.extend(self.opts)
+
+		cmd.append(infile)
+		rc = -1
+		codec = sp.Popen(cmd, stderr=sp.PIPE, stdout=sp.PIPE)
+		stdout, stderr = codec.communicate()
+		errline = stderr.decode('utf-8')
+		errline = errline.upper()
+		rc = codec.returncode
+
+		if rc != 0:
+			return [
+				infile,
+				outfile,
+				"vorbis",
+				"ERROR: %s" % errline,
+				-1,
+				time() - startTime
+			]
+
+		return [
+			infile,
+			outfile,
+			"vorbis",
+			"SUCCESS",
+			rc,
+			time() - startTime
+		]
