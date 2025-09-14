25851cd9789859d3bb946d1f5bd5e2f70d3b5ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:09 +0000

    Add package ingredient-parser-nlp/2.3.0/ingredient_parser_nlp-2.3.0.tar.gz

---
+++ packages/ingredient-parser-nlp/ingredient_parser_nlp-2.3.0.tar.gz/ingredient_parser_nlp-2.3.0/ingredient_parser/__init__.py
@@ -0,0 +1,12 @@
+from ._common import SUPPORTED_LANGUAGES, show_model_card
+from .parsers import inspect_parser, parse_ingredient, parse_multiple_ingredients
+
+__all__ = [
+    "SUPPORTED_LANGUAGES",
+    "inspect_parser",
+    "parse_ingredient",
+    "parse_multiple_ingredients",
+    "show_model_card",
+]
+
+__version__ = "2.3.0"
