465b3037336785a527a8e3a6745ff84a62b7be6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 04:06:51 +0000

    Add package py_log_mjk/0.1.3/py_log_mjk-0.1.3-py3-none-any.whl

---
+++ packages/py_log_mjk/py_log_mjk-0.1.3-py3-none-any.whl/py_log_mjk/config_logging.py
@@ -0,0 +1,111 @@
+import atexit
+import json
+import logging
+import importlib.resources
+from logging.config import dictConfig
+from logging.handlers import QueueHandler, QueueListener
+
+from src.py_log_mjk.settings import LogLevel, settings
+
+_setup_logging_done: bool = False
+_default_queue_listener: QueueListener | None = None
+
+_logger = logging.getLogger(settings.setup_logger_name)
+_logger.setLevel(settings.setup_logger_level)
+
+
+def _get_logging_config() -> dict:
+    """Tenta carregar a configuração de logging do caminho especificado."""
+    # 1. Tenta carregar do caminho do template
+    config_file_path = None
+    try:
+        package = importlib.resources.files('py_log_mjk.templates')
+        config_file_path = package / settings.logging_config_json_path.name
+        with importlib.resources.as_file(config_file_path) as path:
+            with open(path, 'r', encoding='utf-8') as file:
+                return json.load(file)
+    except (FileNotFoundError, AttributeError):
+        # 2. Se falhar, tenta carregar do caminho absoluto (definido via .env)
+        if settings.logging_config_json_path.is_file():
+            with settings.logging_config_json_path.open("r", encoding="utf-8") as file:
+                return json.load(file)
+        
+        msg = (
+            f"Logging config file not found. Expected at "
+            f"'{settings.logging_config_json_path}' or in the library's templates."
+        )
+        _logger.error(msg)
+        raise FileNotFoundError(msg)
+
+
+def _setup_queue_listener() -> None:
+    """Inicia o QueueListener e registra a função de parada."""
+    global _default_queue_listener
+    
+    queue_handlers = [
+        handler for handler in logging.getLogger().handlers if isinstance(handler, QueueHandler)
+    ]
+    
+    if len(queue_handlers) > 1:
+        msg = "This function does not allow more than one QueueHandler"
+        raise RuntimeError(msg)
+        
+    if not queue_handlers:
+        return
+
+    queue_handler = queue_handlers[0]
+    _logger.debug("Found QueueHandler with name: '%s'", queue_handler.name)
+    _default_queue_listener = queue_handler.listener
+    
+    if _default_queue_listener:
+        _default_queue_listener.start()
+        atexit.register(_stop_queue_listener)
+        _logger.debug("QueueListener started and registered with atexit.")
+
+
+def _stop_queue_listener() -> None:
+    """Para o QueueListener de forma controlada."""
+    if _default_queue_listener:
+        _logger.debug("Stopping listener...")
+        _default_queue_listener.stop()
+
+
+def _configure_logging() -> None:
+    """Configura o sistema de logging do Python."""
+    global _setup_logging_done
+    
+    if _setup_logging_done:
+        _logger.debug("Logging already configured.")
+        return
+
+    settings.validate_paths()
+
+    try:
+        config = _get_logging_config()
+        dictConfig(config)
+        _logger.debug("JSON config file loaded and applied.")
+        _setup_queue_listener()
+        _setup_logging_done = True
+    except Exception as e:
+        _logger.exception("Failed to configure logging. Using basic config.")
+        logging.basicConfig(level=settings.default_logger_level)
+        _setup_logging_done = True
+
+
+def get_logger(name: str = __name__, level: LogLevel | None = None) -> logging.Logger:
+    """
+    Obtém um logger configurado. Se o logging ainda não foi configurado,
+    configura-o automaticamente.
+    """
+    _configure_logging()
+    
+    logger = logging.getLogger(name)
+    
+    if level is not None:
+        logger.setLevel(settings._validate_level(level))
+        _logger.debug(f"Level '{level}' set for logger '{name}'.")
+    else:
+        logger.setLevel(settings.default_logger_level)
+        _logger.debug(f"Default level '{settings.default_logger_level}' set for logger '{name}'.")
+        
+    return logger
