45a1b556802f190906ba5b3d4587fe846a7e6862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:05:59 +0000

    Add package streamlit-japanese-date-input/0.1.5/streamlit_japanese_date_input-0.1.5.tar.gz

---
+++ packages/streamlit-japanese-date-input/streamlit_japanese_date_input-0.1.5.tar.gz/streamlit_japanese_date_input-0.1.5/setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import setuptools
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+setuptools.setup(
+    name="streamlit-japanese-date-input",
+    version="0.1.5",
+    author="Ayumu Yamaguchi",
+    author_email="",
+    description="Streamlit component for date input with Japanese localization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gussan-me/streamlit_japanese_date_input",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 1.28.0",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.48.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    }
+)
