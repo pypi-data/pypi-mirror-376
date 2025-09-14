98fd22ba85469010dbf0a4a4c068623a56f76684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:33 +0000

    Add package albionSmallLibs/0.9.4/albionsmalllibs-0.9.4.tar.gz

---
+++ packages/albionSmallLibs/albionsmalllibs-0.9.4.tar.gz/albionsmalllibs-0.9.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="albionSmallLibs",
+    version="0.9.4",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/albionSmallLibs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
