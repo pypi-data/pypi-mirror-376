110abe9060a431a6a3416a7de84ed92d952364b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:30 +0000

    Add package optima-vis/1.4.0/optima_vis-1.4.0-py3-none-any.whl

---
+++ packages/optima-vis/optima_vis-1.4.0-py3-none-any.whl/optima_vis/__init__.py
@@ -0,0 +1,8 @@
+from optima_vis.models.gif_properties import GifProperties
+from optima_vis.models.video_properties import VideoProperties
+from optima_vis.services.animation_generator.optimizer_animation_generator import (
+    OptimizerAnimationGenerator,
+)
+from optima_vis.services.plot_generator.loss_contour_generator import (
+    LossContourGenerator,
+)
