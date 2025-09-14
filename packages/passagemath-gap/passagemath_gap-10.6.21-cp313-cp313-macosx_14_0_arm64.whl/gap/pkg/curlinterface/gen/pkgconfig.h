eb4b7827e98d18442da4309ad2225857ffd5ad97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 04:06:32 +0000

    Add package passagemath-gap/10.6.21/passagemath_gap-10.6.21-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/passagemath-gap/passagemath_gap-10.6.21-cp313-cp313-macosx_14_0_arm64.whl/gap/pkg/curlinterface/gen/pkgconfig.h
@@ -0,0 +1,89 @@
+/* gen/pkgconfig.h.  Generated from pkgconfig.h.in by configure.  */
+/* src/pkgconfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have a functional curl library. */
+#define HAVE_LIBCURL 1
+
+/* Defined if libcurl supports AsynchDNS */
+#define LIBCURL_FEATURE_ASYNCHDNS 1
+
+/* Defined if libcurl supports IDN */
+/* #undef LIBCURL_FEATURE_IDN */
+
+/* Defined if libcurl supports IPv6 */
+/* #undef LIBCURL_FEATURE_IPV6 */
+
+/* Defined if libcurl supports KRB4 */
+/* #undef LIBCURL_FEATURE_KRB4 */
+
+/* Defined if libcurl supports libz */
+#define LIBCURL_FEATURE_LIBZ 1
+
+/* Defined if libcurl supports NTLM */
+#define LIBCURL_FEATURE_NTLM 1
+
+/* Defined if libcurl supports SSL */
+#define LIBCURL_FEATURE_SSL 1
+
+/* Defined if libcurl supports SSPI */
+/* #undef LIBCURL_FEATURE_SSPI */
+
+/* Defined if libcurl supports DICT */
+#define LIBCURL_PROTOCOL_DICT 1
+
+/* Defined if libcurl supports FILE */
+#define LIBCURL_PROTOCOL_FILE 1
+
+/* Defined if libcurl supports FTP */
+#define LIBCURL_PROTOCOL_FTP 1
+
+/* Defined if libcurl supports FTPS */
+#define LIBCURL_PROTOCOL_FTPS 1
+
+/* Defined if libcurl supports HTTP */
+#define LIBCURL_PROTOCOL_HTTP 1
+
+/* Defined if libcurl supports HTTPS */
+#define LIBCURL_PROTOCOL_HTTPS 1
+
+/* Defined if libcurl supports IMAP */
+#define LIBCURL_PROTOCOL_IMAP 1
+
+/* Defined if libcurl supports LDAP */
+#define LIBCURL_PROTOCOL_LDAP 1
+
+/* Defined if libcurl supports POP3 */
+#define LIBCURL_PROTOCOL_POP3 1
+
+/* Defined if libcurl supports RTSP */
+#define LIBCURL_PROTOCOL_RTSP 1
+
+/* Defined if libcurl supports SMTP */
+#define LIBCURL_PROTOCOL_SMTP 1
+
+/* Defined if libcurl supports TELNET */
+#define LIBCURL_PROTOCOL_TELNET 1
+
+/* Defined if libcurl supports TFTP */
+#define LIBCURL_PROTOCOL_TFTP 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "curlInterface"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "curlInterface GAP package"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "curlinterface"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "GAP package"
+
+/* Define curl_free() as free() if our version of curl lacks curl_free. */
+/* #undef curl_free */
