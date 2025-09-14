c3d48bd9bf1228957fedbb5e612be6d4ba192907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 14:06:01 +0000

    Add package sekaibot-plugin-apscheduler/0.1.0/sekaibot_plugin_apscheduler-0.1.0.tar.gz

---
+++ packages/sekaibot-plugin-apscheduler/sekaibot_plugin_apscheduler-0.1.0.tar.gz/sekaibot_plugin_apscheduler-0.1.0/sekaibot/plugins/apscheduler/config.py
@@ -0,0 +1,26 @@
+"""SekaiBot APScheduler 插件配置"""
+
+from typing import Any
+
+from pydantic import Field
+
+from sekaibot.config import ConfigModel
+
+
+class APSchedulerConfig(ConfigModel):
+    """Scheduler 配置模型。
+
+    Attributes:
+        __config_name__: 配置名称。
+        apscheduler_autostart: 是否自动启动调度器。
+        apscheduler_log_level: APScheduler 日志级别。
+        apscheduler_config: APScheduler 配置。
+    """
+
+    __config_name__: str = "scheduler"
+
+    apscheduler_autostart: bool = True
+    apscheduler_log_level: int = 30
+    apscheduler_config: dict[Any, Any] = Field(
+        default_factory=lambda: {"apscheduler.timezone": "Asia/Shanghai"}
+    )
