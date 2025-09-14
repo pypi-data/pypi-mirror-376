cd9637f4fd282d919d9989dba7cb3a310e06b096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 14:06:07 +0000

    Add package py-archons/0.2.0/py_archons-0.2.0-py3-none-any.whl

---
+++ packages/py-archons/py_archons-0.2.0-py3-none-any.whl/py_archons/core.py
@@ -0,0 +1,21 @@
+"""Core functionality for py-archons package."""
+
+
+def get_names():
+    """
+    Return an array of names.
+    
+    Returns:
+        list[str]: A list containing "guapo" and "bandida".
+    """
+    return ["guapo", "bandida"]
+
+
+def get_cities():
+    """
+    Return an array of cities.
+    
+    Returns:
+        list[str]: A list containing "new york" and "london".
+    """
+    return ["new york", "london"]
