4a448f057c3ae8c9f9f0e38985fbf0c2a93d7f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:45 +0000

    Add package pycapnp/2.2.0/pycapnp-2.2.0.tar.gz

---
+++ packages/pycapnp/pycapnp-2.2.0.tar.gz/pycapnp-2.2.0/test/test_blob_to_dict_base64.py
@@ -0,0 +1,20 @@
+import os
+import capnp
+import pytest
+
+this_dir = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def blob_schema():
+    return capnp.load(os.path.join(this_dir, "blob_test.capnp"))
+
+
+def test_blob_to_dict(blob_schema):
+    blob_value = b"hello world"
+    blob = blob_schema.BlobTest(blob=blob_value)
+    blob_dict = blob.to_dict(encode_bytes_as_base64=True)
+    assert blob_dict["blob"].tobytes() == blob_value
+    msg = blob_schema.BlobTest.new_message()
+    msg.from_dict(blob_dict)
+    assert blob.blob == blob_value
