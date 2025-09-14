da82df68b79b557f12e8afd1840e64fee158d9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:46 +0000

    Add package biased/0.1.2.dev1/biased-0.1.2.dev1-py3-none-any.whl

---
+++ packages/biased/biased-0.1.2.dev1-py3-none-any.whl/biased/dtos/logging.py
@@ -0,0 +1,17 @@
+import logging
+from enum import StrEnum, auto
+
+from biased.dtos.base import BaseDto
+from biased.types import LogLevel
+
+
+class LogFormatter(StrEnum):
+    json = auto()
+    human = auto()
+
+
+class LoggingParams(BaseDto):
+    level: LogLevel = logging.INFO
+    formatter: LogFormatter = LogFormatter.json
+    levels: dict[str, LogLevel] = {}
+    colors: bool = True
