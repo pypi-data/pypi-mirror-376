4ce5cd841a9d1abd1c4bca8c23b32f42aa7028c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:44 +0000

    Add package achatbot/0.0.25.post1/achatbot-0.0.25.post1-py3-none-any.whl

---
+++ packages/achatbot/achatbot-0.0.25.post1-py3-none-any.whl/achatbot/cmd/bots/dummy_bot.py
@@ -0,0 +1,20 @@
+import os
+import logging
+import asyncio
+
+from achatbot.cmd.bots.base import AIBot
+from achatbot.common.interface import IBot
+from . import register_ai_room_bots
+
+
+@register_ai_room_bots.register
+class DummyBot(AIBot):
+    def __init__(self, **args) -> None:
+        super().__init__(**args)
+        self.init_bot_config()
+
+    async def arun(self):
+        logging.info(f"Starting bot,env: {os.environ}")
+        logging.info(f"dummy bot run with args: {self.args}")
+        await asyncio.sleep(10)
+        logging.info("dummy bot over")
