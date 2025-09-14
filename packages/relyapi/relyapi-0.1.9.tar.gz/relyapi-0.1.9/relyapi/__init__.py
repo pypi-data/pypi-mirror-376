7ebf29f3c2b5b645be6826bb25fdf0527fc24483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:02 +0000

    Add package relyapi/0.1.9/relyapi-0.1.9.tar.gz

---
+++ packages/relyapi/relyapi-0.1.9.tar.gz/relyapi-0.1.9/relyapi/__init__.py
@@ -0,0 +1,8 @@
+from relyapi.client import Client
+from relyapi.plugin import BasePlugin, BypassType
+
+__all__ = [
+    'Client',
+    'BypassType',
+    'BasePlugin',
+]
---
+++ packages/relyapi/relyapi-0.1.9.tar.gz/relyapi-0.1.9/relyapi/transport.py
@@ -0,0 +1,56 @@
+import os
+
+import httpx
+
+RELY_API_ADDRESS = os.environ.get("RELY_API_ADDRESS", 'https://forwrad.relyapi.com')
+
+
+class ForwardingTransport(httpx.BaseTransport):
+    def __init__(self):
+        self._client = httpx.Client(base_url=RELY_API_ADDRESS)
+
+    def handle_request(self, request: httpx.Request) -> httpx.Response:
+        payload = {
+            "method": request.method,
+            "url": str(request.url),
+            "headers": dict(request.headers),
+            "body": request.read().decode("utf-8") if request.content else None
+        }
+
+        forward_resp = self._client.post("/rely/worker/bypass/forward", json=payload)
+
+        return httpx.Response(
+            status_code=forward_resp.status_code,
+            headers=forward_resp.headers,
+            content=forward_resp.content,
+            request=request
+        )
+
+    def aclose(self):
+        self._client.close()
+
+
+class AsyncForwardingTransport(httpx.AsyncBaseTransport):
+    def __init__(self):
+        self._client = httpx.AsyncClient(base_url=RELY_API_ADDRESS)
+
+    async def handle_async_request(self, request: httpx.Request) -> httpx.Response:
+        payload = {
+            "method": request.method,
+            "url": str(request.url),
+            "headers": dict(request.headers),
+            "body": await request.aread()
+        }
+
+        forward_resp = await self._client.post(
+            "/rely/worker/bypass/forward", json=payload
+        )
+        return httpx.Response(
+            status_code=forward_resp.status_code,
+            headers=forward_resp.headers,
+            content=forward_resp.content,
+            request=request
+        )
+
+    async def aclose(self):
+        await self._client.aclose()
