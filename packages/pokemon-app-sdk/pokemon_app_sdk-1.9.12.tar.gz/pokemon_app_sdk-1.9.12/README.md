3198a9bebdd33903e0e7947ac2a2415276522ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 05:05:54 +0000

    Add package pokemon-app-sdk/1.9.12/pokemon_app_sdk-1.9.12.tar.gz

---
+++ packages/pokemon-app-sdk/pokemon_app_sdk-1.9.12.tar.gz/pokemon_app_sdk-1.9.12/README.md
@@ -0,0 +1,1 @@
+# pokemon_app_sdk\nA dummy Python package version of pokemon_app_sdk.
---
+++ packages/pokemon-app-sdk/pokemon_app_sdk-1.9.12.tar.gz/pokemon_app_sdk-1.9.12/src/ca_certificates/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.-"
+
+def info():
+    return "This is a dummy pokemon_app_sdk package for PyPI."
