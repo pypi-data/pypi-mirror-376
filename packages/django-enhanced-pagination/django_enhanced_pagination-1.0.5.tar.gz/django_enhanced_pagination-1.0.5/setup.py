09f47de1334c2e655d2793cd5ae4d9051cf9b719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:06:57 +0000

    Add package django-enhanced-pagination/1.0.5/django_enhanced_pagination-1.0.5.tar.gz

---
+++ packages/django-enhanced-pagination/django_enhanced_pagination-1.0.5.tar.gz/django_enhanced_pagination-1.0.5/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-enhanced-pagination',
+    version='1.0.5',
+    description='An enhanced version of django-pure-pagination with additional features and improvements',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/django-enhanced-pagination',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'django_enhanced_pagination': [
+            'pagination/templates/pure_pagination/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='django pagination paginator pure enhanced',
+    install_requires=[
+        'Django>=2.2',
+    ],
+    python_requires='>=3.6',
+    license='BSD',
+    zip_safe=False,
+)
