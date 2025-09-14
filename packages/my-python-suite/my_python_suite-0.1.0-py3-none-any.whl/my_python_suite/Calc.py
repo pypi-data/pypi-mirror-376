7594e9e4a0c7444e1ee4cd0379864f89f92dab2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:19:18 +0000

    Add package my-python-suite/0.1.0/my_python_suite-0.1.0-py3-none-any.whl

---
+++ packages/my-python-suite/my_python_suite-0.1.0-py3-none-any.whl/my_python_suite/Calc.py
@@ -0,0 +1,27 @@
+# Calaulator Operation in Python
+def additon(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+def exponentiation(a,b):
+    return a**b
+
+def modulus(a,b):
+    return a%b
+
+def floor_division(a,b):
+    return a//b
+
+def Absolute_Value(a):
+    return abs(a)
+
+def sqaure_root(a):
+    return a**0.5
---
+++ packages/my-python-suite/my_python_suite-0.1.0-py3-none-any.whl/my_python_suite/__init__.py
@@ -0,0 +1,9 @@
+# python_suite/__init__.py
+
+from . import Calc
+from . import unitconvert
+from . import funfact
+
+__version__ = "0.1.0"
+
+__all__ = ["calc", "unitconvert", "funfact"]
