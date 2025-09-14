e623f38e70ef814541d4d3759d0a0de4407a1ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:06:29 +0000

    Add package mantis-web-crawler/1.1.1/mantis_web_crawler-1.1.1.tar.gz

---
+++ packages/mantis-web-crawler/mantis_web_crawler-1.1.1.tar.gz/mantis_web_crawler-1.1.1/src/inspector/__init__.py
@@ -0,0 +1,9 @@
+from inspector.main import Inspector, get_inspector
+from inspector.checks.structured_explorer import StructuredExplorer
+
+
+__all__ = [
+    'Inspector',
+    'get_inspector',
+    'StructuredExplorer'
+]
