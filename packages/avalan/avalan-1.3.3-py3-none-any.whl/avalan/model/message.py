b4613cbb96049498fa54e5ba17b271b105523fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 03:18:45 +0000

    Add package avalan/1.3.3/avalan-1.3.3-py3-none-any.whl

---
+++ packages/avalan/avalan-1.3.3-py3-none-any.whl/avalan/model/message.py
@@ -0,0 +1,14 @@
+from typing import Literal, TypedDict
+
+TemplateMessageRole = Literal["assistant", "system", "tool", "user"]
+
+
+class TemplateMessageContent(TypedDict, total=False):
+    type: Literal["image_url", "text"]
+    text: str | None
+    image_url: dict[str, str] | None
+
+
+class TemplateMessage(TypedDict):
+    role: TemplateMessageRole
+    content: str | TemplateMessageContent | list[TemplateMessageContent]
