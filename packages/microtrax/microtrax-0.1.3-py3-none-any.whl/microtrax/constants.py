d32e6235912803bb034374686ab47d90ca9e6989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-14 15:06:46 +0000

    Add package microtrax/0.1.3/microtrax-0.1.3-py3-none-any.whl

---
+++ packages/microtrax/microtrax-0.1.3-py3-none-any.whl/microtrax/constants.py
@@ -0,0 +1,18 @@
+import os
+
+MTX_GLOBALDIR_NAME = "~/.microtrax"
+MTX_GLOBALDIR = os.path.expanduser(MTX_GLOBALDIR_NAME)
+
+# Directory names
+EXPERIMENTS_DIR = "experiments"
+RESOURCES_DIR = "resources"
+
+# Metric lists
+RESOURCE_METRICS = [
+    'cpu_percent',
+    'memory_percent',
+    'memory_used_mb',
+    'gpu_utilization_percent',
+    'gpu_memory_percent',
+    'gpu_memory_used_mb'
+]
