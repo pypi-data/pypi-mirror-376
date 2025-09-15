b4a016de60e60b979beba4ee636fb37f5461a948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 16:05:49 +0000

    Add package injx/0.1.0/injx-0.1.0.tar.gz

---
+++ packages/injx/injx-0.1.0.tar.gz/injx-0.1.0/src/injx/_metadata.py
@@ -0,0 +1,7 @@
+"""Metadata constants for injx package."""
+
+__org__ = "QriusGlobal"
+__author__ = "Qrius Global"
+__email__ = "mishal@qrius.global"
+__repo__ = "https://github.com/QriusGlobal/injx"
+__docs__ = "https://qriusglobal.github.io/injx/"
