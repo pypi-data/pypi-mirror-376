cc3c9a9535da685f5dff418ce6e928a72bc0cf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:06:51 +0000

    Add package qubitscope/0.0.1/qubitscope-0.0.1.tar.gz

---
+++ packages/qubitscope/qubitscope-0.0.1.tar.gz/qubitscope-0.0.1/README.md
@@ -0,0 +1,2 @@
+# QubitScope
+QubitScope:  Face to quantum analyze.
---
+++ packages/qubitscope/qubitscope-0.0.1.tar.gz/qubitscope-0.0.1/qubitscope/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2025/09/15 13:19:34
+########################################################################
