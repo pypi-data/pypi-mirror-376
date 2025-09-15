b1f1680ea5617d32687430f82cee39c3170d52f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:06:19 +0000

    Add package types-mysqlclient/2.2.0.20250915/types_mysqlclient-2.2.0.20250915.tar.gz

---
+++ packages/types-mysqlclient/types_mysqlclient-2.2.0.20250915.tar.gz/types_mysqlclient-2.2.0.20250915/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'MySQLdb-stubs': ['__init__.pyi', '_exceptions.pyi', '_mysql.pyi', 'connections.pyi', 'constants/CLIENT.pyi', 'constants/CR.pyi', 'constants/ER.pyi', 'constants/FIELD_TYPE.pyi', 'constants/FLAG.pyi', 'constants/__init__.pyi', 'converters.pyi', 'cursors.pyi', 'release.pyi', 'times.pyi', 'METADATA.toml', 'py.typed']})
