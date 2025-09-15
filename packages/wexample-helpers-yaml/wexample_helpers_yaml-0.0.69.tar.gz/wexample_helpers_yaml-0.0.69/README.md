1b3be9b855afb039f7277511cfcb3ba7dd97616f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 17:07:07 +0000

    Add package wexample-helpers-yaml/0.0.69/wexample_helpers_yaml-0.0.69.tar.gz

---
+++ packages/wexample-helpers-yaml/wexample_helpers_yaml-0.0.69.tar.gz/wexample_helpers_yaml-0.0.69/README.md
@@ -0,0 +1,35 @@
+# wexample-helpers-yaml
+
+Helpers for reading yaml files.
+
+Version: 0.0.68
+
+## Requirements
+
+- Python >=3.10
+
+## Dependencies
+
+- attrs>=23.1.0
+- cattrs>=23.1.0
+- pyyaml
+- wexample-helpers==0.0.68
+
+## Installation
+
+```bash
+pip install wexample-helpers-yaml
+```
+
+## Links
+
+- Homepage: https://github.com/wexample/python-helpers-yaml
+
+## License
+
+MIT
+## Credits
+
+This package has been developed by [Wexample](https://wexample.com), a collection of tools and utilities to streamline development workflows.
+
+Visit [wexample.com](https://wexample.com) to discover more tools and resources for efficient development.
---
+++ packages/wexample-helpers-yaml/wexample_helpers_yaml-0.0.69.tar.gz/wexample_helpers_yaml-0.0.69/src/wexample_helpers_yaml/helpers/yaml_helpers.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import yaml
+
+if TYPE_CHECKING:
+    from wexample_helpers_yaml.const.types import YamlContent, YamlContentDict
+
+
+def yaml_read(file_path: str, default: YamlContent | None = None) -> YamlContent | None:
+    try:
+        with open(file_path) as f:
+            content = yaml.safe_load(f)
+
+            if isinstance(content, dict):
+                return content
+            else:
+                return default
+    except Exception:
+        return default
+
+
+def yaml_read_dict(
+    file_path: str, default: YamlContentDict | None = None
+) -> YamlContentDict:
+    content = yaml_read(file_path, default)
+
+    if not content:
+        return default or {}
+
+    assert isinstance(content, dict)
+
+    return content
+
+
+def yaml_write(file_path: str, content: YamlContent) -> None:
+    with open(file_path, "w") as f:
+        yaml.safe_dump(content, f)
