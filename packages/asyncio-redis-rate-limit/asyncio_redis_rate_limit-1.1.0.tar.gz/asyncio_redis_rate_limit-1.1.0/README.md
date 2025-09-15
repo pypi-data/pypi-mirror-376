15e84c5c42b64e925bcd93651cd632d778660b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 16:05:50 +0000

    Add package asyncio-redis-rate-limit/1.1.0/asyncio_redis_rate_limit-1.1.0.tar.gz

---
+++ packages/asyncio-redis-rate-limit/asyncio_redis_rate_limit-1.1.0.tar.gz/asyncio_redis_rate_limit-1.1.0/README.md
@@ -0,0 +1,81 @@
+# asyncio-redis-rate-limit
+
+[![wemake.services](https://img.shields.io/badge/%20-wemake.services-green.svg?label=%20&logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC%2FxhBQAAAAFzUkdCAK7OHOkAAAAbUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP%2F%2F%2F5TvxDIAAAAIdFJOUwAjRA8xXANAL%2Bv0SAAAADNJREFUGNNjYCAIOJjRBdBFWMkVQeGzcHAwksJnAPPZGOGAASzPzAEHEGVsLExQwE7YswCb7AFZSF3bbAAAAABJRU5ErkJggg%3D%3D)](https://wemake-services.github.io)
+[![test](https://github.com/wemake-services/asyncio-redis-rate-limit/actions/workflows/test.yml/badge.svg?branch=master&event=push)](https://github.com/wemake-services/asyncio-redis-rate-limit/actions/workflows/test.yml)
+[![codecov](https://codecov.io/gh/wemake-services/asyncio-redis-rate-limit/branch/master/graph/badge.svg)](https://codecov.io/gh/wemake-services/asyncio-redis-rate-limit)
+[![Python Version](https://img.shields.io/pypi/pyversions/asyncio-redis-rate-limit.svg)](https://pypi.org/project/asyncio-redis-rate-limit/)
+[![wemake-python-styleguide](https://img.shields.io/badge/style-wemake-000000.svg)](https://github.com/wemake-services/wemake-python-styleguide)
+
+Rate limiter for async functions using Redis as a backend.
+
+
+## Features
+
+- Small and simple
+- Can be used as a decorator or as a context manager
+- Can be used for both clients and servers
+- Works with `asyncio`
+- Works with any amount of processes
+- Works with both [`redis.asyncio.client.Redis`](https://redis-py.readthedocs.io/en/stable/examples/asyncio_examples.html) and [`aioredis`](https://github.com/aio-libs/aioredis-py)
+- Free of race-conditions (hopefully!)
+- Supports `redis` since `7.0`
+- Fully typed with annotations and checked with mypy, [PEP561 compatible](https://www.python.org/dev/peps/pep-0561/)
+
+
+## Installation
+
+```bash
+pip install asyncio-redis-rate-limit
+```
+
+Extras available:
+- `pip install asyncio-redis-rate-limit[redis]`
+- `pip install asyncio-redis-rate-limit[aioredis]` (for python versions `<3.11`)
+
+
+## Example
+
+As a decorator:
+
+```python
+>>> from asyncio_redis_rate_limit import rate_limit, RateSpec
+>>> from redis.asyncio import Redis as AsyncRedis  # pip install redis
+
+>>> redis = AsyncRedis.from_url('redis://localhost:6379')
+
+>>> @rate_limit(
+...    rate_spec=RateSpec(requests=1200, seconds=60),
+...    backend=redis,
+... )
+... async def request() -> ...:
+...     ...   # Do something useful! Call this function as usual.
+
+```
+
+Or as a context manager:
+
+```python
+>>> from asyncio_redis_rate_limit import RateLimiter, RateSpec
+>>> from redis.asyncio import Redis as AsyncRedis  # pip install redis
+
+>>> redis = AsyncRedis.from_url('redis://localhost:6379')
+
+>>> async def request() -> ...:
+...     async with RateLimiter(
+...         unique_key='api-name.com',
+...         backend=redis,
+...         rate_spec=RateSpec(requests=5, seconds=1),
+...     ):
+...         ...  # Do the request itself.
+
+```
+
+
+## License
+
+[MIT](https://github.com/wemake-services/asyncio-redis-rate-limit/blob/master/LICENSE)
+
+
+## Credits
+
+This project was generated with [`wemake-python-package`](https://github.com/wemake-services/wemake-python-package). Current template version is: [1d63652fbb33ebe2f6d932f511b7f529a4ce2d2a](https://github.com/wemake-services/wemake-python-package/tree/1d63652fbb33ebe2f6d932f511b7f529a4ce2d2a). See what is [updated](https://github.com/wemake-services/wemake-python-package/compare/1d63652fbb33ebe2f6d932f511b7f529a4ce2d2a...master) since then.
---
+++ packages/asyncio-redis-rate-limit/asyncio_redis_rate_limit-1.1.0.tar.gz/asyncio_redis_rate_limit-1.1.0/asyncio_redis_rate_limit/__init__.py
@@ -0,0 +1,179 @@
+import asyncio
+import hashlib
+from collections.abc import Awaitable, Callable
+from functools import wraps
+from types import TracebackType
+from typing import NamedTuple, TypeAlias, TypeVar
+
+from typing_extensions import ParamSpec, final
+
+from asyncio_redis_rate_limit.compat import (
+    AnyPipeline,
+    AnyRedis,
+    pipeline_expire,
+)
+
+#: These aliases makes our code more readable.
+_Seconds: TypeAlias = int
+
+_ResultT = TypeVar('_ResultT')
+_ParamsT = ParamSpec('_ParamsT')
+
+_CoroutineFunction: TypeAlias = Callable[_ParamsT, Awaitable[_ResultT]]
+
+_RateLimiterT = TypeVar('_RateLimiterT', bound='RateLimiter')
+
+
+@final
+class RateLimitError(Exception):
+    """We raise this error when rate limit is hit."""
+
+
+@final
+class RateSpec(NamedTuple):
+    """
+    Specifies the amount of requests can be made in the time frame in seconds.
+
+    It is much nicier than using a custom string format like ``100/1s``.
+    """
+
+    requests: int
+    seconds: _Seconds
+
+
+class RateLimiter:
+    """Implements rate limiting."""
+
+    __slots__ = (
+        '_backend',
+        '_cache_prefix',
+        '_lock',
+        '_rate_spec',
+        '_unique_key',
+    )
+
+    def __init__(
+        self,
+        unique_key: str,
+        rate_spec: RateSpec,
+        backend: AnyRedis,
+        *,
+        cache_prefix: str,
+    ) -> None:
+        """In the future other backends might be supported as well."""
+        self._unique_key = unique_key
+        self._rate_spec = rate_spec
+        self._backend = backend
+        self._cache_prefix = cache_prefix
+        self._lock = asyncio.Lock()
+
+    async def __aenter__(self: _RateLimiterT) -> _RateLimiterT:
+        """
+        Async context manager API.
+
+        Before this object will be used, we call ``self._acquire`` to be sure
+        that we can actually make any actions in this time frame.
+        """
+        await self._acquire()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None:
+        """Do nothing. We need this to ``__aenter__`` to work."""
+
+    # Private API:
+
+    async def _acquire(self) -> None:
+        cache_key = self._make_cache_key(
+            unique_key=self._unique_key,
+            rate_spec=self._rate_spec,
+            cache_prefix=self._cache_prefix,
+        )
+        pipeline = self._backend.pipeline()
+
+        async with self._lock:
+            current_rate = await self._run_pipeline(cache_key, pipeline)
+            # This looks like a coverage error on 3.10:
+            if current_rate > self._rate_spec.requests:  # pragma: no cover
+                raise RateLimitError('Rate limit is hit', current_rate)
+
+    async def _run_pipeline(
+        self,
+        cache_key: str,
+        pipeline: AnyPipeline,
+    ) -> int:
+        # https://redis.io/commands/incr/#pattern-rate-limiter-1
+        current_rate, _ = await pipeline_expire(
+            pipeline.incr(cache_key),
+            cache_key,
+            self._rate_spec.seconds,
+        ).execute()
+        return current_rate  # type: ignore[no-any-return]
+
+    def _make_cache_key(
+        self,
+        unique_key: str,
+        rate_spec: RateSpec,
+        cache_prefix: str,
+    ) -> str:
+        parts = ''.join([unique_key, str(rate_spec)])
+        return (
+            cache_prefix
+            + hashlib.md5(  # noqa: S324
+                parts.encode('utf-8'),
+            ).hexdigest()
+        )
+
+
+def rate_limit(  # noqa: WPS320
+    rate_spec: RateSpec,
+    backend: AnyRedis,
+    *,
+    cache_prefix: str = 'aio-rate-limit',
+) -> Callable[
+    [_CoroutineFunction[_ParamsT, _ResultT]],
+    _CoroutineFunction[_ParamsT, _ResultT],
+]:
+    """
+    Rate limits a function.
+
+    Code example:
+
+      .. code:: python
+
+        >>> from asyncio_redis_rate_limit import rate_limit, RateSpec
+        >>> from redis.asyncio import Redis as AsyncRedis
+
+        >>> redis = AsyncRedis.from_url('redis://localhost:6379')
+
+        >>> @rate_limit(
+        ...     rate_spec=RateSpec(requests=1200, seconds=60),
+        ...     backend=redis,
+        ... )
+        ... async def request() -> int: ...  # Do something
+
+    """
+
+    def decorator(
+        function: _CoroutineFunction[_ParamsT, _ResultT],
+    ) -> _CoroutineFunction[_ParamsT, _ResultT]:
+        @wraps(function)
+        async def factory(
+            *args: _ParamsT.args,
+            **kwargs: _ParamsT.kwargs,
+        ) -> _ResultT:
+            async with RateLimiter(
+                unique_key=function.__qualname__,
+                backend=backend,
+                rate_spec=rate_spec,
+                cache_prefix=cache_prefix,
+            ):
+                return await function(*args, **kwargs)
+
+        return factory
+
+    return decorator
