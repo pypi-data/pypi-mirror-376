7af5eb3e4265b2972ca0c591ac5541aad34fe7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:53 +0000

    Add package ivrit/0.1.0/ivrit-0.1.0-py3-none-any.whl

---
+++ packages/ivrit/ivrit-0.1.0-py3-none-any.whl/ivrit/__init__.py
@@ -0,0 +1,11 @@
+"""
+ivrit - Python package providing wrappers around ivrit.ai's capabilities
+"""
+
+from importlib.metadata import version, PackageNotFoundError
+__version__ = version("ivrit")
+
+from .audio import load_model, TranscriptionModel, TranscriptionSession, FasterWhisperModel, StableWhisperModel, RunPodModel
+from .types import Segment
+
+__all__ = ['load_model', 'TranscriptionModel', 'TranscriptionSession', 'Segment'] 
