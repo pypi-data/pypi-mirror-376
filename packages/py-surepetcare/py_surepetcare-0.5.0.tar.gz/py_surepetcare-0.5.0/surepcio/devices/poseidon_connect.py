738a166d0fce5163de28e3eda7ff07d9b06a799e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:14 +0000

    Add package py-surepetcare/0.5.0/py_surepetcare-0.5.0.tar.gz

---
+++ packages/py-surepetcare/py_surepetcare-0.5.0.tar.gz/py_surepetcare-0.5.0/surepcio/devices/poseidon_connect.py
@@ -0,0 +1,41 @@
+import logging
+
+from .device import BaseControl
+from .device import BaseStatus
+from .device import DeviceBase
+from surepcio.command import Command
+from surepcio.const import API_ENDPOINT_PRODUCTION
+from surepcio.devices.entities import DevicePetTag
+from surepcio.enums import ProductId
+
+logger = logging.getLogger(__name__)
+
+
+class Control(BaseControl):
+    pass
+
+
+class Status(BaseStatus):
+    pass
+
+
+class PoseidonConnect(DeviceBase[Control, Status]):
+    def refresh(self):
+        def parse(response):
+            if not response:
+                return self
+
+            self.status = BaseStatus(**{**self.status.model_dump(), **response["data"]})
+            self.control = BaseControl(**{**self.control.model_dump(), **response["data"]})
+            self.tags = [DevicePetTag(**tag) for tag in response["data"].get("tags", [])]
+            return self
+
+        return Command(
+            method="GET",
+            endpoint=f"{API_ENDPOINT_PRODUCTION}/device/{self.id}",
+            callback=parse,
+        )
+
+    @property
+    def product(self) -> ProductId:
+        return ProductId.POSEIDON_CONNECT
