923e59c81d7235976ba56c6818d35ea4eb603db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:22:39 +0000

    Add package chemformula/1.5.0/chemformula-1.5.0-py3-none-any.whl

---
+++ packages/chemformula/chemformula-1.5.0-py3-none-any.whl/chemformula/__init__.py
@@ -0,0 +1,8 @@
+﻿__all__ = ["ChemFormula"]
+from .chemformula import ChemFormula
+
+try:
+    from importlib.metadata import version
+    __version__ = version("chemformula")
+except ImportError:  # pragma: no cover
+    __version__ = "unknown"  # pragma: no cover
