1cdbc8f96b06f6fcbfa74f098c4e1a26031f4926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:23:22 +0000

    Add package matteplotlib/0.5/matteplotlib-0.5-py3-none-any.whl

---
+++ packages/matteplotlib/matteplotlib-0.5-py3-none-any.whl/matplotib/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+from .main import (
+    ml_help,
+    ml_index,
+    ml_prac_1a, ml_prac_1b, ml_prac_1c, ml_prac_1d,
+    ml_prac_2a,
+    ml_prac_3a, ml_prac_3b, ml_prac_3c,
+    ml_prac_4a, ml_prac_4b, ml_prac_4c, ml_prac_4d, ml_prac_4e, ml_prac_4f,
+    ml_prac_5a, ml_prac_5b,
+    ml_prac_6a, ml_prac_6b,
+    ml_prac_7a, ml_prac_7b,
+    ml_prac_8a,
+    ml_prac_9a
+)
