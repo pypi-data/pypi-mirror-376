53f40b5a41c4c4d5c66140f981154ce78f78f072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:44 +0000

    Add package plexdl/0.5.0/plexdl-0.5.0-py3-none-any.whl

---
+++ packages/plexdl/plexdl-0.5.0-py3-none-any.whl/plexdl/cli.py
@@ -0,0 +1,106 @@
+"""plexdl CLI."""
+
+import datetime
+import sys
+
+import humanize
+import plexapi
+import typer
+from importlib_metadata import version
+
+__version__ = version(__package__)
+
+from plexapi.myplex import MyPlexAccount
+
+from plexdl.plexdl import Client
+
+app = typer.Typer(
+    context_settings={"max_content_width": 9999},
+    help=__doc__,
+    pretty_exceptions_enable=False,
+)
+
+
+@app.command()
+def get_server_info(
+    username: str = typer.Argument(None, envvar="PLEXDL_USERNAME"),
+    password: str = typer.Argument(None, envvar="PLEXDL_PASSWORD"),
+):
+    """Show info about servers available to your account."""
+    p = MyPlexAccount(
+        username=username,
+        password=password,
+    )
+    servers = p.resources()
+    for server in servers:
+        if server.product == "Plex Media Server":
+            print(f"{server.name=} ", "-" * 70)
+            print(f"    clientIdentifier: {server.clientIdentifier}")
+            last_seen_diff = datetime.datetime.now() - server.lastSeenAt
+            last_seen_time_delta = humanize.naturaldelta(last_seen_diff)
+            print(f"    lastSeenAt: {server.lastSeenAt.strftime('%FT%T%z')} ({last_seen_time_delta})")
+            created_diff = datetime.datetime.now() - server.createdAt
+            created_time_delta = humanize.naturaldelta(created_diff)
+            print(f"    createdAt: {server.createdAt.strftime('%FT%T%z')} ({created_time_delta})")
+            for i, connection in enumerate(server.connections):
+                preferred = connection.uri in server.preferred_connections()
+                print(f"    connections[{i}]:")
+                print(f"        local: {connection.local}")
+                print(f"        uri: {connection.uri}")
+                print(f"        httpuri: {connection.httpuri}")
+                print(f"        relay: {bool(connection.relay)}")
+                print(f"        preferred: {preferred}")
+            print(f"    device: {server.device}")
+            print(f"    home: {server.home}")
+            print(f"    httpsRequired: {server.httpsRequired}")
+            print(f"    name: {server.name}")
+            print(f"    owned: {server.owned}")
+            print(f"    platform: {server.platform}")
+            print(f"    platformVersion: {server.platformVersion}")
+            print(f"    presence: {server.presence}")
+            print(f"    product: {server.product}")
+            print(f"    productVersion: {server.productVersion}")
+            print(f"    provides: {server.provides}")
+            print(f"    publicAddressMatches: {server.publicAddressMatches}")
+            print(f"    sourceTitle: {server.sourceTitle}")
+            print(f"    synced: {server.synced}")
+            print()
+
+
+@app.command()
+def search(
+    username: str = typer.Option(..., "-u", "--username", envvar="PLEXDL_USERNAME"),
+    password: str = typer.Option(..., "-p", "--password", envvar="PLEXDL_PASSWORD"),
+    title: str = typer.Argument(..., envvar="PLEXDL_TITLE"),
+    item_prefix: str = typer.Option("", "--item-prefix", help="String to prefix to each item (eg: curl -o)"),
+    show_summary: bool = typer.Option(False, "--show-summary", help="Show media summary for each result"),
+    show_ratings: bool = typer.Option(False, "--show-ratings", help="Show ratings for each result"),
+    show_metadata: bool = typer.Option(False, "--show-metadata", help="Show file and codec metadata for each file in each result"),
+    include_relays: bool = typer.Option(False, "--include-relays", help="Output relay servers along with direct servers"),
+    debug: bool = typer.Option(False, "--debug"),
+):
+    """Search for media in servers that are available to your account."""
+    p = Client(
+        username=username,
+        password=password,
+        title=title,
+        relay=include_relays,
+        debug=debug,
+        item_prefix=item_prefix,
+        summary=show_summary,
+        ratings=show_ratings,
+        metadata=show_metadata,
+    )
+    p.main()
+
+
+if __name__ == "__main__":
+    try:
+        app()
+    except KeyboardInterrupt:
+        sys.exit(1)
+    except plexapi.exceptions.BadRequest:
+        sys.exit(2)
+
+
+# https://typer.tiangolo.com/tutorial/subcommands/add-typer/
---
+++ packages/plexdl/plexdl-0.5.0-py3-none-any.whl/plexdl/plexdl.py
@@ -0,0 +1,128 @@
+"""plexdl client class."""
+
+import locale
+import logging
+from typing import ClassVar
+
+import humanfriendly
+import requests
+from plexapi.exceptions import Unauthorized
+from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
+
+log = logging.getLogger("plexdl")
+locale.setlocale(locale.LC_ALL, "")
+
+
+class Client:
+    """A client interface to plex for finding direct download URLs."""
+
+    def __init__(self, **kwargs):
+        """plexdl.Client __init__ method."""
+        self.debug = kwargs["debug"]
+        self.item_prefix = kwargs["item_prefix"]
+        self.metadata = kwargs["metadata"]
+        self.password = kwargs["password"]
+        self.ratings = kwargs["ratings"]
+        self.relay = kwargs["relay"]
+        self.summary = kwargs["summary"]
+        self.title = kwargs["title"]
+        self.username = kwargs["username"]
+        self.account = MyPlexAccount(self.username, self.password)
+
+    available_servers: ClassVar[list[PlexServer]] = []
+
+    @staticmethod
+    def print_item_info(self, item, access_token):
+        """Print info about a given media item."""
+        log.debug(f"Filesystem locations: {item.locations}")
+        if not hasattr(item, "iterParts"):
+            return
+        locations = [i for i in item.iterParts() if i]
+        for location in locations:
+            media_info = []
+            download_url = item._server.url(f"{location.key}?X-Plex-Token={access_token}")
+            download_filename = ""
+            if hasattr(item, "seasonEpisode"):
+                download_filename += f"{item.seasonEpisode} "
+            download_filename += f"{item.title}.{location.container}"
+            if self.metadata is True:
+                if item.media[0].width is not None:
+                    media_info.append(f"{item.media[0].width}x{item.media[0].height}")
+                if item.media[0].videoCodec is not None:
+                    media_info.append(item.media[0].videoCodec)
+                if item.media[0].audioCodec is not None:
+                    media_info.append(item.media[0].audioCodec)
+                if item.media[0].bitrate is not None:
+                    media_info.append(f"{item.media[0].bitrate}kbps")
+                try:
+                    length = humanfriendly.format_size(int(requests.head(download_url, timeout=30).headers["Content-Length"]))
+                    media_info.append(f"{length}")
+                except ValueError:
+                    pass
+            if media_info:
+                print(f"({', '.join(media_info)})")
+            print(f'  {self.item_prefix} "{download_filename}" "{download_url}"')
+
+    @staticmethod
+    def print_all_items_for_server(self, item, access_token):
+        """Print info about all media items discovered on a given Plex server."""
+        if item.TYPE in ["movie", "track"]:
+            print("-" * 79)
+            print(f"{item.TYPE.capitalize()}: {item.title}")
+            if self.summary is True and len(item.summary) > 1:
+                print(f"Year: {item.year}")
+                print(f"Studio: {item.studio}")
+                print(f"Summary: {item.summary}")
+            if self.ratings is True:
+                if content_rating := getattr(item, "contentRating", None):
+                    print(f"Content rating: {content_rating}")
+                if audience_rating := getattr(item, "audienceRating", None):
+                    print(f"Audience rating: {audience_rating}")
+                if rating := getattr(item, "rating", None):
+                    print(f"Critic rating: {rating}")
+            self.print_item_info(self, item, access_token)
+        elif item.TYPE in ["show"]:
+            print("-" * 79)
+            if self.summary is True and len(item.summary) > 1:
+                print(f"{item.TYPE.capitalize()}: {item.title}\nSummary: {item.summary}")
+            if self.ratings is True:
+                print(f"Audience rating: {item.audienceRating}\nCritic rating: {item.rating}\nRated: {item.contentRating}")
+            for episode in item.episodes():
+                self.print_item_info(self, episode, access_token)
+
+    # TODO: break this up into just search, with print being handled elsewhere
+    def main(self):
+        """Perform all search and print logic."""
+        for r in self.account.resources():
+            if r.product == "Plex Media Server":
+                self.available_servers.append(r)
+
+        for this_server in self.available_servers:
+            if not this_server.presence:
+                continue
+            for connection in this_server.connections:
+                if connection.local:
+                    continue
+                try:
+                    this_server_connection = PlexServer(connection.uri, this_server.accessToken)
+                    relay_status = ""
+                    if connection.relay:
+                        if self.relay is False:
+                            log.debug(f"Skipping {this_server_connection.friendlyName} relay")
+                            continue
+                        relay_status = " (relay)"
+                    print("\n")
+                    print("=" * 79)
+                    print(f'Server: "{this_server_connection.friendlyName}"{relay_status}')
+
+                    # TODO: add flags for each media type to help sort down what is displayed (since /hub/seach?mediatype="foo" doesn't work)
+                    # TODO: write handlers for each type
+                    # TODO: save results to a native data structure and have different output methods (eg: json, download links, csv)
+                    for item in this_server_connection.search(self.title):
+                        self.print_all_items_for_server(self, item, this_server.accessToken)
+
+                except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, Unauthorized) as e:
+                    print(f'ERROR: connection to "{this_server.name} {connection.address}" failed.')
+                    log.debug(e)
+                    continue
