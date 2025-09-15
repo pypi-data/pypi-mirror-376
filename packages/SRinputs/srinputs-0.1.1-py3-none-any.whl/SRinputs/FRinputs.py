22e9a28425ad1de2c16960906038866168cb780a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 05:06:51 +0000

    Add package SRinputs/0.1.1/srinputs-0.1.1-py3-none-any.whl

---
+++ packages/SRinputs/srinputs-0.1.1-py3-none-any.whl/SRinputs/FRinputs.py
@@ -0,0 +1,25 @@
+
+
+"""Name, Url, Mail, Date, PhoneNumber, Id, Handle
+"""
+def FRNameInput():
+    ...
+
+def FRUrlInput():
+    ...
+
+def FRMailInput():
+    ...
+
+def FRDateInput():
+    ...
+
+def FRPhoneNumInput():
+    ...
+
+def FRIdInput():
+    ...
+
+def FRHandleInput():
+    ...
+
---
+++ packages/SRinputs/srinputs-0.1.1-py3-none-any.whl/SRinputs/__init__.py
@@ -0,0 +1,3 @@
+from .SRinputs import IntInput, StrInput, FloatInput, multiInput 
+
+__all__ = ["IntInput", "StrInput", "FloatInput", "multiInput"]
