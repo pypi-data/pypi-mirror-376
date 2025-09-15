dfd9c315d1c76caffc7df1ba6f9b61ed487f1eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:23:18 +0000

    Add package Eurocybersecurite-AI-Cybersecurity/0.1.2/eurocybersecurite_ai_cybersecurity-0.1.2.tar.gz

---
+++ packages/Eurocybersecurite-AI-Cybersecurity/eurocybersecurite_ai_cybersecurity-0.1.2.tar.gz/eurocybersecurite_ai_cybersecurity-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='Eurocybersecurite-AI-Cybersecurity',
+    version='0.1.2',
+    description='AI-powered cybersecurity application by Eurocybersecurite',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    author='Eurocybersecurite',
+    packages=find_packages(where='RooR'),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "transformers",
+        "torch",
+        "scikit-learn"
+    ],
+    entry_points={
+        'console_scripts': [
+            'cybersecurity=main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
