06b6e84a9268ce562239506e24c74025f6eabbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:23:19 +0000

    Add package casregnum/1.1.2/casregnum-1.1.2-py3-none-any.whl

---
+++ packages/casregnum/casregnum-1.1.2-py3-none-any.whl/casregnum/__init__.py
@@ -0,0 +1,8 @@
+﻿__all__ = ["CAS"]
+from .casregnum import CAS
+
+try:
+    from importlib.metadata import version
+    __version__ = version("casregnum")
+except ImportError:  # pragma: no cover
+    __version__ = "unknown"  # pragma: no cover
