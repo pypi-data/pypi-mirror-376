4bba479de06b6186e2613fc772b5446dee1a4bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:52 +0000

    Add package echocodepackage/0.1/echocodepackage-0.1.tar.gz

---
+++ packages/echocodepackage/echocodepackage-0.1.tar.gz/echocodepackage-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+ name='echocodepackage',
+ version='0.1',
+ author="Yash",
+ author_email="high2005alert@gmail.com",
+ description="Basic calculator package",
+ long_description="Basic Threading package",
+ long_description_content_type="text/plain",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
