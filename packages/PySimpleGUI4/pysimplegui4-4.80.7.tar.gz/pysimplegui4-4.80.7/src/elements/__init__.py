16dae5a5f9e8c5ebd2586b31206cb7aa681adde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:27 +0000

    Add package PySimpleGUI4/4.80.7/pysimplegui4-4.80.7.tar.gz

---
+++ packages/PySimpleGUI4/pysimplegui4-4.80.7.tar.gz/pysimplegui4-4.80.7/src/elements/__init__.py
@@ -0,0 +1,11 @@
+from .text import Text
+from .input import Input, InputText
+from .button import Button
+from .slider import Slider
+from .checkbox import Checkbox
+from .combo import Combo
+from .multiline import Multiline
+# ... 导入所有元素类型
+
+# 保持向后兼容的名称
+InputText = Input
