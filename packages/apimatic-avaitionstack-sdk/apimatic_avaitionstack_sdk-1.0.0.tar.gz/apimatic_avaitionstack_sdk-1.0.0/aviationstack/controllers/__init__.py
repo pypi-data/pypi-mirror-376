c9681c3111f0150791065b733955867f81a807fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:07:26 +0000

    Add package apimatic-avaitionstack-sdk/1.0.0/apimatic_avaitionstack_sdk-1.0.0.tar.gz

---
+++ packages/apimatic-avaitionstack-sdk/apimatic_avaitionstack_sdk-1.0.0.tar.gz/apimatic_avaitionstack_sdk-1.0.0/aviationstack/controllers/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'base_controller',
+    'flights_controller',
+    'routes_controller',
+    'airports_controller',
+    'airlines_controller',
+    'airplanes_controller',
+    'aircraft_types_controller',
+    'taxes_controller',
+    'cities_controller',
+    'countries_controller',
+]
