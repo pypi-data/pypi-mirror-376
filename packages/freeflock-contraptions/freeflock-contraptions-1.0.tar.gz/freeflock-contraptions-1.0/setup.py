a70f6f31875affe458258481f5b66cf8fb88142f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:45 +0000

    Add package freeflock-contraptions/1.0/freeflock-contraptions-1.0.tar.gz

---
+++ packages/freeflock-contraptions/freeflock-contraptions-1.0.tar.gz/freeflock-contraptions-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+installation_requirements = [
+    "openai-agents==0.3.0",
+    "loguru==0.7.3",
+    "neo4j==5.28.2",
+    "google-genai==1.36.0"
+]
+
+setup(
+    version="1.0",
+    name="freeflock-contraptions",
+    description="A collection of contraptions",
+    author="(~)",
+    url="https://github.com/freeflock/contraptions",
+    package_dir={"": "packages"},
+    packages=["freeflock_contraptions"],
+    install_requires=installation_requirements,
+)
