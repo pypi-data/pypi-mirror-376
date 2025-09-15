39164030889dd31613f869c2e7ffb1d999c800ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:22:51 +0000

    Add package datapress/1.1.1/datapress-1.1.1-py3-none-any.whl

---
+++ packages/datapress/datapress-1.1.1-py3-none-any.whl/datapress/__init__.py
@@ -0,0 +1,10 @@
+"""
+DataPress Python Client
+
+A Python client library for interacting with the DataPress API.
+"""
+
+from .client import DataPressClient
+
+__version__ = "1.1.1"
+__all__ = ["DataPressClient"]
