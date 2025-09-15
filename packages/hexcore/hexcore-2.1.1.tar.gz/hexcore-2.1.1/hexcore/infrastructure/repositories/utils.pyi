08306a1ca3536c218673ec924de0e0bd32ebd531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:06:56 +0000

    Add package hexcore/2.1.1/hexcore-2.1.1.tar.gz

---
+++ packages/hexcore/hexcore-2.1.1.tar.gz/hexcore-2.1.1/hexcore/infrastructure/repositories/utils.pyi
@@ -0,0 +1,13 @@
+import typing as t
+from .base import BaseSQLRepository as BaseSQLRepository, BaseSQLTenantAwareRepository as BaseSQLTenantAwareRepository
+from .orms.nosql.beanie import BaseDocument as BaseDocument
+from .orms.sql.alchemy import BaseModel as BaseModel
+from hexcore.domain.base import BaseEntity as BaseEntity
+from hexcore.types import FieldResolversType as FieldResolversType, VisitedResultsType as VisitedResultsType, VisitedType as VisitedType
+
+T = t.TypeVar('T', bound=BaseModel[t.Any] | BaseDocument)
+E = t.TypeVar('E', bound=BaseEntity)
+
+async def to_entity_from_model_or_document(model_instance: T, entity_class: type[E], field_resolvers: FieldResolversType[T] | None = None, is_nosql: bool = False) -> E: ...
+def get_all_concrete_subclasses(cls) -> set[type]: ...
+def discover_sql_repositories() -> dict[str, type[BaseSQLRepository[t.Any]] | type[BaseSQLTenantAwareRepository[t.Any]]]: ...
