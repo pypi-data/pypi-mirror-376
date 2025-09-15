ad727c1016f9f0f60c3f45bef10eb200ca307dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 05:05:50 +0000

    Add package Banz-domainfile-generator/1.0.1/banz_domainfile_generator-1.0.1.tar.gz

---
+++ packages/Banz-domainfile-generator/banz_domainfile_generator-1.0.1.tar.gz/banz_domainfile_generator-1.0.1/src/Banz_domainfile_generator/test.py
@@ -0,0 +1,16 @@
+from .domainfile_generator import run
+
+
+csvTest = 'organization;Person;name,STRING,required;firstName,STRING;dateOfBirth,DATE;gender,ENUM(Gender/MALE/FEMALE/DIVERSE),required;memberships,INCOMING_REFERENCE(organization.Membership/n);contacts,INCOMING_REFERENCE(fan.Contact/n)'
+csvTest += '\norganization;Contact;validFrom,DATE,required;validTo,DATE;type,ENUM(ContactType/EMAIL/ADDRESS/PHONE),required;street,STRING;number,STRING;numberSuffix,STRING;postbox,STRING;zip,STRING;city,STRING;country,STRING;email,STRING;countryCode,STRING;phoneNumber,STRING;person,REFERENCE(organization.Person),required'
+
+paths = {
+    'domain': '\\test\\backend\\domain\\src\\main\\java\\org\\derbanz\\cluborga\\domain\\',
+    'schema': '\\test\\backend\\domain\\src\\main\\resources\\schema\\',
+    'logic': '\\test\\backend\\logic\\src\\main\\java\\org\\derbanz\\cluborga\\logic\\',
+    'commonService': '\\test\\backend\\service\\common\\src\\main\\java\\org\\derbanz\\cluborga\\commonservice\\',
+    'uiService': '\\test\\backend\\service\\ui\\src\\main\\java\\org\\derbanz\\cluborga\\uiservice\\'
+}
+    
+
+run(csvTest, paths)
