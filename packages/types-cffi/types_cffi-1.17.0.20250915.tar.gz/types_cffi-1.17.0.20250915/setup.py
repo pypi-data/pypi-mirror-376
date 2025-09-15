ce108e5a55bdfe36d282c4039744921c7c70144a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:06:20 +0000

    Add package types-cffi/1.17.0.20250915/types_cffi-1.17.0.20250915.tar.gz

---
+++ packages/types-cffi/types_cffi-1.17.0.20250915.tar.gz/types_cffi-1.17.0.20250915/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'_cffi_backend-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'cffi-stubs': ['__init__.pyi', 'api.pyi', 'backend_ctypes.pyi', 'cffi_opcode.pyi', 'commontypes.pyi', 'cparser.pyi', 'error.pyi', 'ffiplatform.pyi', 'lock.pyi', 'model.pyi', 'pkgconfig.pyi', 'recompiler.pyi', 'setuptools_ext.pyi', 'vengine_cpy.pyi', 'vengine_gen.pyi', 'verifier.pyi', 'METADATA.toml', 'py.typed']})
