51dbafc3b935717412665b7abd2debd2176ac3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 17:07:03 +0000

    Add package dfdrift/0.1.0/dfdrift-0.1.0.tar.gz

---
+++ packages/dfdrift/dfdrift-0.1.0.tar.gz/dfdrift-0.1.0/dfdrift/__init__.py
@@ -0,0 +1,4 @@
+from .validator import DfValidator, SchemaStorage, LocalFileStorage, Alerter, StderrAlerter
+from . import pandas
+
+__all__ = ["DfValidator", "SchemaStorage", "LocalFileStorage", "Alerter", "StderrAlerter", "pandas"]
