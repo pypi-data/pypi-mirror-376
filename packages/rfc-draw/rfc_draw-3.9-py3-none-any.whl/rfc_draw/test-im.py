c2d9739a4807cf36eafd997fbeee78d719fd5422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:06:43 +0000

    Add package rfc-draw/3.9/rfc_draw-3.9-py3-none-any.whl

---
+++ packages/rfc-draw/rfc_draw-3.9-py3-none-any.whl/rfc_draw/test-im.py
@@ -0,0 +1,11 @@
+# 1232, Wed 28 Sep 2022 (NZDT)
+# 1535, Wed  2 Apr 2025 (NZDT)
+#
+# rfc-draw: Nevil's python3 program to draw images for SVG-RFC-1.2 diagrams
+#
+# Copyright 2025, Nevil Brownlee, Taupo NZ
+
+import install_modules as im
+print("$$$ im >%s<" % im)
+im.main()
+
---
+++ packages/rfc-draw/rfc_draw-3.9-py3-none-any.whl/rfc_draw/test-rd32.py
@@ -0,0 +1,8 @@
+
+import RFC_Draw as RD
+
+print("imported %s" % RD)
+
+help(RD)
+
+RD.rfc_draw()  # Start RFC_Draw
