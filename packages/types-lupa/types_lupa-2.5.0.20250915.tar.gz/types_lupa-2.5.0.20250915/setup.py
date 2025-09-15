ba3ebcd1970e1e878d82f43ade722ca65253d7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:06:19 +0000

    Add package types-lupa/2.5.0.20250915/types_lupa-2.5.0.20250915.tar.gz

---
+++ packages/types-lupa/types_lupa-2.5.0.20250915.tar.gz/types_lupa-2.5.0.20250915/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'lupa-stubs': ['__init__.pyi', 'lua51.pyi', 'lua52.pyi', 'lua53.pyi', 'lua54.pyi', 'luajit20.pyi', 'luajit21.pyi', 'version.pyi', 'METADATA.toml', 'py.typed']})
