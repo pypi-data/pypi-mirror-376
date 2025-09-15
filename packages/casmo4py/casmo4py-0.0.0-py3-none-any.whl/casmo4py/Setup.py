b97599c77dac189d6809a3b949fd1d682f761576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 05:05:47 +0000

    Add package casmo4py/0.0.0/casmo4py-0.0.0-py3-none-any.whl

---
+++ packages/casmo4py/casmo4py-0.0.0-py3-none-any.whl/casmo4py/Setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+"""
+"""
+
+
+class Setup:
+    def __init__(self):
+        """
+        Args:
+            bla (test.type): does blub
+        """
+
+        print('hello setup')
+
+    if __name__ == "__main__":
+        s = Setup()
