1a4d9aec4c1c604a94bf34193307110767c2bf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 05:06:50 +0000

    Add package mlc-scripts/1.1.0/mlc_scripts-1.1.0.tar.gz

---
+++ packages/mlc-scripts/mlc_scripts-1.1.0.tar.gz/mlc_scripts-1.1.0/README.md
@@ -0,0 +1,70 @@
+# MLPerf Automations and Scripts
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-green)](LICENSE.md)
+[![Downloads](https://static.pepy.tech/badge/mlcflow)](https://pepy.tech/project/mlcflow)
+[![MLC script automation features test](https://github.com/mlcommons/mlperf-automations/actions/workflows/test-mlc-script-features.yml/badge.svg?cache-bust=1)](https://github.com/mlcommons/mlperf-automations/actions/workflows/test-mlc-script-features.yml)
+[![MLPerf Inference ABTF POC Test](https://github.com/mlcommons/mlperf-automations/actions/workflows/test-mlperf-inference-abtf-poc.yml/badge.svg)](https://github.com/mlcommons/mlperf-automations/actions/workflows/test-mlperf-inference-abtf-poc.yml)
+
+
+Welcome to the **MLPerf Automations and Scripts** repository! This repository is your go-to resource for tools, automations, and scripts designed to streamline the execution of **MLPerf benchmarks**—with a strong emphasis on **MLPerf Inference benchmarks**.
+
+Starting **January 2025**, MLPerf automation scripts is powered by [MLCFlow](https://github.com/mlcommons/mlcflow) automation interface. This new and simplified framework replaces the previous [Collective Mind (CM)](https://github.com/mlcommons/ck/tree/master/cm), providing a more robust, efficient, and self-contained solution for benchmarking workflows, making MLPerf automations independent of any external projects. 
+
+
+---
+
+## 🚀 Key Features
+- **Automated Benchmarking** – Simplifies running MLPerf Inference benchmarks with minimal manual intervention.
+- **Modular and Extensible** – Easily extend the scripts to support additional benchmarks and configurations.
+- **Seamless Integration** – Compatible with Docker, cloud environments, and local machines.
+
+---
+
+## 🧰 MLCFlow (MLC) Automations
+
+Building upon the robust foundation of its predecessor, the Collective Mind (CM) framework, MLCFlow elevates machine learning workflows by simplifying complex tasks such as Docker container management and caching. Written in Python, the mlcflow package offers a versatile interface, supporting both a user-friendly command-line interface (CLI) and a flexible API for effortless automation script management.
+
+At its core, MLCFlow relies on a single powerful automation, the Script, which is extended by two actions: CacheAction and DockerAction. Together, these components provide streamlined functionality to optimize and enhance your ML workflow automation experience.
+
+---
+
+## 🤝 Contributing
+We welcome contributions from the community! To contribute:
+1. Submit pull requests (PRs) to the **`dev`** branch.
+2. Review our [CONTRIBUTORS.md](here) for guidelines and best practices.
+3. Explore more about MLPerf Inference automation in the official [MLPerf Inference Documentation](https://docs.mlcommons.org/inference/).
+
+Your contributions help drive the project forward!
+
+
+---
+
+## 💬 Join the Discussion  
+Connect with us on the [MLCommons Benchmark Infra Discord channel](https://discord.gg/T9rHVwQFNX) to engage in discussions about **MLCFlow** and **MLPerf Automations**. We’d love to hear your thoughts, questions, and ideas!  
+
+---
+
+## 📰 Stay Updated  
+Keep track of the latest development progress and tasks on our [MLPerf Automations Development Board](https://github.com/orgs/mlcommons/projects/50/views/7?sliceBy%5Bvalue%5D=_noValue).  
+Stay tuned for exciting updates and announcements!  
+
+---
+
+## 📄 License
+This project is licensed under the [Apache 2.0 License](LICENSE.md).
+
+---
+
+## 💡 Acknowledgments and Funding
+This project is made possible through the generous support of:
+- [OctoML](https://octoml.ai)
+- [cKnowledge.org](https://cKnowledge.org)
+- [cTuning Foundation](https://cTuning.org)
+- [GATEOverflow](https://gateoverflow.in)
+- [MLCommons](https://mlcommons.org)
+
+We appreciate their contributions and sponsorship!
+
+---
+
+Thank you for your interest and support in MLPerf Automations and Scripts!
---
+++ packages/mlc-scripts/mlc_scripts-1.1.0.tar.gz/mlc_scripts-1.1.0/setup.py
@@ -0,0 +1,133 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+import os
+
+try:
+    import tomllib  # For Python 3.11+
+except ImportError:
+    import toml  # For older Python versions (requires `pip install toml`)
+
+
+def get_project_meta(file_path="pyproject.toml"):
+    """
+    Extracts project metadata from the given pyproject.toml file.
+
+    Args:
+        file_path (str): Path to the pyproject.toml file.
+
+    Returns:
+        dict: Dictionary containing the project metadata.
+    """
+    try:
+        with open(file_path, "r") as f:
+            content_tmp = f.read()
+            if 'tomllib' in globals():
+                # Use tomllib for Python 3.11+
+                project_data = tomllib.loads(content_tmp)
+            else:
+                # Use toml for older versions
+                project_data = toml.loads(content_tmp)
+
+        # Extract metadata under [project]
+        project_meta = project_data.get("project", {})
+        return project_meta
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: {file_path} not found.")
+    except Exception as e:
+        raise RuntimeError(f"Error reading {file_path}: {e}")
+    return {}
+
+
+def check_prerequisites():
+    """Check if Git and python-venv are installed on the system. """
+    try:
+        # Check for Git
+        subprocess.run(["git", "--version"], check=True,
+                       stdout=subprocess.DEVNULL)
+    except FileNotFoundError:
+        sys.exit(
+            "Error: Git is not installed on the system. Please install Git and try again.")
+
+    try:
+        # Check for python-venv
+        subprocess.run([sys.executable, "-m", "venv", "--help"],
+                       check=True, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        sys.exit(
+            "Error: python-venv module is not available. Please install it and try again.")
+
+
+class CustomInstallCommand(install):
+    """Custom install command to run a custom command after installation."""
+
+    def run(self):
+        # Run the standard install process
+        install.run(self)
+
+        # Run custom post-install command
+        try:
+            print("Running custom post-install command...")
+            commit_hash = get_commit_hash()
+            import mlc
+            branch = os.environ.get('MLC_REPO_BRANCH', 'dev')
+
+            res = mlc.access({'action': 'pull',
+                              'target': 'repo',
+                              'repo': 'mlcommons@mlperf-automations',
+                              'branch': branch,
+                              # 'checkout': commit_hash
+                              })
+
+            if res['return'] > 0:
+                raise Exception(
+                    f"Return code:{res['return']} with error:{res.get('error')}")
+
+            # subprocess.run(["echo", "Custom command executed!"], check=True)
+        except Exception as e:
+            sys.exit(f"Error running post-install command: {e}")
+
+
+def read_file(file_name, default=""):
+    if os.path.isfile(file_name):
+        with open(file_name, "r", encoding="utf-8") as f:
+            return f.read().strip()
+    return default
+
+
+def get_commit_hash():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), 'git_commit_hash.txt'), 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        return "unknown"
+
+
+# Check prerequisites before setup
+check_prerequisites()
+
+
+# Get project metadata from pyproject.toml
+project_meta = get_project_meta()
+
+# Read version from the VERSION file
+version = read_file("VERSION", default="0.0.1")
+
+setup(
+
+    name=project_meta.get("name", "mlperf"),
+    version=version,
+    description=project_meta.get("description", "MLPerf Automations."),
+    author=", ".join(a.get("name", "")
+                     for a in project_meta.get("authors", [])),
+    author_email=", ".join(a.get("email", "")
+                           for a in project_meta.get("authors", [])),
+    packages=find_packages(),
+    install_requires=project_meta.get("dependencies", []),
+    python_requires=project_meta.get("python-requires", ">=3.8"),
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
