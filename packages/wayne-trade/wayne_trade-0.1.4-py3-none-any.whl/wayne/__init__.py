3ea8b44963d8142df58537c771f593ea4e1baac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:31 +0000

    Add package wayne-trade/0.1.4/wayne_trade-0.1.4-py3-none-any.whl

---
+++ packages/wayne-trade/wayne_trade-0.1.4-py3-none-any.whl/wayne/__init__.py
@@ -0,0 +1,8 @@
+"""
+Wayne: Formula to model matrix converter using fiasto-py
+"""
+
+from .trade_formula_for_matrix import trade_formula_for_matrix
+
+__version__ = "0.1.4"
+__all__ = ["trade_formula_for_matrix"]
