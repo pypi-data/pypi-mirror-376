e5b84e3ca0564ca0207cd3e2eebc582557109160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:07:11 +0000

    Add package saiunit/0.1.1/saiunit-0.1.1.tar.gz

---
+++ packages/saiunit/saiunit-0.1.1.tar.gz/saiunit-0.1.1/setup.py
@@ -0,0 +1,39 @@
+# Copyright 2024- BrainPy Ecosystem Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import re
+
+from setuptools import setup
+
+# version
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'saiunit/', '__init__.py'), 'r') as f:
+    init_py = f.read()
+version = re.search('__version__ = "(.*)"', init_py).groups()[0]
+
+# obtain long description from README
+with io.open(os.path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
+    README = f.read()
+
+# setup
+setup(
+    version=version,
+    long_description=README,
+    long_description_content_type="text/markdown",
+)
