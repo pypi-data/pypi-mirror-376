28fb8892154207980807194791c5224456ec776e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 17:07:06 +0000

    Add package wexample-config/0.0.53/wexample_config-0.0.53.tar.gz

---
+++ packages/wexample-config/wexample_config-0.0.53.tar.gz/wexample_config-0.0.53/README.md
@@ -0,0 +1,35 @@
+# wexample-config
+
+Helpers to manage dict types configurations.
+
+Version: 0.0.52
+
+## Requirements
+
+- Python >=3.10
+
+## Dependencies
+
+- attrs>=23.1.0
+- cattrs>=23.1.0
+- pydantic>=2,<3
+- wexample-helpers==0.0.68
+
+## Installation
+
+```bash
+pip install wexample-config
+```
+
+## Links
+
+- Homepage: https://github.com/wexample/python-config
+
+## License
+
+MIT
+## Credits
+
+This package has been developed by [Wexample](https://wexample.com), a collection of tools and utilities to streamline development workflows.
+
+Visit [wexample.com](https://wexample.com) to discover more tools and resources for efficient development.
---
+++ packages/wexample-config/wexample_config-0.0.53.tar.gz/wexample_config-0.0.53/src/wexample_config/config_option/children_config_option.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from types import UnionType
+from typing import TYPE_CHECKING, Any
+
+from wexample_config.config_option.abstract_list_config_option import (
+    AbstractListConfigOption,
+)
+from wexample_helpers.decorator.base_class import base_class
+
+if TYPE_CHECKING:
+    from types import UnionType
+
+
+@base_class
+class ChildrenConfigOption(AbstractListConfigOption):
+    def dump(self) -> Any:
+        output = []
+
+        for child in self.children:
+            output.append(child.dump())
+
+        return output
+
+    def get_item_class_type(self) -> type | UnionType:
+        from wexample_config.config_option.abstract_nested_config_option import (
+            AbstractNestedConfigOption,
+        )
+
+        return AbstractNestedConfigOption
