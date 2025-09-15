de97b02dddf9a8f4bf8bcf348cdd58762a29d39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:07:01 +0000

    Add package echorun/0.2/echorun-0.2.tar.gz

---
+++ packages/echorun/echorun-0.2.tar.gz/echorun-0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='echorun',
+ version='0.2',
+ author="Albert",
+ author_email="echorun@dev.org",
+ description="A simple package for calculating and analyzing the values",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
