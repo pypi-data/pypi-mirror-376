dd2ec71564a8bdb8467e533f1b4a42b0f456faa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 16:06:23 +0000

    Add package tkr-aer-worker/0.1.2/tkr_aer_worker-0.1.2.tar.gz

---
+++ packages/tkr-aer-worker/tkr_aer_worker-0.1.2.tar.gz/tkr_aer_worker-0.1.2/main.py
@@ -0,0 +1,25 @@
+import logging
+from sys import argv
+
+from tierkreis import Worker
+from pytket._tket.circuit import Circuit
+from pytket.backends.backendresult import BackendResult
+from pytket.extensions.qiskit.backends.aer import AerBackend
+
+logger = logging.getLogger(__name__)
+
+worker = Worker("aer_worker")
+
+
+@worker.task()
+def submit(circuits: list[Circuit], n_shots: int) -> list[BackendResult]:
+    return AerBackend().run_circuits(circuits, n_shots=n_shots)
+
+
+@worker.task()
+def submit_single(circuit: Circuit, n_shots: int) -> BackendResult:
+    return AerBackend().run_circuit(circuit, n_shots=n_shots)
+
+
+if __name__ == "__main__":
+    worker.app(argv)
