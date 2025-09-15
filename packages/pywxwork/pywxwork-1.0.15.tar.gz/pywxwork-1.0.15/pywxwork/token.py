b090022bba38567ab176f90c76e457cf41257b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 17:07:15 +0000

    Add package pywxwork/1.0.15/pywxwork-1.0.15.tar.gz

---
+++ packages/pywxwork/pywxwork-1.0.15.tar.gz/pywxwork-1.0.15/pywxwork/token.py
@@ -0,0 +1,35 @@
+from .base import base
+from loguru import logger
+import datetime
+
+
+class token(base):
+    def __init__(self, corpid: str, corpsecret: str) -> None:
+        """init
+
+        Args:
+            corpid (str): 企微corpid
+            corpsecret (str): 企微secret
+        """
+        self.corpid = corpid
+        self.corpsecret = corpsecret
+        response = self.get()
+        self.token = response.get("access_token")
+        self.expires_in = response.get("expires_in")
+        logger.debug(dict(msg="请求的token", token=self.token))
+        self.token_info = response
+
+    def get(self):
+        api_name = "gettoken"
+        params = {
+            "corpid": self.corpid,
+            "corpsecret": self.corpsecret,
+        }
+        response = self.request(api_name=api_name, params=params)
+        return response
+
+    def cache_in(self):
+        pass
+
+    def cache_out(self):
+        pass
