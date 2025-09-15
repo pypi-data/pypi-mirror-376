6601245a7d71a5f3db0645b2e78259f0553bb4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:42 +0000

    Add package beancount/3.2.0/beancount-3.2.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/beancount/beancount-3.2.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/beancount/plugins/__init__.py
@@ -0,0 +1,12 @@
+"""Example plugins for filtering transactions.
+
+These are various examples of how to filter entries in various creative ways.
+
+IMPORTANT: These are not meant to be complete features, rather just experiments
+in problem-solving using Beancount, work-in-progress that can be selectively
+installed via a --plugin option, or one-offs to answer questions on the
+mailing-list.
+"""
+
+__copyright__ = "Copyright (C) 2014, 2016-2017, 2020, 2024  Martin Blais"
+__license__ = "GNU GPLv2"
