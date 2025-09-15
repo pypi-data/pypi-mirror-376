15c1522ff0b6b006f9822656643493e7a1a1a397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 16:06:09 +0000

    Add package robotframework-doc-only/0.1.2/robotframework_doc_only-0.1.2-py3-none-any.whl

---
+++ packages/robotframework-doc-only/robotframework_doc_only-0.1.2-py3-none-any.whl/robot_doc_only/testdoc.py
@@ -0,0 +1,21 @@
+from sys import argv
+from packaging.version import Version
+from robot.testdoc import TestDoc
+
+from robot.version import get_version as get_robot_version
+robot_version = get_robot_version()
+
+if Version(robot_version) < Version('4.0'):
+    from .testdoc_322 import TestDocSmall
+else:
+    from .testdoc_400 import TestDocSmall
+
+def create_testdoc_small():
+    rc = TestDocSmall().execute_cli(argv[1:])
+
+def create_testdoc_full():
+    rc = TestDoc().execute_cli(argv[1:])
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    create_testdoc_small()
