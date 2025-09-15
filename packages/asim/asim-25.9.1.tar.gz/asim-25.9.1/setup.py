3779d813c9bccf78e124edbc3a701d1d1bb36b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:09 +0000

    Add package asim/25.9.1/asim-25.9.1.tar.gz

---
+++ packages/asim/asim-25.9.1.tar.gz/asim-25.9.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asim",
+    version="25.9.1",
+    author="SHUniversity",
+    description="Automatic simulation system for neural networks",
+    python_requires='>=3.12',
+    install_requires=[],
+)
