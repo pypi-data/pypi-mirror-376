c550e7e7f725568de76a4062419c3579377c61c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 04:07:41 +0000

    Add package beancount/3.2.0/beancount-3.2.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/beancount/beancount-3.2.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/beancount/ops/__init__.py
@@ -0,0 +1,7 @@
+"""Operations on the entries defined in the core modules.
+
+This package contains various functions which operate on lists of entries.
+"""
+
+__copyright__ = "Copyright (C) 2013-2017, 2024  Martin Blais"
+__license__ = "GNU GPLv2"
