d8e96c68c6cd89f8263de5f3e62d8b1081d1f7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:06:20 +0000

    Add package dice-apply/0.1.0/dice-apply-0.1.0.tar.gz

---
+++ packages/dice-apply/dice-apply-0.1.0.tar.gz/dice-apply-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Setup script for dice-apply package.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dice-apply",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
