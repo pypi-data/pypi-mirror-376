4ab42ae410b974d0cf1b6ea0ccfa9efeba486df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 16:06:15 +0000

    Add package tgz-message-sdk/0.0.3/tgz_message_sdk-0.0.3.tar.gz

---
+++ packages/tgz-message-sdk/tgz_message_sdk-0.0.3.tar.gz/tgz_message_sdk-0.0.3/tgz_message/delivery/broadcast/whatsapp/__init__.py
@@ -0,0 +1,26 @@
+from ...broadcast import BroadCastDelivery
+
+
+class WhatsappDirect(BroadCastDelivery):
+
+    def __init__(
+            self,
+            event: str,
+            recipients: list[str],
+            params: dict
+    ):
+        super().__init__(
+            event,
+            recipients,
+            params
+        )
+
+        if params is None:
+            self.params = {}
+
+    def build_payload(self) -> dict:
+        return {
+            "event": self.event,
+            "recipients": self.recipients,
+            "params": self.params
+        }
---
+++ packages/tgz-message-sdk/tgz_message_sdk-0.0.3.tar.gz/tgz_message_sdk-0.0.3/tgz_message/delivery/direct/whatsapp/__init__.py
@@ -0,0 +1,26 @@
+from ...direct import DirectDelivery
+
+
+class WhatsappDirect(DirectDelivery):
+
+    def __init__(
+            self,
+            event: str,
+            recipient: str,
+            params: dict
+    ):
+        super().__init__(
+            event,
+            recipient,
+            params
+        )
+
+        if params is None:
+            self.params = {}
+
+    def build_payload(self) -> dict:
+        return {
+            "event": self.event,
+            "recipient": self.recipient,
+            "params": self.params
+        }
