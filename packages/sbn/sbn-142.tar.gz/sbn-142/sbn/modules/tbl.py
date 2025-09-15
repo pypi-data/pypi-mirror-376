a3985bf451a01fdd32d87739ec57837657bf3239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:50 +0000

    Add package sbn/142/sbn-142.tar.gz

---
+++ packages/sbn/sbn-142.tar.gz/sbn-142/sbn/modules/tbl.py
@@ -0,0 +1,18 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "pth": "pth",
+    "req": "req",
+    "slg": "slg"
+}
+
+MD5 = {
+    "pth": "c40e295d52a782524f4dbd0b629f5852",
+    "req": "793704d64443c4f19512924e294943ea",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+}
