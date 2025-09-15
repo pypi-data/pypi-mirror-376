417726fe99d518aeab4576d81e7bafcbd9fc4767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:07:02 +0000

    Add package echoapp/0.1/echoapp-0.1.tar.gz

---
+++ packages/echoapp/echoapp-0.1.tar.gz/echoapp-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+name='echoapp',
+ version='0.1',
+ author="C",
+ author_email="pandos@python.com",
+ description="Powerful data structures for data analysis, time series, and statistics",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
