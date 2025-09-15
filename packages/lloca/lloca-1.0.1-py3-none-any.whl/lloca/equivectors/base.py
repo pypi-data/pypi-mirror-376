63bc28f961cdfac275a6d0c294f5636bd3bbb290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:34 +0000

    Add package lloca/1.0.1/lloca-1.0.1-py3-none-any.whl

---
+++ packages/lloca/lloca-1.0.1-py3-none-any.whl/lloca/equivectors/base.py
@@ -0,0 +1,12 @@
+from torch import nn
+
+
+class EquiVectors(nn.Module):
+    """Abstract class for equivariantly predicting vectors
+    based on fourmomenta and scalars"""
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, fourmomenta, scalars, *args, **kwargs):
+        raise NotImplementedError
