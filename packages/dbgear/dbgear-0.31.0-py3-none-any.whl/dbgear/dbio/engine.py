86224c3175f82e5ccbdb5c6f79d9064ae12c6bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:06:13 +0000

    Add package dbgear/0.31.0/dbgear-0.31.0-py3-none-any.whl

---
+++ packages/dbgear/dbgear-0.31.0-py3-none-any.whl/dbgear/dbio/engine.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+def _get_engine(conn):
+    return create_engine(conn, echo=False)
+
+
+def get_connection(conn):
+    engine = _get_engine(conn)
+    return engine.connect()
+
+
+def execute(conn, sql, params=None):
+    return conn.execute(text(sql), params)
+
+
+def select_all(conn, sql, params=None):
+    return execute(conn, sql, params).mappings().fetchall()
+
+
+def select_one(conn, sql, params=None):
+    return execute(conn, sql, params).mappings().fetchone()
+
+
+def commit(conn):
+    conn.commit()
