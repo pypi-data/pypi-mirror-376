74becf5a0ca3b4594e9eea256cb03c5049f65940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:07:26 +0000

    Add package gisl-data-library/0.1.4/gisl_data_library-0.1.4.tar.gz

---
+++ packages/gisl-data-library/gisl_data_library-0.1.4.tar.gz/gisl_data_library-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gisl-data-library',
+    version='0.1.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'gisl_data_library': ['gisl_data.json'],
+    },
+    install_requires=[],
+    author='Imran Bin Gifary (System Delta or Imran Delta Online)',
+    author_email='imran.sdelta@gmail.com',
+    description='A static library of Genshin item/char info. (WIP) - Albedo added.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Imran-Delta/GI-Static-Data-Library',
+)
