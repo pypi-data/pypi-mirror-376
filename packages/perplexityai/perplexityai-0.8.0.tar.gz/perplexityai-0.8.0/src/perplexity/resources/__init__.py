d6c7c9b2bb41c1f7cf19724ff421b3b4ba604bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:06:19 +0000

    Add package perplexityai/0.8.0/perplexityai-0.8.0.tar.gz

---
+++ packages/perplexityai/perplexityai-0.8.0.tar.gz/perplexityai-0.8.0/src/perplexity/resources/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .chat import (
+    ChatResource,
+    AsyncChatResource,
+    ChatResourceWithRawResponse,
+    AsyncChatResourceWithRawResponse,
+    ChatResourceWithStreamingResponse,
+    AsyncChatResourceWithStreamingResponse,
+)
+from .async_ import (
+    AsyncResource,
+    AsyncAsyncResource,
+    AsyncResourceWithRawResponse,
+    AsyncAsyncResourceWithRawResponse,
+    AsyncResourceWithStreamingResponse,
+    AsyncAsyncResourceWithStreamingResponse,
+)
+from .search import (
+    SearchResource,
+    AsyncSearchResource,
+    SearchResourceWithRawResponse,
+    AsyncSearchResourceWithRawResponse,
+    SearchResourceWithStreamingResponse,
+    AsyncSearchResourceWithStreamingResponse,
+)
+from .content import (
+    ContentResource,
+    AsyncContentResource,
+    ContentResourceWithRawResponse,
+    AsyncContentResourceWithRawResponse,
+    ContentResourceWithStreamingResponse,
+    AsyncContentResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ChatResource",
+    "AsyncChatResource",
+    "ChatResourceWithRawResponse",
+    "AsyncChatResourceWithRawResponse",
+    "ChatResourceWithStreamingResponse",
+    "AsyncChatResourceWithStreamingResponse",
+    "AsyncResource",
+    "AsyncAsyncResource",
+    "AsyncResourceWithRawResponse",
+    "AsyncAsyncResourceWithRawResponse",
+    "AsyncResourceWithStreamingResponse",
+    "AsyncAsyncResourceWithStreamingResponse",
+    "SearchResource",
+    "AsyncSearchResource",
+    "SearchResourceWithRawResponse",
+    "AsyncSearchResourceWithRawResponse",
+    "SearchResourceWithStreamingResponse",
+    "AsyncSearchResourceWithStreamingResponse",
+    "ContentResource",
+    "AsyncContentResource",
+    "ContentResourceWithRawResponse",
+    "AsyncContentResourceWithRawResponse",
+    "ContentResourceWithStreamingResponse",
+    "AsyncContentResourceWithStreamingResponse",
+]
---
+++ packages/perplexityai/perplexityai-0.8.0.tar.gz/perplexityai-0.8.0/src/perplexity/types/__init__.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    Choice as Choice,
+    UsageInfo as UsageInfo,
+    ChatMessageInput as ChatMessageInput,
+    ChatMessageOutput as ChatMessageOutput,
+    APIPublicSearchResult as APIPublicSearchResult,
+)
+from .search_create_params import SearchCreateParams as SearchCreateParams
+from .content_create_params import ContentCreateParams as ContentCreateParams
+from .search_create_response import SearchCreateResponse as SearchCreateResponse
+from .content_create_response import ContentCreateResponse as ContentCreateResponse
