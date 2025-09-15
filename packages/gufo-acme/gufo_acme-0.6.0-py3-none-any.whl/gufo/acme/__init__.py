d1fe05a9803d05487421584dbcab13ee563cc3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:22:34 +0000

    Add package gufo-acme/0.6.0/gufo_acme-0.6.0-py3-none-any.whl

---
+++ packages/gufo-acme/gufo_acme-0.6.0-py3-none-any.whl/gufo/acme/__init__.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------------------
+# Gufo ACME
+# ---------------------------------------------------------------------
+# Copyright (C) 2023-25, Gufo Labs
+# ---------------------------------------------------------------------
+"""
+An Python asyncio ACME client.
+
+Attributes:
+    __version__: Gufo ACME version.
+
+The package consists of the following modules:
+
+* [acme][gufo.acme.acme] - ACME protocol messages.
+* [clients][gufo.acme.clients] - Various ACME clients imlementations.
+* [error][gufo.acme.error] - Error classes.
+* [log][gufo.acme.log] - Logging utilities.
+* [types][gufo.acme.types] - Package public types.
+"""
+
+__version__ = "0.6.0"
---
+++ packages/gufo-acme/gufo_acme-0.6.0-py3-none-any.whl/gufo/acme/log.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------
+# Gufo ACME: Logging
+# ---------------------------------------------------------------------
+# Copyright (C) 2023, Gufo Labs
+# ---------------------------------------------------------------------
+
+"""
+logging utilities.
+
+Attributes:
+    logger: Gufo ACME logger.
+"""
+
+# Python modules
+from logging import getLogger
+
+logger = getLogger("gufo.acme")
