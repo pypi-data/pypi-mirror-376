ae4aca21c56e607b9c3f022592001941ffb4b227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:23:02 +0000

    Add package spiegel-lib/0.1.0/spiegel_lib-0.1.0-py3-none-any.whl

---
+++ packages/spiegel-lib/spiegel_lib-0.1.0-py3-none-any.whl/cleaning/__init__.py
@@ -0,0 +1,3 @@
+from .ukdata_cleaning import compare_dataframes, merge_csv_files, set_location, split_month_year
+
+__all__ = ["compare_dataframes", "merge_csv_files", "set_location", "split_month_year"]
