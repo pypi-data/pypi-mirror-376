680c69f693df5c85b1b2d7cff46972b582a2f690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 03:23:20 +0000

    Add package mkast/2.0.0/mkast-2.0.0.tar.gz

---
+++ packages/mkast/mkast-2.0.0.tar.gz/mkast-2.0.0/src/mkast/__init__.py
@@ -0,0 +1,12 @@
+# ruff: noqa: E402,F401
+
+from .__version__ import __version__
+
+# load input files and  their config
+from .cli import load_input
+# load configs, config types
+from .cfg import read_config, Config, Modifier, ModifierKey
+# generate the ast, manage custom emitters
+from .mkast import generate_ast, register_emitter, unregister_emitter, get_emitter
+# emitter type
+from .domain import Emitter
