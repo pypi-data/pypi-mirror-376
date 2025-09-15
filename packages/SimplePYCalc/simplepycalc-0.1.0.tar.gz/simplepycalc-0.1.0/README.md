5827ef1140586ab36b8a30583f0a3aa1c2b835b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 14:07:01 +0000

    Add package SimplePYCalc/0.1.0/simplepycalc-0.1.0.tar.gz

---
+++ packages/SimplePYCalc/simplepycalc-0.1.0.tar.gz/simplepycalc-0.1.0/README.md
@@ -0,0 +1,21 @@
+# SimpleCalc
+A simple, easy-to-use Python package for basic arithmetic.
+
+## Installation
+You can install this package using `pip`:
+```bash
+pip install SimplePYCalc
+```
+
+## Usage
+Here's how to use the functions in this package:
+```python
+from simplecalc import add, subtract
+
+# Example usage
+print(f"2 + 2 = {add(2, 2)}")
+print(f"5 - 3 = {subtract(5, 3)}")
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ packages/SimplePYCalc/simplepycalc-0.1.0.tar.gz/simplepycalc-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimplePYCalc',
+    version='0.1.0',
+    description='A simple calculator package for basic arithmetic.',
+    long_description_content_type='text/markdown',
+    author='randomfnp',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[],
+)
