5cdc31419cdc9c9e872860281e1b163cc3606398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 15:07:23 +0000

    Add package aixtools/0.1.3/aixtools-0.1.3.tar.gz

---
+++ packages/aixtools/aixtools-0.1.3.tar.gz/aixtools-0.1.3/aixtools/a2a/__init__.py
@@ -0,0 +1,5 @@
+"""A2A (Agent-to-Agent) communication utilities."""
+
+from .utils import fetch_agent_card, task, text_message, tool2skill
+
+__all__ = ["fetch_agent_card", "task", "text_message", "tool2skill"]
