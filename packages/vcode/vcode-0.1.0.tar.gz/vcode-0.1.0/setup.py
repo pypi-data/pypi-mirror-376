9b6fbe1b77fea49b043fce3ef80e00a654f60786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-15 06:05:50 +0000

    Add package vcode/0.1.0/vcode-0.1.0.tar.gz

---
+++ packages/vcode/vcode-0.1.0.tar.gz/vcode-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vcode',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='vCode is a coding agent runs in your terminal.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
