f85ae868134df60448c103c6c2010b7605409319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:05:53 +0000

    Add package list-ops/0.1.2/list_ops-0.1.2.tar.gz

---
+++ packages/list-ops/list_ops-0.1.2.tar.gz/list_ops-0.1.2/src/list_ops/__init__.py
@@ -0,0 +1,9 @@
+"""Your library description."""
+
+__version__ = "0.1.1"
+
+
+# import the functions
+from .main_code import get_text_index
+from .main_code import get_prev_text
+from .main_code import get_next_text
