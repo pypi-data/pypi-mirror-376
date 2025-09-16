4c3ebf72d13cf86d20f05dc302046ca04ce12ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:15:18 +0000

    Add package databricks-connect/14.3.12/databricks_connect-14.3.12-py2.py3-none-any.whl

---
+++ packages/databricks-connect/databricks_connect-14.3.12-py2.py3-none-any.whl/pyspark/dbconnect_version.py
@@ -0,0 +1,23 @@
+#
+# DATABRICKS CONFIDENTIAL & PROPRIETARY
+# __________________
+#
+# Copyright 2020-present Databricks, Inc.
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains the property of Databricks, Inc.
+# and its suppliers, if any.  The intellectual and technical concepts contained herein are
+# proprietary to Databricks, Inc. and its suppliers and may be covered by U.S. and foreign Patents,
+# patents in process, and are protected by trade secret and/or copyright law. Dissemination, use,
+# or reproduction of this information is strictly forbidden unless prior written permission is
+# obtained from Databricks, Inc.
+#
+# If you view or obtain a copy of this information and believe Databricks, Inc. may not have
+# intended it to be made available, please promptly report it to Databricks Legal Department
+# @ legal@databricks.com.
+#
+
+# The below are updated during build (dbconnect/python/build-dist-v2.sh)
+
+__dbconnect_version__ = "14.3.12"
+__git_version__ = "6c8bb2e4540c073c63d3b7ba2d3539942b83a8ce"
