bcf0c39825e5428923bcc32302a5dbf7bff42a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:15:40 +0000

    Add package libucx-cu12/1.19.0/libucx_cu12-1.19.0-py3-none-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/libucx-cu12/libucx_cu12-1.19.0-py3-none-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/libucx/lib/cmake/ucx/ucx-targets.cmake
@@ -0,0 +1,35 @@
+#
+# Copyright (c) NVIDIA CORPORATION & AFFILIATES, 2021. ALL RIGHTS RESERVED.
+#
+# See file LICENSE for terms.
+#
+
+get_filename_component(prefix "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)
+set(exec_prefix "${prefix}")
+
+if(NOT TARGET ucx::ucs)
+  add_library(ucx::ucs SHARED IMPORTED)
+
+  set_target_properties(ucx::ucs PROPERTIES
+    IMPORTED_LOCATION "${exec_prefix}/lib/libucs.so"
+    INTERFACE_INCLUDE_DIRECTORIES "${prefix}/include"
+  )
+endif()
+
+if(NOT TARGET ucx::ucp)
+  add_library(ucx::ucp SHARED IMPORTED)
+
+  set_target_properties(ucx::ucp PROPERTIES
+    IMPORTED_LOCATION "${exec_prefix}/lib/libucp.so"
+    INTERFACE_INCLUDE_DIRECTORIES "${prefix}/include"
+  )
+endif()
+
+if(NOT TARGET ucx::uct)
+  add_library(ucx::uct SHARED IMPORTED)
+
+  set_target_properties(ucx::uct PROPERTIES
+    IMPORTED_LOCATION "${exec_prefix}/lib/libuct.so"
+    INTERFACE_INCLUDE_DIRECTORIES "${prefix}/include"
+  )
+endif()
