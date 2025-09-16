d7fc5abd26640cb8137dc438eb69be4b0fae4afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:05:43 +0000

    Add package zih-models/0.3.0/zih_models-0.3.0.tar.gz

---
+++ packages/zih-models/zih_models-0.3.0.tar.gz/zih_models-0.3.0/zih_models/tkdb/history_models/user.py
@@ -0,0 +1,45 @@
+"""User history schema"""
+
+from typing import override
+
+from .base import (
+    BaseDeleteSchemaModel,
+    BaseDiffSchemaModel,
+    BaseSchemaModel,
+    FieldDiff,
+    FunctionalNumber,
+    PersonalNumber,
+)
+
+"""
+!!! NEVER Change the type of a Column/Attribute of this models so the history never breaks !!!
+"""
+
+
+class User(BaseSchemaModel):
+
+    uid: str
+    personal_numbers: list[PersonalNumber]
+    functional_numbers: list[FunctionalNumber]
+
+    table = "users_v2"
+
+    @override
+    def get_user_logins(self) -> set[str]:
+        logins = super().get_user_logins()
+        logins.add(self.uid)
+        return logins
+
+
+class UserDiff(BaseDiffSchemaModel):
+
+    personal_numbers: FieldDiff[list[PersonalNumber]] = None
+    functional_numbers: FieldDiff[list[str]] = None
+
+    table = "users_v2"
+
+
+class UserDelete(BaseDeleteSchemaModel):
+    """delete model"""
+
+    table = "users_v2"
