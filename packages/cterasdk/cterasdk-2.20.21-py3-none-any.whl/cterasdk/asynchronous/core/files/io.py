513c1ef1a7dfcc62256a8d1806d4207e17688529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:06:05 +0000

    Add package cterasdk/2.20.21/cterasdk-2.20.21-py3-none-any.whl

---
+++ packages/cterasdk/cterasdk-2.20.21-py3-none-any.whl/cterasdk/asynchronous/core/files/io.py
@@ -0,0 +1,181 @@
+import logging
+from ....cio.common import encode_request_parameter
+from ....cio import core as fs
+from ....exceptions.io import ResourceNotFoundError, ResourceExistsError
+from .. import query
+
+
+logger = logging.getLogger('cterasdk.core')
+
+
+async def listdir(core, path, depth=None, include_deleted=False, search_criteria=None, limit=None):
+    with fs.fetch_resources(path, depth, include_deleted, search_criteria, limit) as param:
+        if param.depth > 0:
+            return query.iterator(core, '', param, 'fetchResources', callback_response=fs.FetchResourcesResponse)
+        return await core.v1.api.execute('', 'fetchResources', param)
+
+
+async def exists(core, path):
+    present, *_ = await metadata(core, path, suppress_error=True)
+    return present
+
+
+async def metadata(core, path, suppress_error=False):
+    """
+    Get item metadata.
+
+    :returns: A tuple indicating if a file exists, and its metadata
+    """
+    response = await listdir(core, path, 0)
+    if response.root is None:
+        if not suppress_error:
+            raise ResourceNotFoundError(path.absolute)
+        return False, None
+    return True, response.root
+
+
+async def versions(core, path):
+    with fs.versions(path):
+        return await core.v1.api.execute('', 'listSnapshots', path.absolute)
+
+
+async def walk(core, scope, path, include_deleted=False):
+    target = fs.CorePath.instance(scope, path)
+    await ensure_directory(core, target)
+    paths = [fs.CorePath.instance(scope, path)]
+    while len(paths) > 0:
+        path = paths.pop(0)
+        entries = await listdir(core, path, include_deleted=include_deleted)
+        async for e in entries:
+            if e.isFolder:
+                paths.append(fs.CorePath.instance(scope, e))
+            yield e
+
+
+async def mkdir(core, path):
+    with fs.makedir(path) as param:
+        response = await core.v1.api.execute('', 'makeCollection', param)
+    fs.accept_error(response)
+
+
+async def makedirs(core, path):
+    directories = path.parts
+    for i in range(1, len(directories) + 1):
+        path = fs.CorePath.instance(path.scope, '/'.join(directories[:i]))
+        try:
+            await mkdir(core, path)
+        except ResourceExistsError:
+            logger.debug('Resource already exists: %s', path.reference.as_posix())
+
+
+async def rename(core, path, name):
+    with fs.rename(path, name) as param:
+        return await core.v1.api.execute('', 'moveResources', param)
+
+
+async def remove(core, *paths):
+    with fs.delete(*paths) as param:
+        return await core.v1.api.execute('', 'deleteResources', param)
+
+
+async def recover(core, *paths):
+    with fs.recover(*paths) as param:
+        return await core.v1.api.execute('', 'restoreResources', param)
+
+
+async def copy(core, *paths, destination=None, resolver=None, cursor=None):
+    with fs.copy(*paths, destination=destination, resolver=resolver, cursor=cursor) as param:
+        return await core.v1.api.execute('', 'copyResources', param)
+
+
+async def move(core, *paths, destination=None, resolver=None, cursor=None):
+    with fs.move(*paths, destination=destination, resolver=resolver, cursor=cursor) as param:
+        return await core.v1.api.execute('', 'moveResources', param)
+
+
+async def ensure_directory(core, directory, suppress_error=False):
+    present, resource = await metadata(core, directory, suppress_error=True)
+    fs.ensure_directory(present, resource, directory, suppress_error)
+    return resource.isFolder if present else False, resource
+
+
+def handle(path):
+    """
+    Create function to retrieve file handle.
+
+    :param cterasdk.cio.edge.CorePath path: Path to file.
+    :returns: Callable function to retrieve file handle.
+    :rtype: callable
+    """
+    async def wrapper(core):
+        """
+        Get file handle.
+
+        :param cterasdk.objects.synchronous.core.Portal core: Portal object.
+        """
+        with fs.handle(path) as param:
+            return await core.io.download(param)
+    return wrapper
+
+
+def handle_many(directory, *objects):
+    """
+    Create function to retrieve zip archive
+
+    :param cterasdk.cio.edge.CorePath directory: Path to directory.
+    :param args objects: List of files and folders.
+    :returns: Callable function to retrieve file handle.
+    :rtype: callable
+    """
+    async def wrapper(core):
+        """
+        Upload file from metadata and file handle.
+
+        :param cterasdk.objects.synchronous.core.Portal core: Portal object.
+        :param str name: File name.
+        :param object handle: File handle.
+        """
+        with fs.handle_many(directory, objects) as param:
+            _, resource = await ensure_directory(core, directory)
+            return await core.io.download_zip(str(resource.cloudFolderInfo.uid), encode_request_parameter(param))
+    return wrapper
+
+
+async def _validate_destination(core, name, destination):
+    is_dir, resource = await ensure_directory(core, destination, suppress_error=True)
+    if not is_dir:
+        is_dir, resource = await ensure_directory(core, destination.parent)
+        fs.ensure_writeable(resource, destination.parent)
+        return resource.cloudFolderInfo.uid, destination.name, destination.parent
+    fs.ensure_writeable(resource, destination)
+    return resource.cloudFolderInfo.uid, name, destination
+
+
+def upload(name, size, destination, fd):
+    """
+    Create upload function
+
+    :param str name: File name.
+    :param cterasdk.cio.core.CorePath destination: Path to directory.
+    :param object fd: File handle.
+    :returns: Callable function to start the upload.
+    :rtype: callable
+    """
+    fs.destination_prerequisite_conditions(destination, name)
+
+    async def wrapper(core):
+        """
+        Upload file from metadata and file handle.
+
+        :param cterasdk.objects.synchronous.core.Portal core: Portal object.
+        """
+        uid, filename, directory = await _validate_destination(core, name, destination)
+        with fs.upload(core, filename, directory, size, fd) as param:
+            return fs.validate_transfer_success(await core.io.upload(str(uid), param), destination.join(name).reference.as_posix())
+    return wrapper
+
+
+async def public_link(core, path, access, expire_in):
+    with fs.public_link(path, access, expire_in) as param:
+        response = await core.v1.api.execute('', 'createShare', param)
+    return response.publicLink
---
+++ packages/cterasdk/cterasdk-2.20.21-py3-none-any.whl/cterasdk/cio/edge.py
@@ -0,0 +1,153 @@
+import logging
+from datetime import datetime
+from contextlib import contextmanager
+from pathlib import Path
+from ..common import Object
+from ..objects.uri import unquote
+from . import common
+from ..exceptions.transport import HTTPError
+from ..exceptions.io import ResourceExistsError, RestrictedPathError, RemoteStorageError
+
+
+logger = logging.getLogger('cterasdk.edge')
+
+
+class EdgePath(common.BasePath):
+    """Path for CTERA Edge Filer"""
+
+    def __init__(self, scope, reference):
+        """
+        Initialize a CTERA Edge Filer Path.
+
+        :param str scope: Scope.
+        :param str reference: Reference.
+        """
+        if isinstance(reference, Object):
+            super().__init__(scope, reference.path)
+        elif isinstance(reference, str):
+            super().__init__(scope, reference)
+        elif reference is None:
+            super().__init__(scope, '')
+        else:
+            message = 'Path validation failed: ensure the path exists and is correctly formatted.'
+            logger.error(message)
+            raise ValueError(message)
+
+    @staticmethod
+    def instance(scope, reference):
+        return EdgePath(scope, reference)
+
+
+def fetch_reference(href):
+    namespace = '/localFiles'
+    return unquote(href[href.index(namespace)+len(namespace) + 1:])
+
+
+def format_listdir_response(parent, response):
+    entries = []
+    for e in response:
+        path = fetch_reference(e.href)
+        if path and parent != path:
+            is_dir = e.getcontenttype == 'httpd/unix-directory'
+            param = Object(
+                path=path,
+                name=Path(path).name,
+                is_dir=is_dir,
+                is_file=not is_dir,
+                created_at=e.creationdate,
+                last_modified=datetime.strptime(e.getlastmodified, "%a, %d %b %Y %H:%M:%S GMT").isoformat(),
+                size=e.getcontentlength
+            )
+            entries.append(param)
+    return entries
+
+
+@contextmanager
+def makedir(path):
+    directory = path.absolute
+    logger.info('Creating directory: %s', path.absolute)
+    try:
+        yield path.absolute
+    except HTTPError as error:
+        try:
+            accept_error(error.error.response.error.msg, path=directory)
+        except ResourceExistsError:
+            logger.info('Directory already exists: %s', directory)
+    logger.info('Directory created: %s', directory)
+
+
+@contextmanager
+def copy(path, destination):
+    destination = destination.join(path.name).absolute
+    logger.info('Copying: %s to: %s', path.absolute, destination)
+    yield path.absolute, destination
+
+
+@contextmanager
+def move(path, destination):
+    destination = destination.join(path.name).absolute
+    logger.info('Moving: %s to: %s', path.absolute, destination)
+    yield path.absolute, destination
+
+
+def delete_generator(*paths):
+    for path in paths:
+        logger.info('Deleting item: %s', path.absolute)
+        yield path
+
+
+@contextmanager
+def handle(path):
+    logger.info('Getting file handle: %s', path.reference)
+    yield path.absolute
+
+
+@contextmanager
+def handle_many(directory, objects):
+    param = Object()
+    param.paths = ['/'.join([directory.absolute, item]) for item in objects]
+    param.snapshot = Object()
+    param._classname = 'BackupRepository'  # pylint: disable=protected-access
+    param.snapshot.location = 1
+    param.snapshot.timestamp = None
+    param.snapshot.path = None
+    logger.info('Getting directory handle: %s', directory.reference)
+    yield param
+
+
+@contextmanager
+def upload(name, destination, fd):
+    fd, *_ = common.encode_stream(fd, 0)
+    param = dict(
+        name=name,
+        fullpath=f'{destination.absolute}/{name}',
+        filedata=fd
+    )
+    logger.info('Uploading: %s to: %s', name, destination.reference)
+    yield param
+
+
+file_access_errors = {
+    "File exists": ResourceExistsError,
+    "Creating a folder in this location is forbidden": RestrictedPathError
+}
+
+
+def accept_error(error_message, **kwargs):
+    """
+    Check if response contains an error.
+    """
+    if error_message not in ['OK']:
+        exception_classname = file_access_errors.get(error_message, RemoteStorageError)
+        try:
+            if exception_classname:
+                raise exception_classname(**kwargs)
+        except ResourceExistsError as error:
+            logger.warning('Resource already exists: a file or folder with this name already exists.')
+            raise error
+        except RestrictedPathError as error:
+            logger.error('Creating a folder in the specified location is forbidden.')
+            raise error
+        except RemoteStorageError as error:
+            logger.error('An error occurred while performing operation.')
+            raise error
