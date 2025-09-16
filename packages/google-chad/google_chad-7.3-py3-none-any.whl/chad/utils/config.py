ed16822aee73e5d5f55a098c00dd878dd8643259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:16:01 +0000

    Add package google-chad/7.3/google_chad-7.3-py3-none-any.whl

---
+++ packages/google-chad/google_chad-7.3-py3-none-any.whl/chad/utils/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+APP_VERSION = "v7.3"
+
+def banner():
+	"""
+	Display the banner.
+	"""
+	print("#########################################################################")
+	print("#                                                                       #")
+	print("#                               Chad v7.3                               #")
+	print("#                                                                       #")
+	print("# Search Google Dorks like Chad.                                        #")
+	print("# GitHub repository at github.com/ivan-sincek/chad.                     #")
+	print("#                                                                       #")
+	print("#########################################################################")
