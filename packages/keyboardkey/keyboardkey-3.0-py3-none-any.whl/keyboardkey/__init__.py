feb4b8d349485cb0896a2627c4d927a3f4e35b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:05:40 +0000

    Add package keyboardkey/3.0/keyboardkey-3.0-py3-none-any.whl

---
+++ packages/keyboardkey/keyboardkey-3.0-py3-none-any.whl/keyboardkey/__init__.py
@@ -0,0 +1,4 @@
+from .keys import sletters, cletters, numbers, punctuations, functions, controls, system, arrows, everykey
+
+__all__ = ["sletters", "cletters", "numbers", "punctuations", "functions", "controls", "system", "arrows", "everykey"]
+__version__ = "3.0"
