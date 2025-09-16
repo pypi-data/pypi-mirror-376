87f8095429ecb5344e018f6c0d49ab53495ff24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:15:56 +0000

    Add package octicons-pack/19.18.0/octicons_pack-19.18.0.tar.gz

---
+++ packages/octicons-pack/octicons_pack-19.18.0.tar.gz/octicons_pack-19.18.0/crates/octicons/src/py_binding/part_00.rs
@@ -0,0 +1,225 @@
+// This file was generated. DO NOT EDIT.
+use crate::icons;
+use pyo3::prelude::*;
+
+pub(super) fn bind_part_0(m: &Bound<PyModule>) -> PyResult<()> {
+    m.add("HOME_FILL_16", icons::HOME_FILL_16)?;
+    m.add("TOOLS_24", icons::TOOLS_24)?;
+    m.add("MULTI_SELECT_24", icons::MULTI_SELECT_24)?;
+    m.add("REPO_FORKED_16", icons::REPO_FORKED_16)?;
+    m.add("MOON_24", icons::MOON_24)?;
+    m.add("FISCAL_HOST_24", icons::FISCAL_HOST_24)?;
+    m.add("GIT_PULL_REQUEST_24", icons::GIT_PULL_REQUEST_24)?;
+    m.add("DOWNLOAD_24", icons::DOWNLOAD_24)?;
+    m.add("CIRCLE_SLASH_16", icons::CIRCLE_SLASH_16)?;
+    m.add("REPO_DELETE_24", icons::REPO_DELETE_24)?;
+    m.add("STOP_16", icons::STOP_16)?;
+    m.add("REPO_16", icons::REPO_16)?;
+    m.add("FOLD_24", icons::FOLD_24)?;
+    m.add(
+        "FEED_PULL_REQUEST_DRAFT_16",
+        icons::FEED_PULL_REQUEST_DRAFT_16,
+    )?;
+    m.add("SLIDERS_16", icons::SLIDERS_16)?;
+    m.add("KEY_16", icons::KEY_16)?;
+    m.add("SQUIRREL_16", icons::SQUIRREL_16)?;
+    m.add("CLOUD_16", icons::CLOUD_16)?;
+    m.add("SIGN_IN_16", icons::SIGN_IN_16)?;
+    m.add("FOLD_16", icons::FOLD_16)?;
+    m.add("STRIKETHROUGH_16", icons::STRIKETHROUGH_16)?;
+    m.add("DEVICES_16", icons::DEVICES_16)?;
+    m.add("X_CIRCLE_FILL_12", icons::X_CIRCLE_FILL_12)?;
+    m.add("TAB_24", icons::TAB_24)?;
+    m.add("ALERT_16", icons::ALERT_16)?;
+    m.add("FEED_ISSUE_REOPEN_16", icons::FEED_ISSUE_REOPEN_16)?;
+    m.add("TRIANGLE_DOWN_24", icons::TRIANGLE_DOWN_24)?;
+    m.add("FOLD_UP_16", icons::FOLD_UP_16)?;
+    m.add("UNREAD_16", icons::UNREAD_16)?;
+    m.add("REDO_16", icons::REDO_16)?;
+    m.add(
+        "FILE_DIRECTORY_SYMLINK_24",
+        icons::FILE_DIRECTORY_SYMLINK_24,
+    )?;
+    m.add("DEVICES_24", icons::DEVICES_24)?;
+    m.add("REPO_24", icons::REPO_24)?;
+    m.add("UNDO_24", icons::UNDO_24)?;
+    m.add("MCP_24", icons::MCP_24)?;
+    m.add("DISCUSSION_DUPLICATE_24", icons::DISCUSSION_DUPLICATE_24)?;
+    m.add("MOVE_TO_TOP_16", icons::MOVE_TO_TOP_16)?;
+    m.add("TASKLIST_16", icons::TASKLIST_16)?;
+    m.add("CONTAINER_24", icons::CONTAINER_24)?;
+    m.add("CHEVRON_UP_16", icons::CHEVRON_UP_16)?;
+    m.add("PULSE_16", icons::PULSE_16)?;
+    m.add("UPLOAD_24", icons::UPLOAD_24)?;
+    m.add("TABLE_16", icons::TABLE_16)?;
+    m.add("READ_16", icons::READ_16)?;
+    m.add("SPARKLES_FILL_24", icons::SPARKLES_FILL_24)?;
+    m.add("DISCUSSION_CLOSED_24", icons::DISCUSSION_CLOSED_24)?;
+    m.add("ARROW_UP_16", icons::ARROW_UP_16)?;
+    m.add("BROADCAST_16", icons::BROADCAST_16)?;
+    m.add("CHECK_16", icons::CHECK_16)?;
+    m.add("ISSUE_TRACKS_24", icons::ISSUE_TRACKS_24)?;
+    m.add("LOG_24", icons::LOG_24)?;
+    m.add("SUN_16", icons::SUN_16)?;
+    m.add("PASTE_24", icons::PASTE_24)?;
+    m.add("WEBHOOK_16", icons::WEBHOOK_16)?;
+    m.add(
+        "GIT_PULL_REQUEST_DRAFT_24",
+        icons::GIT_PULL_REQUEST_DRAFT_24,
+    )?;
+    m.add("MORTAR_BOARD_24", icons::MORTAR_BOARD_24)?;
+    m.add("GIT_COMPARE_24", icons::GIT_COMPARE_24)?;
+    m.add(
+        "FILE_DIRECTORY_OPEN_FILL_16",
+        icons::FILE_DIRECTORY_OPEN_FILL_16,
+    )?;
+    m.add("HOME_FILL_24", icons::HOME_FILL_24)?;
+    m.add(
+        "TRACKED_BY_CLOSED_NOT_PLANNED_24",
+        icons::TRACKED_BY_CLOSED_NOT_PLANNED_24,
+    )?;
+    m.add("REPO_PULL_24", icons::REPO_PULL_24)?;
+    m.add("FEED_PERSON_16", icons::FEED_PERSON_16)?;
+    m.add("DIFF_RENAMED_24", icons::DIFF_RENAMED_24)?;
+    m.add("SKIP_16", icons::SKIP_16)?;
+    m.add("PLAY_24", icons::PLAY_24)?;
+    m.add("ITALIC_16", icons::ITALIC_16)?;
+    m.add("UNFOLD_16", icons::UNFOLD_16)?;
+    m.add("ALERT_FILL_12", icons::ALERT_FILL_12)?;
+    m.add("EYE_CLOSED_16", icons::EYE_CLOSED_16)?;
+    m.add("PACKAGE_DEPENDENCIES_16", icons::PACKAGE_DEPENDENCIES_16)?;
+    m.add("DEVICE_DESKTOP_16", icons::DEVICE_DESKTOP_16)?;
+    m.add("EYE_16", icons::EYE_16)?;
+    m.add("SYNC_24", icons::SYNC_24)?;
+    m.add("DEVICE_CAMERA_24", icons::DEVICE_CAMERA_24)?;
+    m.add("TAB_EXTERNAL_16", icons::TAB_EXTERNAL_16)?;
+    m.add("REPO_PULL_16", icons::REPO_PULL_16)?;
+    m.add("CHEVRON_DOWN_24", icons::CHEVRON_DOWN_24)?;
+    m.add("ARROW_RIGHT_24", icons::ARROW_RIGHT_24)?;
+    m.add("DEPENDABOT_24", icons::DEPENDABOT_24)?;
+    m.add("BOOKMARK_24", icons::BOOKMARK_24)?;
+    m.add("BOOKMARK_FILL_24", icons::BOOKMARK_FILL_24)?;
+    m.add("CHECK_CIRCLE_FILL_12", icons::CHECK_CIRCLE_FILL_12)?;
+    m.add("LOCATION_16", icons::LOCATION_16)?;
+    m.add("TRIANGLE_RIGHT_24", icons::TRIANGLE_RIGHT_24)?;
+    m.add("REPLY_16", icons::REPLY_16)?;
+    m.add("COPILOT_WARNING_16", icons::COPILOT_WARNING_16)?;
+    m.add("HORIZONTAL_RULE_16", icons::HORIZONTAL_RULE_16)?;
+    m.add("PLUS_24", icons::PLUS_24)?;
+    m.add("ARROW_UP_RIGHT_16", icons::ARROW_UP_RIGHT_16)?;
+    m.add("ARROW_UP_24", icons::ARROW_UP_24)?;
+    m.add("SPARKLE_16", icons::SPARKLE_16)?;
+    m.add("CHECKBOX_FILL_16", icons::CHECKBOX_FILL_16)?;
+    m.add("ARROW_DOWN_LEFT_16", icons::ARROW_DOWN_LEFT_16)?;
+    m.add("FEED_ISSUE_CLOSED_16", icons::FEED_ISSUE_CLOSED_16)?;
+    m.add("BELL_16", icons::BELL_16)?;
+    m.add("SIGN_OUT_24", icons::SIGN_OUT_24)?;
+    m.add("LAW_24", icons::LAW_24)?;
+    m.add("ISSUE_REOPENED_24", icons::ISSUE_REOPENED_24)?;
+    m.add("THREE_BARS_24", icons::THREE_BARS_24)?;
+    m.add("PLUS_CIRCLE_24", icons::PLUS_CIRCLE_24)?;
+    m.add("CHECKBOX_FILL_24", icons::CHECKBOX_FILL_24)?;
+    m.add("REPO_CLONE_16", icons::REPO_CLONE_16)?;
+    m.add("LOGO_GIST_24", icons::LOGO_GIST_24)?;
+    m.add("LOOP_24", icons::LOOP_24)?;
+    m.add("GIT_COMMIT_24", icons::GIT_COMMIT_24)?;
+    m.add("CODE_REVIEW_24", icons::CODE_REVIEW_24)?;
+    m.add("FILE_BINARY_16", icons::FILE_BINARY_16)?;
+    m.add("STOP_24", icons::STOP_24)?;
+    m.add("CHECKLIST_16", icons::CHECKLIST_16)?;
+    m.add("GRABBER_16", icons::GRABBER_16)?;
+    m.add("FEED_TAG_16", icons::FEED_TAG_16)?;
+    m.add("ROCKET_16", icons::ROCKET_16)?;
+    m.add("HEADING_24", icons::HEADING_24)?;
+    m.add("HISTORY_24", icons::HISTORY_24)?;
+    m.add("CALENDAR_24", icons::CALENDAR_24)?;
+    m.add("NO_ENTRY_16", icons::NO_ENTRY_16)?;
+    m.add("MARK_GITHUB_16", icons::MARK_GITHUB_16)?;
+    m.add("CHECK_24", icons::CHECK_24)?;
+    m.add("PERSON_FILL_16", icons::PERSON_FILL_16)?;
+    m.add("FILE_ZIP_16", icons::FILE_ZIP_16)?;
+    m.add("TAB_16", icons::TAB_16)?;
+    m.add("SQUIRREL_24", icons::SQUIRREL_24)?;
+    m.add("SLIDERS_24", icons::SLIDERS_24)?;
+    m.add("REDO_24", icons::REDO_24)?;
+    m.add("KEY_ASTERISK_16", icons::KEY_ASTERISK_16)?;
+    m.add("FILE_REMOVED_24", icons::FILE_REMOVED_24)?;
+    m.add("PROJECT_TEMPLATE_16", icons::PROJECT_TEMPLATE_16)?;
+    m.add("SHIELD_CHECK_24", icons::SHIELD_CHECK_24)?;
+    m.add("PERSON_FILL_24", icons::PERSON_FILL_24)?;
+    m.add("ROWS_24", icons::ROWS_24)?;
+    m.add("RSS_24", icons::RSS_24)?;
+    m.add("ARROW_DOWN_RIGHT_24", icons::ARROW_DOWN_RIGHT_24)?;
+    m.add("NO_ENTRY_FILL_12", icons::NO_ENTRY_FILL_12)?;
+    m.add("MAXIMIZE_16", icons::MAXIMIZE_16)?;
+    m.add("SCREEN_NORMAL_24", icons::SCREEN_NORMAL_24)?;
+    m.add("ZOOM_OUT_24", icons::ZOOM_OUT_24)?;
+    m.add("PLUG_16", icons::PLUG_16)?;
+    m.add("ARROW_RIGHT_16", icons::ARROW_RIGHT_16)?;
+    m.add("ITALIC_24", icons::ITALIC_24)?;
+    m.add("PACKAGE_DEPENDENTS_16", icons::PACKAGE_DEPENDENTS_16)?;
+    m.add("COPILOT_24", icons::COPILOT_24)?;
+    m.add("STOPWATCH_24", icons::STOPWATCH_24)?;
+    m.add("SHIELD_LOCK_24", icons::SHIELD_LOCK_24)?;
+    m.add("LOGO_GITHUB_24", icons::LOGO_GITHUB_24)?;
+    m.add("LIST_UNORDERED_16", icons::LIST_UNORDERED_16)?;
+    m.add("PAPERCLIP_24", icons::PAPERCLIP_24)?;
+    m.add("CODE_SQUARE_24", icons::CODE_SQUARE_24)?;
+    m.add("ZAP_16", icons::ZAP_16)?;
+    m.add("GLOBE_24", icons::GLOBE_24)?;
+    m.add("SIDEBAR_EXPAND_16", icons::SIDEBAR_EXPAND_16)?;
+    m.add("MOVE_TO_BOTTOM_24", icons::MOVE_TO_BOTTOM_24)?;
+    m.add("PROJECT_SYMLINK_16", icons::PROJECT_SYMLINK_16)?;
+    m.add("SHIELD_X_24", icons::SHIELD_X_24)?;
+    m.add("MAIL_24", icons::MAIL_24)?;
+    m.add("ACCESSIBILITY_INSET_16", icons::ACCESSIBILITY_INSET_16)?;
+    m.add("PROJECT_24", icons::PROJECT_24)?;
+    m.add("ISSUE_TRACKED_BY_24", icons::ISSUE_TRACKED_BY_24)?;
+    m.add("NORTH_STAR_24", icons::NORTH_STAR_24)?;
+    m.add("MILESTONE_24", icons::MILESTONE_24)?;
+    m.add("GRAPH_16", icons::GRAPH_16)?;
+    m.add("DIFF_ADDED_24", icons::DIFF_ADDED_24)?;
+    m.add("REPO_PUSH_24", icons::REPO_PUSH_24)?;
+    m.add("SQUARE_24", icons::SQUARE_24)?;
+    m.add("REL_FILE_PATH_24", icons::REL_FILE_PATH_24)?;
+    m.add("COLUMNS_24", icons::COLUMNS_24)?;
+    m.add("MINIMIZE_24", icons::MINIMIZE_24)?;
+    m.add("CHECKBOX_24", icons::CHECKBOX_24)?;
+    m.add("ZAP_24", icons::ZAP_24)?;
+    m.add("FEED_ISSUE_OPEN_16", icons::FEED_ISSUE_OPEN_16)?;
+    m.add("EYE_24", icons::EYE_24)?;
+    m.add("FILE_DIRECTORY_FILL_16", icons::FILE_DIRECTORY_FILL_16)?;
+    m.add("PROJECT_16", icons::PROJECT_16)?;
+    m.add("SIGN_OUT_16", icons::SIGN_OUT_16)?;
+    m.add("FILE_BADGE_24", icons::FILE_BADGE_24)?;
+    m.add("RUBY_24", icons::RUBY_24)?;
+    m.add("SMILEY_16", icons::SMILEY_16)?;
+    m.add("BOLD_24", icons::BOLD_24)?;
+    m.add("PLUG_24", icons::PLUG_24)?;
+    m.add("PENCIL_16", icons::PENCIL_16)?;
+    m.add("BOOK_16", icons::BOOK_16)?;
+    m.add("REPO_LOCKED_24", icons::REPO_LOCKED_24)?;
+    m.add("ALERT_FILL_16", icons::ALERT_FILL_16)?;
+    m.add(
+        "FILE_DIRECTORY_OPEN_FILL_24",
+        icons::FILE_DIRECTORY_OPEN_FILL_24,
+    )?;
+    m.add("GIFT_16", icons::GIFT_16)?;
+    m.add("FILE_SYMLINK_FILE_16", icons::FILE_SYMLINK_FILE_16)?;
+    m.add("FILE_BINARY_24", icons::FILE_BINARY_24)?;
+    m.add("WORKFLOW_16", icons::WORKFLOW_16)?;
+    m.add("REL_FILE_PATH_16", icons::REL_FILE_PATH_16)?;
+    m.add("CONTAINER_16", icons::CONTAINER_16)?;
+    m.add("VERSIONS_24", icons::VERSIONS_24)?;
+    m.add("INBOX_16", icons::INBOX_16)?;
+    m.add("GRABBER_24", icons::GRABBER_24)?;
+    m.add("TELESCOPE_24", icons::TELESCOPE_24)?;
+    m.add("CODE_OF_CONDUCT_24", icons::CODE_OF_CONDUCT_24)?;
+    m.add("CODE_SQUARE_16", icons::CODE_SQUARE_16)?;
+    m.add("TROPHY_24", icons::TROPHY_24)?;
+    m.add("ORGANIZATION_24", icons::ORGANIZATION_24)?;
+    m.add("FILE_24", icons::FILE_24)?;
+    m.add("SHARE_16", icons::SHARE_16)?;
+    m.add("MOON_16", icons::MOON_16)?;
+    Ok(())
+}
---
+++ packages/octicons-pack/octicons_pack-19.18.0.tar.gz/octicons_pack-19.18.0/crates/octicons/src/py_binding/part_03.rs
@@ -0,0 +1,75 @@
+// This file was generated. DO NOT EDIT.
+use crate::icons;
+use pyo3::prelude::*;
+
+pub(super) fn bind_part_3(m: &Bound<PyModule>) -> PyResult<()> {
+    m.add("DOT_16", icons::DOT_16)?;
+    m.add("CHEVRON_RIGHT_16", icons::CHEVRON_RIGHT_16)?;
+    m.add("ORGANIZATION_16", icons::ORGANIZATION_16)?;
+    m.add("DIAMOND_16", icons::DIAMOND_16)?;
+    m.add("SIDEBAR_EXPAND_24", icons::SIDEBAR_EXPAND_24)?;
+    m.add("PULSE_24", icons::PULSE_24)?;
+    m.add("ISSUE_DRAFT_24", icons::ISSUE_DRAFT_24)?;
+    m.add("DUPLICATE_24", icons::DUPLICATE_24)?;
+    m.add("LOCK_24", icons::LOCK_24)?;
+    m.add("FOLD_DOWN_16", icons::FOLD_DOWN_16)?;
+    m.add("FEED_STAR_16", icons::FEED_STAR_16)?;
+    m.add("DUPLICATE_16", icons::DUPLICATE_16)?;
+    m.add("DEVICE_DESKTOP_24", icons::DEVICE_DESKTOP_24)?;
+    m.add("PIN_16", icons::PIN_16)?;
+    m.add("DEVICE_MOBILE_16", icons::DEVICE_MOBILE_16)?;
+    m.add("FILTER_REMOVE_16", icons::FILTER_REMOVE_16)?;
+    m.add("LINK_24", icons::LINK_24)?;
+    m.add("LOG_16", icons::LOG_16)?;
+    m.add("SPONSOR_TIERS_16", icons::SPONSOR_TIERS_16)?;
+    m.add("CHEVRON_UP_12", icons::CHEVRON_UP_12)?;
+    m.add("SQUARE_CIRCLE_16", icons::SQUARE_CIRCLE_16)?;
+    m.add("HUBOT_16", icons::HUBOT_16)?;
+    m.add("CALENDAR_16", icons::CALENDAR_16)?;
+    m.add("X_CIRCLE_FILL_24", icons::X_CIRCLE_FILL_24)?;
+    m.add("IMAGE_24", icons::IMAGE_24)?;
+    m.add("LIST_ORDERED_16", icons::LIST_ORDERED_16)?;
+    m.add("MOVE_TO_END_24", icons::MOVE_TO_END_24)?;
+    m.add("COLUMNS_16", icons::COLUMNS_16)?;
+    m.add("SPACE_24", icons::SPACE_24)?;
+    m.add("SHARE_24", icons::SHARE_24)?;
+    m.add("SYNC_16", icons::SYNC_16)?;
+    m.add("GOAL_16", icons::GOAL_16)?;
+    m.add("QUESTION_24", icons::QUESTION_24)?;
+    m.add("SQUARE_CIRCLE_24", icons::SQUARE_CIRCLE_24)?;
+    m.add("FILE_CODE_16", icons::FILE_CODE_16)?;
+    m.add("ZOOM_OUT_16", icons::ZOOM_OUT_16)?;
+    m.add("THUMBSDOWN_24", icons::THUMBSDOWN_24)?;
+    m.add("SPONSOR_TIERS_24", icons::SPONSOR_TIERS_24)?;
+    m.add("ACCESSIBILITY_24", icons::ACCESSIBILITY_24)?;
+    m.add("STOPWATCH_16", icons::STOPWATCH_16)?;
+    m.add("ARROW_BOTH_16", icons::ARROW_BOTH_16)?;
+    m.add("HEART_FILL_16", icons::HEART_FILL_16)?;
+    m.add("PERSON_ADD_24", icons::PERSON_ADD_24)?;
+    m.add("SCREEN_FULL_24", icons::SCREEN_FULL_24)?;
+    m.add("NORTH_STAR_16", icons::NORTH_STAR_16)?;
+    m.add("CPU_16", icons::CPU_16)?;
+    m.add("FILE_DIRECTORY_24", icons::FILE_DIRECTORY_24)?;
+    m.add("BUG_24", icons::BUG_24)?;
+    m.add("FEED_MERGED_16", icons::FEED_MERGED_16)?;
+    m.add("DASH_24", icons::DASH_24)?;
+    m.add("SHIELD_LOCK_16", icons::SHIELD_LOCK_16)?;
+    m.add("NO_ENTRY_24", icons::NO_ENTRY_24)?;
+    m.add("RSS_16", icons::RSS_16)?;
+    m.add("DEVICE_CAMERA_VIDEO_24", icons::DEVICE_CAMERA_VIDEO_24)?;
+    m.add("TELESCOPE_FILL_16", icons::TELESCOPE_FILL_16)?;
+    m.add("FEED_FORKED_16", icons::FEED_FORKED_16)?;
+    m.add("STACK_24", icons::STACK_24)?;
+    m.add("PROJECT_ROADMAP_24", icons::PROJECT_ROADMAP_24)?;
+    m.add("BOOKMARK_SLASH_FILL_16", icons::BOOKMARK_SLASH_FILL_16)?;
+    m.add("BRIEFCASE_16", icons::BRIEFCASE_16)?;
+    m.add(
+        "GIT_PULL_REQUEST_DRAFT_16",
+        icons::GIT_PULL_REQUEST_DRAFT_16,
+    )?;
+    m.add("UNREAD_24", icons::UNREAD_24)?;
+    m.add("LINK_16", icons::LINK_16)?;
+    m.add("CODE_REVIEW_16", icons::CODE_REVIEW_16)?;
+    m.add("SPARKLE_FILL_16", icons::SPARKLE_FILL_16)?;
+    Ok(())
+}
